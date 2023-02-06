2dee59d0e3c990724e3b1760f254c14964c962d1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-07 00:03:13 +0900

    Create Dependencies.scala

---
+++ project/Dependencies.scala
@@ -0,0 +1,12 @@
+/**
+ *  This file is part of the ldbc.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+import sbt._
+
+object Dependencies {
+
+  val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.6"
+}
