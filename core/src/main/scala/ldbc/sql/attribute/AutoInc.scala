0dac7c86f4fbb9ab20ea8a27f6f4fca87e5f2995
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-11 23:34:48 +0900

    Create AutoInc attribute

---
+++ core/src/main/scala/ldbc/sql/attribute/AutoInc.scala
@@ -0,0 +1,16 @@
+/**
+ *  This file is part of the ldbc.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package ldbc.sql.attribute
+
+/**
+ * Model for specifying an additional attribute AUTO_INCREMENT for DataType.
+ */
+private[ldbc] case class AutoInc[T <: Byte | Short | Int | Long]() extends Attribute[T]:
+
+  override def queryString: String = "AUTO_INCREMENT"
+
+  override def toString: String = queryString
