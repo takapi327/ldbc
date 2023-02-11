0878a3ffb88a4a0cfcbd3b3d06fed18644faa859
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-11 23:34:17 +0900

    Create attribute

---
+++ core/src/main/scala/ldbc/sql/attribute/package.scala
@@ -0,0 +1,18 @@
+/**
+ *  This file is part of the ldbc.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package ldbc.sql
+
+package object attribute:
+
+  private[ldbc] trait Attribute[T]:
+
+    /** Define an SQL query string for each attribute.
+     *
+     * @return
+     * SQL query string
+     */
+    def queryString: String
