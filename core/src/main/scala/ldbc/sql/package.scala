dc5bad234c8a367aa66640a613cb1a083370814d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-13 21:59:22 +0900

    Create package

---
+++ core/src/main/scala/ldbc/sql/package.scala
@@ -0,0 +1,15 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc
+
+/**
+ * Top-level imports provide aliases for the most commonly used types and modules. A typical starting set of imports might look something like this.
+ * 
+ * example:
+ * {{{
+ *   import ldbc.sql.*
+ * }}}
+ */
+package object sql extends DataTypes
