ab925d9469f09e26a7ae173bb71fa3fc50b8129b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-15 00:08:41 +0900

    Create free

---
+++ core/src/main/scala/ldbc/sql/free/Column.scala
@@ -0,0 +1,35 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.sql.free
+
+import ldbc.sql.DataType
+import ldbc.sql.attribute.Attribute
+
+/** Trait for representing SQL Column
+ *
+ * @tparam T
+ *   Scala types that match SQL DataType
+ */
+private[ldbc] trait Column[T]:
+
+  /** Column Field Name */
+  def label: String
+
+  /** Column type */
+  def dataType: DataType[T]
+
+  /** Extra attribute of column */
+  def attributes: Seq[Attribute[T]]
+
+  /** Column comment */
+  def comment: Option[String]
+
+  /** Define SQL query string for each Column
+   *
+   * @return
+   * SQL query string
+   */
+  def queryString: String
+  
