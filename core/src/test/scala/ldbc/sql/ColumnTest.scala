5887ebb2f6a49d7e40a73cc2e5e17b2aca221f94
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-20 23:46:29 +0900

    Fix core test

--- core/src/test/scala/ldbc/sql/ColumnTest.scala
+++ core/src/test/scala/ldbc/sql/ColumnTest.scala
@@ -6,8 +6,6 @@ package ldbc.core
 
 import org.specs2.mutable.Specification
 
-import cats.Id
-
 import ldbc.core.DataType.*
 import ldbc.core.Character.*
 import ldbc.core.attribute.AutoInc
@@ -17,42 +15,42 @@ object ColumnTest extends Specification:
   "Column Test" should {
     "The query string of the Column model generated with only label and DataType matches the specified string." in {
 
-      val column = Column[Id, Long]("id", Bigint(64, None))
+      val column = Column[Long]("id", Bigint(64, None))
 
       column.queryString === "`id` BIGINT(64) NOT NULL"
     }
 
     "The query string of the Column model generated with only label and DataType and comment matches the specified string." in {
 
-      val column = Column[Id, Long]("id", Bigint(64, None), "identifier")
+      val column = Column[Long]("id", Bigint(64, None), "identifier")
 
       column.queryString === "`id` BIGINT(64) NOT NULL COMMENT 'identifier'"
     }
 
     "The query string of the Column model generated with only label and DataType and attributes matches the specified string." in {
 
-      val column = Column[Id, Long]("id", Bigint(64, None), AutoInc[Long]())
+      val column = Column[Long]("id", Bigint(64, None), AutoInc[Long]())
 
       column.queryString === "`id` BIGINT(64) NOT NULL AUTO_INCREMENT"
     }
 
     "The query string of the Column model generated with only label and DataType and attributes and comment matches the specified string." in {
 
-      val column = Column[Id, Long]("id", Bigint(64, None), "identifier", AutoInc[Long]())
+      val column = Column[Long]("id", Bigint(64, None), "identifier", AutoInc[Long]())
 
       column.queryString === "`id` BIGINT(64) NOT NULL AUTO_INCREMENT COMMENT 'identifier'"
     }
 
     "The query string of the Column model generated with only label and DataType matches the specified string." in {
 
-      val column = Column[Id, String]("name", Varchar(255, None, Some(Big5(None))))
+      val column = Column[String]("name", Varchar(255, None, Some(Big5(None))))
 
       column.queryString === "`name` VARCHAR(255) CHARACTER SET Big5 NOT NULL"
     }
 
     "The query string of the Column model generated with only label and DataType and comment matches the specified string." in {
 
-      val column = Column[Id, String]("name", Varchar(255, None, Some(Big5(None))), "name")
+      val column = Column[String]("name", Varchar(255, None, Some(Big5(None))), "name")
 
       column.queryString === "`name` VARCHAR(255) CHARACTER SET Big5 NOT NULL COMMENT 'name'"
     }
@@ -60,7 +58,7 @@ object ColumnTest extends Specification:
     "The query string of the Column model generated with only label and DataType and comment matches the specified string." in {
 
       val column =
-        Column[Id, String]("name", Varchar(255, None, None).CHARACTER_SET(Big5(Some(Big5.Collate.big5_bin))), "name")
+        Column[String]("name", Varchar(255, None, None).CHARACTER_SET(Big5(Some(Big5.Collate.big5_bin))), "name")
 
       column.queryString === "`name` VARCHAR(255) CHARACTER SET Big5 COLLATE big5_bin NOT NULL COMMENT 'name'"
     }
--- core/src/test/scala/ldbc/sql/TableTest.scala
+++ core/src/test/scala/ldbc/sql/TableTest.scala
@@ -2,47 +2,38 @@
   * distributed with this source code.
   */
 
-package ldbc.sql
+package ldbc.core
 
 import org.scalatest.flatspec.AnyFlatSpec
 
-import cats.Id
-
-import ldbc.sql.{ Column, Table }
-import ldbc.sql.DataTypes.*
+import ldbc.core.*
 
 class TableTest extends AnyFlatSpec:
 
   it should "Successful generation of Table" in {
     assertCompiles("""
-      import cats.Id
-
-      import ldbc.sql.{ Column, Table }
-      import ldbc.sql.DataTypes.*
+      import ldbc.core.*
 
       case class User(id: Long, name: String, age: Int)
 
-      val table: Table[Id, User] = Table[Id, User]("user")(
-        Column("id", BIGINT(64)),
-        Column("name", VARCHAR(255)),
-        Column("age", INT(255))
+      val table: Table[User] = Table[User]("user")(
+        column("id", BIGINT(64)),
+        column("name", VARCHAR(255)),
+        column("age", INT(255))
       )
     """.stripMargin)
   }
 
   it should "If columns are passed in an order that does not match the type information of the properties of the specified case class, a compile error will occur." in {
     assertDoesNotCompile("""
-      import cats.Id
-
-      import ldbc.sql.{ Column, Table }
-      import ldbc.sql.DataTypes.*
+      import ldbc.core.*
 
       case class User(id: Long, name: String, age: Int)
 
-      val table: Table[Id, User] = Table[Id, User]("user")(
-        Column("name", VARCHAR(255)),
-        Column("id", BIGINT(64)),
-        Column("age", INT(255))
+      val table: Table[User] = Table[User]("user")(
+        column("name", VARCHAR(255)),
+        column("id", BIGINT(64)),
+        column("age", INT(255))
       )
     """.stripMargin)
   }
@@ -50,7 +41,7 @@ class TableTest extends AnyFlatSpec:
   it should "The type of the column accessed by selectDynamic matches the type of the specified column." in {
     case class User(id: Long, name: String, age: Int)
 
-    val table: Table[Id, User] = Table[Id, User]("user")(
+    val table: Table[User] = Table[User]("user")(
       Column("id", BIGINT(64)),
       Column("name", VARCHAR(255)),
       Column("age", INT(255))
@@ -62,11 +53,52 @@ class TableTest extends AnyFlatSpec:
   it should "The column accessed by selectDynamic does not match the type of the specified column." in {
     case class User(id: Long, name: String, age: Int)
 
-    val table: Table[Id, User] = Table[Id, User]("user")(
-      Column("id", BIGINT(64)),
-      Column("name", VARCHAR(255)),
-      Column("age", INT(255))
+    val table: Table[User] = Table[User]("user")(
+      column("id", BIGINT(64)),
+      column("name", VARCHAR(255)),
+      column("age", INT(255))
     )
 
     table.id !== Column("name", VARCHAR(255))
+  }
+
+  it should "" in {
+
+    import cats.data.NonEmptyList
+
+    import ldbc.core.attribute.*
+    import ldbc.core.syntax.given
+
+    case class User(
+      id:        Long,
+      name:      String,
+      age:       Option[Int],
+      updatedAt: java.time.LocalDateTime,
+      createdAt: java.time.LocalDateTime
+    )
+
+    case class Country(id: Long)
+
+    val countryTable: Table[Country] = Table[Country]("country")(
+      column("id", BIGINT(64), AutoInc(), Key.Primary())
+    )
+
+    val table: Table[User] = Table[User]("user")(
+      column("id", BIGINT(64), AutoInc(), Key.Unique()),
+      column("name", VARCHAR(255)),
+      column("age", INT(255).DEFAULT_NULL),
+      column("updated_at", TIMESTAMP.DEFAULT_CURRENT_TIMESTAMP()),
+      column("created_at", TIMESTAMP.DEFAULT_CURRENT_TIMESTAMP(true))
+    ).keys(table =>
+      Seq(
+        PrimaryKey(None, NonEmptyList.of(table.id, table.age), None),
+        IndexKey(Some("index_age"), None, NonEmptyList.one(table.age), None),
+        Constraint("fk_id", ForeignKey(None, NonEmptyList.one(table.id), Reference(countryTable)(countryTable.id)))
+      )
+    )
+
+    val query = CreateTableQueryGenerator.build(table).querySting
+    println(CreateTableQueryGenerator.build(countryTable).querySting)
+    println(query)
+    true
   }
