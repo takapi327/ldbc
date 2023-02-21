6d03b451407c9e63f36ea5576345c7e3d30511e0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-22 00:54:03 +0900

    Fix ResultSetReader

--- module/ldbc-sql/src/main/scala/ldbc/sql/ResultSetReader.scala
+++ module/ldbc-sql/src/main/scala/ldbc/sql/ResultSetReader.scala
@@ -22,42 +22,41 @@ import ldbc.core.Column
   */
 trait ResultSetReader[F[_], T]:
 
-  def read(resultSet: ResultSet[F]): F[T]
+  def read(resultSet: ResultSet[F], columnLabel: String): F[T]
 
 object ResultSetReader:
 
-  def apply[F[_], T](func: ResultSet[F] => String => F[T])(using column: Column[T]): ResultSetReader[F, T] =
+  def apply[F[_], T](func: ResultSet[F] => String => F[T]): ResultSetReader[F, T] =
     new ResultSetReader[F, T]:
-      override def read(resultSet: ResultSet[F]): F[T] =
-        func(resultSet)(column.label)
+      override def read(resultSet: ResultSet[F], columnLabel: String): F[T] =
+        func(resultSet)(columnLabel)
 
-  given [F[_]](using Column[String]):      ResultSetReader[F, String]      = ResultSetReader(_.getString)
-  given [F[_]](using Column[Boolean]):     ResultSetReader[F, Boolean]     = ResultSetReader(_.getBoolean)
-  given [F[_]](using Column[Byte]):        ResultSetReader[F, Byte]        = ResultSetReader(_.getByte)
-  given [F[_]](using Column[Array[Byte]]): ResultSetReader[F, Array[Byte]] = ResultSetReader(_.getBytes)
-  given [F[_]](using Column[Short]):       ResultSetReader[F, Short]       = ResultSetReader(_.getShort)
-  given [F[_]](using Column[Int]):         ResultSetReader[F, Int]         = ResultSetReader(_.getInt)
-  given [F[_]](using Column[Long]):        ResultSetReader[F, Long]        = ResultSetReader(_.getLong)
-  given [F[_]](using Column[Float]):       ResultSetReader[F, Float]       = ResultSetReader(_.getFloat)
-  given [F[_]](using Column[Double]):      ResultSetReader[F, Double]      = ResultSetReader(_.getDouble)
-  given [F[_]](using Column[Date]):        ResultSetReader[F, Date]        = ResultSetReader(_.getDate)
-  given [F[_]](using Column[Time]):        ResultSetReader[F, Time]        = ResultSetReader(_.getTime)
-  given [F[_]](using Column[Timestamp]):   ResultSetReader[F, Timestamp]   = ResultSetReader(_.getTimestamp)
-  given [F[_]](using Column[InputStream]): ResultSetReader[F, InputStream] = ResultSetReader(_.getAsciiStream)
-  given [F[_]](using Column[Object]):      ResultSetReader[F, Object]      = ResultSetReader(_.getObject)
-  given [F[_]](using Column[Reader]):      ResultSetReader[F, Reader]      = ResultSetReader(_.getCharacterStream)
-  given [F[_]](using Column[BigDecimal]):  ResultSetReader[F, BigDecimal]  = ResultSetReader(_.getBigDecimal)
+  given [F[_]]: ResultSetReader[F, String]      = ResultSetReader(_.getString)
+  given [F[_]]: ResultSetReader[F, Boolean]     = ResultSetReader(_.getBoolean)
+  given [F[_]]: ResultSetReader[F, Byte]        = ResultSetReader(_.getByte)
+  given [F[_]]: ResultSetReader[F, Array[Byte]] = ResultSetReader(_.getBytes)
+  given [F[_]]: ResultSetReader[F, Short]       = ResultSetReader(_.getShort)
+  given [F[_]]: ResultSetReader[F, Int]         = ResultSetReader(_.getInt)
+  given [F[_]]: ResultSetReader[F, Long]        = ResultSetReader(_.getLong)
+  given [F[_]]: ResultSetReader[F, Float]       = ResultSetReader(_.getFloat)
+  given [F[_]]: ResultSetReader[F, Double]      = ResultSetReader(_.getDouble)
+  given [F[_]]: ResultSetReader[F, Date]        = ResultSetReader(_.getDate)
+  given [F[_]]: ResultSetReader[F, Time]        = ResultSetReader(_.getTime)
+  given [F[_]]: ResultSetReader[F, Timestamp]   = ResultSetReader(_.getTimestamp)
+  given [F[_]]: ResultSetReader[F, InputStream] = ResultSetReader(_.getAsciiStream)
+  given [F[_]]: ResultSetReader[F, Object]      = ResultSetReader(_.getObject)
+  given [F[_]]: ResultSetReader[F, Reader]      = ResultSetReader(_.getCharacterStream)
+  given [F[_]]: ResultSetReader[F, BigDecimal]  = ResultSetReader(_.getBigDecimal)
 
   given [F[_]: Functor](using loader: ResultSetReader[F, Timestamp]): ResultSetReader[F, LocalDateTime] with
 
-    override def read(resultSet: ResultSet[F]): F[LocalDateTime] =
-      for result <- loader.read(resultSet)
-      yield result.toLocalDateTime
+    override def read(resultSet: ResultSet[F], columnLabel: String): F[LocalDateTime] =
+      loader.read(resultSet, columnLabel).map(_.toLocalDateTime)
 
   given [F[_]: Monad, A](using loader: ResultSetReader[F, A]): ResultSetReader[F, Option[A]] with
 
-    override def read(resultSet: ResultSet[F]): F[Option[A]] =
+    override def read(resultSet: ResultSet[F], columnLabel: String): F[Option[A]] =
       for
-        result <- loader.read(resultSet)
+        result <- loader.read(resultSet, columnLabel)
         bool   <- resultSet.wasNull()
       yield if bool then None else Some(result)
