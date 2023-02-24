be6b07a1b0967a8e7cbf244ec7044bfd73f21808
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-24 16:22:01 +0900

    Create dsl

---
+++ module/ldbc-sql/src/main/scala/ldbc/sql/dsl/package.scala
@@ -0,0 +1,11 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.sql
+
+import cats.effect.IO
+
+package object dsl:
+
+  val io: SQLSyntax[IO] = new SQLSyntax[IO] {}
