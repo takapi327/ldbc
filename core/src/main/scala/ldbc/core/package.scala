5ba651743acdae495e14a3873fe2f6612e86a9c6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-24 16:30:56 +0900

    Create Alias

--- core/src/main/scala/ldbc/core/package.scala
+++ core/src/main/scala/ldbc/core/package.scala
@@ -4,8 +4,6 @@
 
 package ldbc
 
-import ldbc.core.attribute.Attribute
-
 /** Top-level imports provide aliases for the most commonly used types and modules. A typical starting set of imports
   * might look something like this.
   *
@@ -14,28 +12,4 @@ import ldbc.core.attribute.Attribute
   *   import ldbc.core.*
   * }}}
   */
-package object core extends DataTypes:
-
-  def column[T](
-    label:    String,
-    dataType: DataType[T]
-  ): Column[T] = Column[T](label, dataType)
-
-  def column[T](
-    label:    String,
-    dataType: DataType[T],
-    comment:  String
-  ): Column[T] = Column[T](label, dataType, comment)
-
-  def column[T](
-    label:      String,
-    dataType:   DataType[T],
-    attributes: Attribute[T]*
-  ): Column[T] = Column[T](label, dataType, attributes: _*)
-
-  def column[T](
-    label:      String,
-    dataType:   DataType[T],
-    comment:    String,
-    attributes: Attribute[T]*
-  ): Column[T] = Column[T](label, dataType, comment, attributes: _*)
+package object core extends Alias, DataTypes
