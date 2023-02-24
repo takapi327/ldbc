a37f11db3b53720192eff0b318ee4e5f6ca98913
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-24 17:12:19 +0900

    Create sql syntax package object

---
+++ module/ldbc-sql/src/main/scala/ldbc/sql/syntax/package.scala
@@ -0,0 +1,17 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.sql
+
+import ldbc.core.syntax.DataTypeConversion
+
+/** Top-level imports provide aliases for the most commonly used types and modules. A typical starting set of imports
+ * might look something like this.
+ *
+ * example:
+ * {{{
+ *   import ldbc.sql.syntax.{ *, given }
+ * }}}
+ */
+package object syntax extends TableSyntax, DataTypeConversion
