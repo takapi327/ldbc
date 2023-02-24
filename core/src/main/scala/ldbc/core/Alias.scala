932b653b98b64c5c5d9b2551de2fef736ec7fb38
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-24 16:31:26 +0900

    Action sbt scalafmtAll

--- core/src/main/scala/ldbc/core/Alias.scala
+++ core/src/main/scala/ldbc/core/Alias.scala
@@ -1,6 +1,6 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.core
 
@@ -9,25 +9,25 @@ import ldbc.core.attribute.Attribute
 private[ldbc] trait Alias:
 
   def column[T](
-                 label: String,
-                 dataType: DataType[T]
-               ): Column[T] = Column[T](label, dataType)
+    label:    String,
+    dataType: DataType[T]
+  ): Column[T] = Column[T](label, dataType)
 
   def column[T](
-                 label: String,
-                 dataType: DataType[T],
-                 comment: String
-               ): Column[T] = Column[T](label, dataType, comment)
+    label:    String,
+    dataType: DataType[T],
+    comment:  String
+  ): Column[T] = Column[T](label, dataType, comment)
 
   def column[T](
-                 label: String,
-                 dataType: DataType[T],
-                 attributes: Attribute[T]*
-               ): Column[T] = Column[T](label, dataType, attributes: _*)
+    label:      String,
+    dataType:   DataType[T],
+    attributes: Attribute[T]*
+  ): Column[T] = Column[T](label, dataType, attributes: _*)
 
   def column[T](
-                 label: String,
-                 dataType: DataType[T],
-                 comment: String,
-                 attributes: Attribute[T]*
-               ): Column[T] = Column[T](label, dataType, comment, attributes: _*)
+    label:      String,
+    dataType:   DataType[T],
+    comment:    String,
+    attributes: Attribute[T]*
+  ): Column[T] = Column[T](label, dataType, comment, attributes: _*)
