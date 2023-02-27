b30357ab5b0023d6fd97c9a9d55b539cdf927a98
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-27 19:54:13 +0900

    Create SQLSyntax

---
+++ module/ldbc-dsl-io/src/main/scala/ldbc/dsl/SQLSyntax.scala
@@ -0,0 +1,27 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.dsl
+
+import cats.data.Kleisli
+
+import ldbc.sql.{ Connection, ResultSetConsumer }
+
+/** Trait for generating SQL models from string completion knowledge.
+ *
+ * @tparam F
+ *   The effect type
+ */
+trait SQLSyntax[F[_]]:
+
+  extension (sc: StringContext)
+    inline def sql(inline args: ParameterBinder[F]*): SQL[F] =
+      val strings     = sc.parts.iterator
+      val expressions = args.iterator
+      SQL(strings.mkString("?"), expressions.toSeq)
+
+  extension (sql: SQL[F])
+    def query[T](using consumer: ResultSetConsumer[F, T]): Kleisli[F, Connection[F], T]
+
+    def update(): Kleisli[F, Connection[F], Int]
