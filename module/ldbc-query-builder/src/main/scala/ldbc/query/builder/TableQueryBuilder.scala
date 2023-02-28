8b05855287fdb7399ff248bd6c6cac2d5cf25ddf
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-02-28 21:37:38 +0900

    Create TableQueryBuilder

---
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/TableQueryBuilder.scala
@@ -0,0 +1,74 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.query.builder
+
+import ldbc.core.*
+import ldbc.core.attribute.*
+
+private[ldbc] case class TableQueryBuilder(table: Table[?]):
+
+  private val autoInc = table.*.filter {
+    case c: Column[?] => c.attributes.contains(AutoInc())
+    case unknown => throw new IllegalStateException(s"$unknown is not a Column.")
+  }
+  private val primaryKey = table.*.filter {
+    case c: Column[?] => c.attributes.contains(Key.Primary())
+    case unknown => throw new IllegalStateException(s"$unknown is not a Column.")
+  }
+  private val keyPart = table.keyDefinitions.flatMap {
+    case key: PrimaryKey => key.keyPart.toList
+    case key: UniqueKey => key.keyPart.toList
+    case _ => List.empty
+  }
+
+  require(
+    table.*.distinctBy {
+      case c: Column[?] => c.label
+      case unknown => throw new IllegalStateException(s"$unknown is not a Column.")
+    }.length == table.*.length,
+    "Columns with the same name cannot be defined in a single table."
+  )
+
+  require(
+    autoInc.length <= 1,
+    "AUTO_INCREMENT can only be set on one of the table columns."
+  )
+
+  require(
+    primaryKey.length <= 1
+      && table.keyDefinitions.count(_.label == "PRIMARY KEY") <= 1
+      && primaryKey.length + table.keyDefinitions.count(_.label == "PRIMARY KEY") <= 1,
+    "PRIMARY KEY can only be set on one of the table columns."
+  )
+
+  require(
+    autoInc.count {
+      case column: Column[?] => column.attributes.contains(Key.Primary()) || column.attributes.contains(Key.Unique())
+      case unknown => throw new IllegalStateException(s"$unknown is not a Column.")
+    } == 1 || keyPart.count(key => autoInc.map {
+      case c: Column[?] => c.label
+      case unknown => throw new IllegalStateException(s"$unknown is not a Column.")
+    }.contains(key)) == 1,
+    "The columns with AUTO_INCREMENT must have a Primary Key or Unique Key."
+  )
+
+  private val columnDefinitions: Seq[String] =
+    table.*.map {
+      case c: Column[?] => c.queryString
+      case unknown => throw new IllegalStateException(s"$unknown is not a Column.")
+    }
+
+  private val options: Seq[String] =
+    columnDefinitions ++ table.keyDefinitions.map(_.queryString)
+
+  def querySting: String =
+    s"""
+       |CREATE TABLE `${table.name}` (
+       |  ${options.mkString(",\n  ")}
+       |);
+       |
+       |""".stripMargin
+
+object TableQueryBuilder
