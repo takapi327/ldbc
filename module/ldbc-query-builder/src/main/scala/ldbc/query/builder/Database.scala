8ac7cb84d00e850ecbfcd7ae2b1dd78d84db6a14
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-03-05 00:08:35 +0900

    Action sbt scalafmtAll

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/Database.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/Database.scala
@@ -1,23 +1,22 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.query.builder
 
 import ldbc.core.Character
 
-/**
- * A model representing SQL database information.
- *
- * @param label
- *   Database Name
- * @param characterSet
- *   A value to represent the character set and collation.
- */
+/** A model representing SQL database information.
+  *
+  * @param label
+  *   Database Name
+  * @param characterSet
+  *   A value to represent the character set and collation.
+  */
 case class Database(label: String, characterSet: Option[Character]):
 
   def queryString: String =
-    s"CREATE DATABASE `$label`" + characterSet.map(v => s" ${v.queryString}").getOrElse("")
+    s"CREATE DATABASE `$label`" + characterSet.map(v => s" ${ v.queryString }").getOrElse("")
 
 object Database:
 
