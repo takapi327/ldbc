3579ae18ef2739be1378ab38edf9b717d052624e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-03-18 23:53:58 +0900

    Create SchemaSpyGraphvizConfig

---
+++ module/ldbc-schemaspy/src/main/scala/ldbc/schemaspy/SchemaSpyGraphvizConfig.scala
@@ -0,0 +1,22 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.schemaspy
+
+import org.schemaspy.output.diagram.graphviz.GraphvizConfig
+
+class SchemaSpyGraphvizConfig(
+  graphvizDir: Option[String] = None,
+  renderer:    Option[String] = None,
+  lowQuality:  Boolean        = false,
+  imageFormat: String         = "png"
+) extends GraphvizConfig:
+
+  override def getGraphvizDir: String = graphvizDir.orNull
+
+  override def getRenderer: String = renderer.orNull
+
+  override def isLowQuality: Boolean = lowQuality
+
+  override def getImageFormat: String = imageFormat
