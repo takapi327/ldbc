2ed7f8146215d497aa350cd3f9a62029da7e00ca
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-03-27 02:44:01 +0900

    Create SlickTable

---
+++ module/ldbc-slick/src/main/scala/ldbc/slick/SlickTable.scala
@@ -0,0 +1,22 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.slick
+
+import scala.language.dynamics
+
+import slick.ast.TableNode
+import slick.lifted.{ Rep, ProvenShape }
+
+import ldbc.core.{ Table, Key }
+import ldbc.slick.lifted.Tag
+
+trait SlickTable[P <: Product] extends Table[P], Rep[P]:
+
+  def tag: Tag
+
+  def tableNode: TableNode
+
+  def allColumnShape: ProvenShape[?]
+
