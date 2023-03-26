0ef2152e750195c0316cf63b1602c9e9c4a5c637
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-03-27 02:04:29 +0900

    Create TableQuery

---
+++ module/ldbc-slick/src/main/scala/ldbc/slick/lifted/TableQuery.scala
@@ -0,0 +1,21 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.slick.lifted
+
+import slick.lifted.{ Query, ShapedValue, FlatShapeLevel }
+
+import ldbc.slick.SlickTable
+
+class TableQuery[T <: SlickTable[?]](table: T) extends Query[T, TableQuery.Extract[T], Seq]:
+
+  override lazy val shaped: ShapedValue[T, TableQuery.Extract[T]] =
+    ShapedValue(table, slick.lifted.RepShape[FlatShapeLevel, T, TableQuery.Extract[T]])
+
+  override lazy val toNode = shaped.toNode
+
+object TableQuery:
+
+  type Extract[T] = T match
+    case SlickTable[t] => t
