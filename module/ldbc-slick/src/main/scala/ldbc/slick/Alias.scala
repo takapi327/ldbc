710c06c09e26659fe9866d45580fee2dcedb2d76
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-03-27 22:07:54 +0900

    Create Alias

---
+++ module/ldbc-slick/src/main/scala/ldbc/slick/Alias.scala
@@ -0,0 +1,36 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.slick
+
+import slick.ast.TypedType
+
+import ldbc.core.DataType
+import ldbc.core.attribute.{ Attribute, AutoInc, Key }
+
+private[ldbc] trait Alias:
+
+  def column[T](
+    label: String,
+    dataType: DataType[T]
+  )(using tt: TypedType[T]): TypedColumn[T] = TypedColumn[T](label, dataType)
+
+  def column[T](
+    label: String,
+    dataType: DataType[T],
+    comment: String
+  )(using tt: TypedType[T]): TypedColumn[T] = TypedColumn[T](label, dataType, comment)
+
+  def column[T](
+    label: String,
+    dataType: DataType[T],
+    attributes: Attribute[T]*
+  )(using tt: TypedType[T]): TypedColumn[T] = TypedColumn[T](label, dataType, attributes: _*)
+
+  def column[T](
+    label: String,
+    dataType: DataType[T],
+    comment: String,
+    attributes: Attribute[T]*
+  )(using tt: TypedType[T]): TypedColumn[T] = TypedColumn[T](label, dataType, comment, attributes: _*)
