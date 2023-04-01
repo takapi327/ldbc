b0f43fa029f4fe3a2af027eb59e75bcc58efd961
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-04-01 15:45:22 +0900

    Action sbt scalafmtAll

--- core/src/main/scala/ldbc/core/syntax/OrderingTable.scala
+++ core/src/main/scala/ldbc/core/syntax/OrderingTable.scala
@@ -1,34 +1,32 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.core.syntax
 
 import ldbc.core.{ Table, ForeignKey, Constraint }
 
-/**
- * Trait that provides for sorting an array of tables.
- */
+/** Trait that provides for sorting an array of tables.
+  */
 trait OrderingTable:
 
-  /**
-   * Methods for calculating specific weights according to external reference keys.
-   *
-   * @param table
-   * Trait for generating SQL table information.
-   */
+  /** Methods for calculating specific weights according to external reference keys.
+    *
+    * @param table
+    *   Trait for generating SQL table information.
+    */
   private def calculateWeightByReference(table: Table[?]): Int =
     if table.keyDefinitions.nonEmpty then
       table.keyDefinitions.map {
         case _: ForeignKey => 1
-        case constraint: Constraint => constraint.key match
-          case _: ForeignKey => 1
-          case _             => 0
+        case constraint: Constraint =>
+          constraint.key match
+            case _: ForeignKey => 1
+            case _             => 0
       }.sum
     else 0
 
-  extension (table: Table[?])
-    def calculateWeight: Int = calculateWeightByReference(table)
+  extension (table: Table[?]) def calculateWeight: Int = calculateWeightByReference(table)
 
   given Ordering[Table[?]] with
 
