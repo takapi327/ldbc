227c6259eaba8c1d28a9de7b85d0ecca63144d41
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-04-04 23:50:09 +0900

    Fix Character

--- core/src/main/scala/ldbc/core/Character.scala
+++ core/src/main/scala/ldbc/core/Character.scala
@@ -6,20 +6,27 @@ package ldbc.core
 
 /** A model for representing character sets and collations to be set in column definitions for the string data types
   * CHAR, VARCHAR, TEXT, ENUM, SET, and any synonym.
+  *
+  * @param name
+  *   Character name
+  *
+  * @param collate
+  *   Collate information to be set for Character
   */
-trait Character:
-
-  /** Character name */
-  def name: String
-
-  /** Collate information to be set for Character */
-  def collate: Option[Character.Collate]
+case class Character(name: String, collate: Option[String]):
 
   /** Methods for setting collate to Character */
-  def set(collate: Character.Collate): Character
+  def set(collate: String): Character = this.copy(collate = Some(collate))
 
-  val queryString: String = collate.fold(s"CHARACTER SET $name")(v => s"CHARACTER SET $name COLLATE ${ v.name }")
+  /** Variable that contains the SQL string of Character
+    *
+    * @return
+    *   SQL query string
+    */
+  val queryString: String = collate.fold(s"CHARACTER SET $name")(v => s"CHARACTER SET $name COLLATE $v")
 
 object Character:
 
-  trait Collate(val name: String)
+  def apply(name: String): Character = Character(name, None)
+
+  def apply(name: String, collate: String): Character = Character(name, Some(collate))
