5860346e20c8dee1a67f60803ae3e23276eebf18
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-04-16 01:33:12 +0900

    Fix SchemaSpyGenerator

--- module/ldbc-schemaspy/src/main/scala/ldbc/schemaspy/SchemaSpyGenerator.scala
+++ module/ldbc-schemaspy/src/main/scala/ldbc/schemaspy/SchemaSpyGenerator.scala
@@ -4,80 +4,225 @@
 
 package ldbc.schemaspy
 
-import java.io.{ File, IOException }
+import java.io.{ File, IOException, FileFilter }
+import java.nio.file.{ Files, Path, StandardOpenOption }
+import java.nio.charset.StandardCharsets
 import java.sql.SQLException
+import java.util.ArrayList
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
-import org.schemaspy.{ Arguments, LayoutFolder, SchemaAnalyzer }
-import org.schemaspy.model.{ EmptySchemaException, InvalidConfigurationException }
-import org.schemaspy.input.dbms.service.{ DatabaseServiceFactory, SqlService }
-import org.schemaspy.input.dbms.exceptions.ConnectionFailure
-import org.schemaspy.output.OutputProducer
-import org.schemaspy.output.xml.dom.XmlProducerUsingDOM
+import scala.util.Using
 
-import ldbc.core.Database
+import org.apache.commons.io.filefilter.FileFilterUtils
+
+import org.schemaspy.{ DbAnalyzer, SimpleRuntimeDotConfig, LayoutFolder, SchemaAnalyzer }
+import org.schemaspy.model.Database as SchemaspyDatabase
+import org.schemaspy.model.Table as SchemaspyTable
+import org.schemaspy.model.{ TableIndex, ProgressListener, Tracked, Console }
+import org.schemaspy.util.{ Markdown, ManifestUtils, DataTableConfig, DefaultPrintWriter, Jar }
+import org.schemaspy.util.naming.FileNameGenerator
+import org.schemaspy.view.*
+import org.schemaspy.output.dot.schemaspy.{ DefaultFontConfig, DotFormatter, OrphanGraph }
+import org.schemaspy.output.diagram.{ SummaryDiagram, TableDiagram }
+import org.schemaspy.output.diagram.graphviz.GraphvizDot
+import org.schemaspy.output.html.mustache.diagrams.{ MustacheSummaryDiagramFactory, OrphanDiagram, MustacheTableDiagramFactory }
+import org.schemaspy.analyzer.ImpliedConstraintsFinder
+import org.schemaspy.cli.CommandLineArguments
+
+import ldbc.core.*
 import ldbc.schemaspy.result.Status
+import ldbc.schemaspy.builder.{ DbmsMetaBuilder, TableBuilder }
+
+class SchemaSpyGenerator(database: Database):
+
+  private val DOT_HTML = ".html"
+  private val INDEX_DOT_HTML = "index.html"
+
+  private val layoutFolder = new LayoutFolder(this.getClass.getClassLoader)
+  private val builder = new DbmsMetaBuilder(database)
+  private val commandLineArguments = new CommandLineArguments
+  private val progressListener = new Console(commandLineArguments, new Tracked())
+
+  private def writeInfo(key: String, value: String, infoFile: Path): Unit  =
+    try {
+      Files.write(
+        infoFile,
+        (key + "=" + "\n").getBytes(StandardCharsets.UTF_8),
+        StandardOpenOption.CREATE,
+        StandardOpenOption.APPEND,
+        StandardOpenOption.WRITE
+      )
+    } catch
+      case e: IOException =>
+        println(s"Failed to write `$key=$value`, to $infoFile")
+        e.printStackTrace()
+
+  private def notHtml(): FileFilter =
+    val notHtmlFilter = FileFilterUtils.notFileFilter(FileFilterUtils.suffixFileFilter(DOT_HTML))
+    FileFilterUtils.and(notHtmlFilter)
+
+  private def generateHtmlDoc(
+    db: SchemaspyDatabase,
+    outputDirectory: File,
+    progressListener: ProgressListener
+  ): Unit =
+    val tables = db.getTables
+
+    Markdown.registryPage(new ArrayList[SchemaspyTable](tables))
+
+    new Jar(layoutFolder.url(), outputDirectory, notHtml()).copyJarResourceToPath()
+
+    val renderer = new GraphvizDot(commandLineArguments.getGraphVizConfig)
+
+    val htmlInfoFile = outputDirectory.toPath.resolve("info-html.txt")
+    Files.deleteIfExists(htmlInfoFile)
+
+    writeInfo("date", ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssZ")), htmlInfoFile)
+    writeInfo("os", System.getProperty("os.name") + " " + System.getProperty("os.version"), htmlInfoFile)
+    writeInfo("schemaspy-version", ManifestUtils.getImplementationVersion, htmlInfoFile)
+    writeInfo("schemaspy-build", ManifestUtils.getImplementationBuild, htmlInfoFile)
+    writeInfo("renderer", renderer.identifier(), htmlInfoFile)
+    progressListener.graphingSummaryProgressed()
+
+    val impliedConstraintsFinder = new ImpliedConstraintsFinder()
+
+    val impliedConstraints = impliedConstraintsFinder.find(tables)
+
+    val runtimeDotConfig = new SimpleRuntimeDotConfig(
+      new DefaultFontConfig(commandLineArguments.getDotConfig),
+      commandLineArguments.getDotConfig,
+      "svg".equalsIgnoreCase(renderer.format()),
+      true
+    )
+
+    val dotProducer = new DotFormatter(runtimeDotConfig)
+    val diagramDir = new File(outputDirectory, "diagrams")
+    diagramDir.mkdirs()
+    val summaryDir = new File(outputDirectory, "summary")
+    summaryDir.mkdirs()
+    val summaryDiagram = new SummaryDiagram(renderer, summaryDir)
+
+    val mustacheSummaryDiagramFactory = new MustacheSummaryDiagramFactory(dotProducer, summaryDiagram, true, !impliedConstraints.isEmpty, outputDirectory)
+    val results = mustacheSummaryDiagramFactory.generateSummaryDiagrams(db, tables, progressListener)
+
+    val dataTableConfig = new DataTableConfig(commandLineArguments)
+    val mustacheCompiler = new MustacheCompiler(
+      db.getName,
+      db.getSchema.getName,
+      commandLineArguments.getHtmlConfig,
+      true,
+      dataTableConfig
+    )
+
+    val htmlRelationshipsPage = new HtmlRelationshipsPage(mustacheCompiler, true, !impliedConstraints.isEmpty)
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve("relationships.html").toFile)) { writer =>
+      htmlRelationshipsPage.write(results, writer)
+    }
+
+    progressListener.graphingSummaryProgressed()
+
+    val htmlOrphansPage = new HtmlOrphansPage(
+      mustacheCompiler,
+      new OrphanDiagram(
+        new OrphanGraph(runtimeDotConfig, tables),
+        renderer,
+        outputDirectory
+      )
+    )
+
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve("orphans.html").toFile)) { writer =>
+      htmlOrphansPage.write(writer)
+    }
+
+    progressListener.graphingSummaryProgressed()
+
+    val htmlMainIndexPage = new HtmlMainIndexPage(
+      mustacheCompiler,
+      commandLineArguments.getHtmlConfig.getDescription
+    )
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve(INDEX_DOT_HTML).toFile)) { writer =>
+      htmlMainIndexPage.write(db, tables, impliedConstraints, writer)
+    }
+
+    progressListener.graphingSummaryProgressed()
+
+    val constraints = DbAnalyzer.getForeignKeyConstraints(tables)
+
+    val htmlConstraintsPage = new HtmlConstraintsPage(mustacheCompiler)
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve("constraints.html").toFile)) { writer =>
+      htmlConstraintsPage.write(constraints, tables, writer)
+    }
+
+    progressListener.graphingSummaryProgressed()
+
+    val htmlAnomaliesPage = new HtmlAnomaliesPage(mustacheCompiler)
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve("anomalies.html").toFile)) { writer =>
+      htmlAnomaliesPage.write(tables, impliedConstraints, writer)
+    }
+
+    progressListener.graphingSummaryProgressed()
+
+    val htmlColumnsPage = new HtmlColumnsPage(mustacheCompiler)
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve("columns.html").toFile)) { writer =>
+      htmlColumnsPage.write(tables, writer)
+    }
+
+    progressListener.graphingSummaryProgressed()
+
+    val htmlRoutinesPage = new HtmlRoutinesPage(mustacheCompiler)
+    Using(new DefaultPrintWriter(outputDirectory.toPath.resolve("routines.html").toFile)) { writer =>
+      htmlRoutinesPage.write(db.getRoutines, writer)
+    }
+
+    val htmlRoutinePage = new HtmlRoutinePage(mustacheCompiler)
+    db.getRoutines.forEach(routine => {
+      Using(
+        new DefaultPrintWriter(
+          outputDirectory.toPath
+            .resolve("routines")
+            .resolve(new FileNameGenerator(routine.getName).value() + DOT_HTML)
+            .toFile)
+      ) { writer => htmlRoutinePage.write(routine, writer) }
+    })
+
+    val sqlAnalyzer = new SqlAnalyzer(db.getDbmsMeta.getIdentifierQuoteString, db.getDbmsMeta.getAllKeywords, db.getTables, db.getViews)
+
+    val tablesDir = new File(diagramDir, "tables")
+    tablesDir.mkdirs()
+
+    val tableDiagram = new TableDiagram(renderer, tablesDir)
+
+    val mustacheTableDiagramFactory = new MustacheTableDiagramFactory(dotProducer, tableDiagram, outputDirectory, commandLineArguments.getDegreeOfSeparation)
+    val htmlTablePage = new HtmlTablePage(mustacheCompiler, sqlAnalyzer)
+
+    tables.forEach(table => {
+      val mustacheTableDiagrams = mustacheTableDiagramFactory.generateTableDiagrams(table)
+      progressListener.graphingDetailsProgressed(table)
+      Using(
+        new DefaultPrintWriter(
+          outputDirectory.toPath
+            .resolve("tables")
+            .resolve(new FileNameGenerator(table.getName).value() + DOT_HTML).toFile
+        )
+      ) { writer =>
+        htmlTablePage.write(table, mustacheTableDiagrams, writer)
+      }
+    })
+
+  def generateTo(outputDirectory: File): Unit =
+
+    val dbmsMeta = builder.build
+    val db = new SchemaspyDatabase(dbmsMeta, database.name, database.catalog.orNull, database.schema)
+
+    database.tables.map(table => {
+      val builder = TableBuilder(db, table)
+      builder.build
+    })
+
+    generateHtmlDoc(db, outputDirectory, progressListener)
 
 object SchemaSpyGenerator:
 
-  def generate(
-    database:        Database,
-    outputDirectory: File,
-    user:            String,
-    password:        Option[String]
-  ): Status =
-    val sqlService:             SqlService             = new SqlService()
-    val databaseServiceFactory: DatabaseServiceFactory = new DatabaseServiceFactory(sqlService)
-    val outputProducer:         OutputProducer         = new XmlProducerUsingDOM()
-    val layoutFolder:           LayoutFolder           = new LayoutFolder(classOf[SchemaAnalyzer].getClassLoader)
-
-    val arguments: Arguments = new SchemaSpyArguments(
-      nohtml          = false,
-      noImplied       = false,
-      databaseType    = database.databaseType.toString.toLowerCase,
-      databaseName    = database.name,
-      schemaMeta      = database.schemaMeta.orNull,
-      sso             = false,
-      user            = user,
-      schema          = database.schema,
-      catalog         = database.catalog.orNull,
-      outputDirectory = outputDirectory,
-      port            = database.port,
-      _useVizJS       = false
-    )
-
-    val analyzer: SchemaAnalyzer = new SchemaAnalyzer(
-      sqlService,
-      databaseServiceFactory,
-      arguments,
-      outputProducer,
-      layoutFolder
-    )
-
-    val config = ConfigBuilder.build(
-      db       = database.name,
-      host     = database.host,
-      port     = database.port,
-      user     = user,
-      dbType   = Some(database.databaseType.toString.toLowerCase()),
-      password = password
-    )
-
-    try
-      val result = Option(analyzer.analyze(config))
-      if result.nonEmpty then Status.Success else Status.Failure
-    catch
-      case ex: ConnectionFailure =>
-        ex.printStackTrace()
-        Status.ConnectionFailure
-      case ex: EmptySchemaException =>
-        ex.printStackTrace()
-        Status.EmptySchema
-      case ex: InvalidConfigurationException =>
-        ex.printStackTrace()
-        Status.InvalidConfig
-      case ex: SQLException =>
-        ex.printStackTrace()
-        Status.Failure
-      case ex: IOException =>
-        ex.printStackTrace()
-        Status.Failure
+  def apply(database: Database): SchemaSpyGenerator =
+    new SchemaSpyGenerator(database)
