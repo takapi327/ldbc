e659ba792e347f270ab8eec4604eca9092a3f448
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-04-23 13:35:56 +0900

    Fix TableColumnBuilder setDefaultValue

--- module/ldbc-schemaspy/src/main/scala/ldbc/schemaspy/builder/TableColumnBuilder.scala
+++ module/ldbc-schemaspy/src/main/scala/ldbc/schemaspy/builder/TableColumnBuilder.scala
@@ -19,27 +19,28 @@ object TableColumnBuilder:
     // column.setLength(255)
     // column.setDecimalDigits(???)
     column.setNullable(_column.dataType.isOptional)
-    _column match
-      case v: DataType.IntegerType[?]  => v.default foreach column.setDefaultValue
-      case v: DataType.Decimal[?]      => v.default foreach column.setDefaultValue
-      case v: DataType.DecimalOpt[?]   => v.default foreach column.setDefaultValue
-      case v: DataType.CFloat[?]       => v.default foreach column.setDefaultValue
-      case v: DataType.FloatOpt[?]     => v.default foreach column.setDefaultValue
-      case v: DataType.CChar[?]        => v.default foreach column.setDefaultValue
-      case v: DataType.CharOpt[?]      => v.default foreach column.setDefaultValue
-      case v: DataType.Varchar[?]      => v.default foreach column.setDefaultValue
-      case v: DataType.VarcharOpt[?]   => v.default foreach column.setDefaultValue
-      case v: DataType.Date[?]         => v.default foreach column.setDefaultValue
-      case v: DataType.DateOpt[?]      => v.default foreach column.setDefaultValue
-      case v: DataType.DateTime[?]     => v.default foreach column.setDefaultValue
-      case v: DataType.DateTimeOpt[?]  => v.default foreach column.setDefaultValue
-      case v: DataType.TimeStamp[?]    => v.default foreach column.setDefaultValue
-      case v: DataType.TimeStampOpt[?] => v.default foreach column.setDefaultValue
-      case v: DataType.Time[?]         => v.default foreach column.setDefaultValue
-      case v: DataType.TimeOpt[?]      => v.default foreach column.setDefaultValue
-      case v: DataType.Year[?]         => v.default foreach column.setDefaultValue
-      case v: DataType.YearOpt[?]      => v.default foreach column.setDefaultValue
-      case _                           =>
+    _column.dataType match
+      case v: DataType.IntegerType[?]    => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.IntegerOptType[?] => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.Decimal[?]        => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.DecimalOpt[?]     => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.CFloat[?]         => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.FloatOpt[?]       => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.CChar[?]          => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.CharOpt[?]        => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.Varchar[?]        => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.VarcharOpt[?]     => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.Date[?]           => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.DateOpt[?]        => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.DateTime[?]       => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.DateTimeOpt[?]    => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.TimeStamp[?]      => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.TimeStampOpt[?]   => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.Time[?]           => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.TimeOpt[?]        => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.Year[?]           => v.default.map(_.value) foreach column.setDefaultValue
+      case v: DataType.YearOpt[?]        => v.default.map(_.value) foreach column.setDefaultValue
+      case unknown                       => throw new IllegalArgumentException(s"The $unknown in the ${ _column.label } column is not a DataType type.")
 
     _column.comment foreach column.setComments
     column.setId(index)
