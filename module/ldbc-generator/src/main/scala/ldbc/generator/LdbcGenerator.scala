aba4d1b29f39238666740fca613188b41b41306c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-12 02:29:07 +0900

    Create LdbcGenerator

---
+++ module/ldbc-generator/src/main/scala/ldbc/generator/LdbcGenerator.scala
@@ -0,0 +1,77 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.generator
+
+import java.io.File
+import java.nio.file.Files
+import java.nio.charset.Charset
+
+import scala.io.Codec
+
+private[ldbc] object LdbcGenerator:
+
+  def generate(
+    sqlFilePaths:  Array[File],
+    sourceManaged: File,
+    baseDirectory: File
+  ): Array[File] =
+
+    sqlFilePaths.flatMap(file =>
+
+      val content = new String(
+        Files.readAllBytes(file.toPath),
+        Charset.defaultCharset()
+      )
+
+      Parser.parse(content) match
+        case Parser.Success(statements, _) =>
+          statements.map(statement =>
+            val className = s"${statement.tableName.head.toUpper}${statement.tableName.tail}"
+            val properties = statement.columnDefinitions.map(column =>
+              val isNullable = column.attributes.flatMap(_.constraint)
+              val scalaType = if isNullable.getOrElse("NULL") == "NULL" then
+                s"Option[${column.dataType.scalaType}]"
+              else s"${column.dataType.scalaType}"
+              s"${column.name}: $scalaType"
+            )
+            val test = statement.columnDefinitions.count(column =>
+              column.attributes.flatMap(_.constraint).getOrElse("NULL") == "NULL"
+            )
+
+            val outputFile = new File(sourceManaged, s"$className.scala")
+
+            if !outputFile.exists() then
+              outputFile.getParentFile.mkdirs()
+              outputFile.createNewFile()
+
+            val scalaSource =
+              s"""
+                 |import ldbc.core.*
+                 |${if test > 0 then "import ldbc.core.syntax.given" else ""}
+                 |
+                 |case class $className(
+                 |  ${properties.mkString(",\n  ")}
+                 |)
+                 |
+                 |object $className:
+                 |
+                 |  val table: TABLE[$className] = Table[$className]("${statement.tableName}")(
+                 |    ${statement.columnDefinitions.map(_.toCode).mkString(",\n    ")}
+                 |  )
+                 |""".stripMargin
+
+            Files.write(outputFile.toPath, scalaSource.getBytes(summon[Codec].name))
+            outputFile
+          )
+        case Parser.NoSuccess(errorMessage, _) =>
+          println(s"NoSuccess: $errorMessage")
+          List.empty
+        case Parser.Failure(errorMessage, _) =>
+          println(s"Failure: $errorMessage")
+          List.empty
+        case Parser.Error(errorMessage, _) =>
+          println(s"Error: $errorMessage")
+          List.empty
+    )
