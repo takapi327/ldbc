4fcc5dcca83c0e4d1914a8e448a744b398aac17d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-12 02:27:35 +0900

    Create CreateStatement model

---
+++ module/ldbc-generator/src/main/scala/ldbc/generator/model/CreateStatement.scala
@@ -0,0 +1,10 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.generator.model
+
+case class CreateStatement(
+  tableName: String,
+  columnDefinitions: List[ColumnDefinition]
+)
