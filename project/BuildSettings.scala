cd592df485835e0d70745413e13c918b5f1fa535
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-11 19:53:28 +0900

    Add LepusSbtPluginProject

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,15 +6,17 @@
 
 import sbt._
 import sbt.Keys._
+import sbt.plugins.SbtPlugin
 
 import sbtrelease.ReleasePlugin.autoImport._
 import ReleaseTransformations._
+
+import ScalaVersions._
 
 object BuildSettings {
 
   val baseScalaSettings: Seq[String] = Seq(
     "-Xfatal-warnings",
-    "-Wunused:all",
     "-deprecation",
     "-feature",
     "-unchecked",
@@ -23,6 +25,10 @@ object BuildSettings {
     "-language:existentials",
     "-language:higherKinds",
     "-language:implicitConversions"
+  )
+
+  val scala3Settings: Seq[String] = baseScalaSettings ++ Seq(
+    "-Wunused:all",
   )
 
   /**
@@ -49,21 +55,32 @@ object BuildSettings {
 
   /** These settings are used by all projects. */
   def commonSettings: Seq[Setting[_]] = Def.settings(
-    organization  := "com.github.takapi327",
-    startYear     := Some(2023),
-    homepage      := Some(url(s"https://github.com/takapi327/ldbc")),
-    licenses      := Seq("MIT" -> url("https://img.shields.io/badge/license-MIT-green")),
-    Test / fork   := true,
-    run / fork    := true,
-    scalacOptions ++= baseScalaSettings,
-    developers    += Developer("takapi327", "Takahiko Tominaga", "t.takapi0327@gmail.com", url("https://github.com/takapi327"))
+    organization := "com.github.takapi327",
+    startYear    := Some(2023),
+    homepage     := Some(url(s"https://github.com/takapi327/ldbc")),
+    licenses     := Seq("MIT" -> url("https://img.shields.io/badge/license-MIT-green")),
+    Test / fork  := true,
+    run / fork   := true,
+    developers   += Developer("takapi327", "Takahiko Tominaga", "t.takapi0327@gmail.com", url("https://github.com/takapi327"))
   )
 
   /** A project that runs in the sbt runtime. */
   object LepusSbtProject {
     def apply(name: String, dir: String): Project =
       Project(name, file(dir))
+        .settings(scalacOptions ++= scala3Settings)
         .settings(commonSettings: _*)
         .settings(publishSettings: _*)
+  }
+
+  /** A project that is an sbt plugin. */
+  object LepusSbtPluginProject {
+    def apply(name: String, dir: String): Project =
+      Project(name, file(dir))
+        .settings(scalaVersion := scala2)
+        .settings(scalacOptions ++= baseScalaSettings)
+        .settings(commonSettings: _*)
+        .settings(publishSettings: _*)
+        .enablePlugins(SbtPlugin)
   }
 }
