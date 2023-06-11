20a488122e1a80b4a0a090b5f47492ece4345c1e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-11 21:09:14 +0900

    Create AutoImport

---
+++ plugin/src/main/scala/ldbc/sbt/AutoImport.scala
@@ -0,0 +1,15 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package ldbc.sbt
+
+import sbt._
+
+object AutoImport extends Dependencies {
+
+  val sqlFilePaths = SettingKey[List[String]](
+    label       = "sqlFilePaths",
+    description = "Path list of SQL files to read"
+  )
+}
