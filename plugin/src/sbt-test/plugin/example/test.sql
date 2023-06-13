0dcd2eb6c1a8be85f660229510360773063ce648
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-13 20:25:51 +0900

    Fix sbt plugin test

--- plugin/src/sbt-test/plugin/example/test.sql
+++ plugin/src/sbt-test/plugin/example/test.sql
@@ -8,6 +8,13 @@ CREATE /* Comment */ TABLE /* Comment */ country /* Comment */ (
   code3 Bit(64) DEFAULT 1
 );
 
-CREATE TABLE country1 (
-  code2 TINYINT(3) UNSIGNED NULL
+CREATE TABLE `test` (
+  `id` BIGINT(64) NOT NULL AUTO_INCREMENT,
+  `sub_id` BIGINT(64) NOT NULL,
+  `sub_type` BIGINT(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX (`sub_id`),
+  INDEX (`id`),
+  UNIQUE KEY (`id`),
+  CONSTRAINT `fk_id` FOREIGN KEY (`sub_id`, `sub_type`) REFERENCES `sub_test` (`id`, `type`)
 );
