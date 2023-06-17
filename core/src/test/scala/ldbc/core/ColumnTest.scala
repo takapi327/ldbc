81e1c19e92817354d37978dfbcd58bc7977989e1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-18 01:03:58 +0900

    Fix test

--- core/src/test/scala/ldbc/core/ColumnTest.scala
+++ core/src/test/scala/ldbc/core/ColumnTest.scala
@@ -14,52 +14,30 @@ object ColumnTest extends Specification:
 
   "Column Test" should {
     "The query string of the Column model generated with only label and DataType matches the specified string." in {
-
-      val column = Column[Long]("id", Bigint(64))
-
-      column.queryString === "`id` BIGINT(64) NOT NULL"
+      column[Long]("id", BIGINT(64)).queryString === "`id` BIGINT(64) NOT NULL"
     }
 
     "The query string of the Column model generated with only label and DataType and comment matches the specified string." in {
-
-      val column = Column[Long]("id", Bigint(64), "identifier")
-
-      column.queryString === "`id` BIGINT(64) NOT NULL COMMENT 'identifier'"
+      column[Long]("id", BIGINT(64), "identifier").queryString === "`id` BIGINT(64) NOT NULL COMMENT 'identifier'"
     }
 
     "The query string of the Column model generated with only label and DataType and attributes matches the specified string." in {
-
-      val column = Column[Long]("id", Bigint(64), AutoInc[Long]())
-
-      column.queryString === "`id` BIGINT(64) NOT NULL AUTO_INCREMENT"
+      column[Long]("id", BIGINT(64), AutoInc[Long]()).queryString === "`id` BIGINT(64) NOT NULL AUTO_INCREMENT"
     }
 
     "The query string of the Column model generated with only label and DataType and attributes and comment matches the specified string." in {
-
-      val column = Column[Long]("id", Bigint(64), "identifier", AutoInc[Long]())
-
-      column.queryString === "`id` BIGINT(64) NOT NULL AUTO_INCREMENT COMMENT 'identifier'"
+      column[Long]("id", BIGINT(64), "identifier", AutoInc[Long]()).queryString === "`id` BIGINT(64) NOT NULL AUTO_INCREMENT COMMENT 'identifier'"
     }
 
     "The query string of the Column model generated with only label and DataType matches the specified string." in {
-
-      val column = Column[String]("name", Varchar(255, None, Some(Character("ascii"))))
-
-      column.queryString === "`name` VARCHAR(255) CHARACTER SET ascii NOT NULL"
+      column[String]("name", VARCHAR(255).CHARACTER_SET(Character("ascii"))).queryString === "`name` VARCHAR(255) CHARACTER SET ascii NOT NULL"
     }
 
     "The query string of the Column model generated with only label and DataType and comment matches the specified string." in {
-
-      val column = Column[String]("name", Varchar(255, None, Some(Character("ascii"))), "name")
-
-      column.queryString === "`name` VARCHAR(255) CHARACTER SET ascii NOT NULL COMMENT 'name'"
+      column[String]("name", VARCHAR(255).CHARACTER_SET(Character("ascii")), "name").queryString === "`name` VARCHAR(255) CHARACTER SET ascii NOT NULL COMMENT 'name'"
     }
 
     "The query string of the Column model generated with only label and DataType and comment matches the specified string." in {
-
-      val column =
-        Column[String]("name", Varchar(255, None, None).CHARACTER_SET(Character("ascii").set("ascii_bin")), "name")
-
-      column.queryString === "`name` VARCHAR(255) CHARACTER SET ascii COLLATE ascii_bin NOT NULL COMMENT 'name'"
+      column[String]("name", VARCHAR(255).CHARACTER_SET(Character("ascii").set("ascii_bin")), "name").queryString === "`name` VARCHAR(255) CHARACTER SET ascii COLLATE ascii_bin NOT NULL COMMENT 'name'"
     }
   }
