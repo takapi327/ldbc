b3ea5ba279da44bc986a43879498a72a0986d893
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-17 15:56:47 +0900

    Changed DataType implementation

--- module/ldbc-generator/src/main/scala/ldbc/generator/model/DataType.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/model/DataType.scala
@@ -7,9 +7,11 @@ package ldbc.generator.model
 import ldbc.core.JdbcType
 
 enum ScalaType:
-  case Byte, String, Short, Int, Long, BigDecimal, Float
+  case Byte, String, Short, Int, Long, BigDecimal, Float, BigInt
 
 trait DataType:
+
+  val name: String
 
   val jdbcType: JdbcType
 
@@ -17,64 +19,72 @@ trait DataType:
 
 object DataType:
 
-  case class Bit(length: Int) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.Bit
+  def BIT(length: Int): DataType = new DataType:
+    override val name: String = "BIT"
+    override val jdbcType: JdbcType = JdbcType.Bit
     override val scalaType: ScalaType = ScalaType.Byte
 
-    override def toString: String = s"BIT[$scalaType]($length)"
+    override def toString: String = s"$name[$scalaType]($length)"
 
-  case class Tinyint(length: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.TinyInt
-    override val scalaType: ScalaType = ScalaType.Byte
-
-    override def toString: String =
-      if unsigned then s"TINYINT[$scalaType]($length).UNSIGNED"
-      else s"TINYINT[$scalaType]($length)"
-
-  case class Smallint(length: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.SmallInt
-    override val scalaType: ScalaType = ScalaType.Short
+  def TINYINT(length: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "TINYINT"
+    override val jdbcType: JdbcType = JdbcType.TinyInt
+    override val scalaType: ScalaType = if unsigned then ScalaType.Short else ScalaType.Byte
 
     override def toString: String =
-      if unsigned then s"SMALLINT[$scalaType]($length).UNSIGNED"
-      else s"SMALLINT[$scalaType]($length)"
+      if unsigned then s"$name[$scalaType]($length).UNSIGNED"
+      else s"$name[$scalaType]($length)"
 
-  case class Mediumint(length: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.Integer
+  def SMALLINT(length: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "SMALLINT"
+    override val jdbcType: JdbcType = JdbcType.SmallInt
+    override val scalaType: ScalaType = if unsigned then ScalaType.Int else ScalaType.Short
+
+    override def toString: String =
+      if unsigned then s"$name[$scalaType]($length).UNSIGNED"
+      else s"$name[$scalaType]($length)"
+
+  def MEDIUMINT(length: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "MEDIUMINT"
+    override val jdbcType: JdbcType = JdbcType.Integer
     override val scalaType: ScalaType = ScalaType.Int
 
     override def toString: String =
-      if unsigned then s"MEDIUMINT[$scalaType]($length).UNSIGNED"
-      else s"MEDIUMINT[$scalaType]($length)"
+      if unsigned then s"$name[$scalaType]($length).UNSIGNED"
+      else s"$name[$scalaType]($length)"
 
-  case class Integer(length: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.Integer
-    override val scalaType: ScalaType = ScalaType.Int
-
-    override def toString: String =
-      if unsigned then s"INT[$scalaType]($length).UNSIGNED"
-      else s"INT[$scalaType]($length)"
-
-  case class BigInt(length: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.BigInt
-    override val scalaType: ScalaType = ScalaType.Long
+  def INT(length: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "INT"
+    override val jdbcType: JdbcType = JdbcType.Integer
+    override val scalaType: ScalaType = if unsigned then ScalaType.Long else ScalaType.Int
 
     override def toString: String =
-      if unsigned then s"BIGINT[$scalaType]($length).UNSIGNED"
-      else s"BIGINT[$scalaType]($length)"
+      if unsigned then s"$name[$scalaType]($length).UNSIGNED"
+      else s"$name[$scalaType]($length)"
 
-  case class Decimal(accuracy: Int, scale: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.Decimal
+  def BIGINT(length: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "BIGINT"
+    override val jdbcType: JdbcType = JdbcType.BigInt
+    override val scalaType: ScalaType = if unsigned then ScalaType.BigInt else ScalaType.Long
+
+    override def toString: String =
+      if unsigned then s"$name[$scalaType]($length).UNSIGNED"
+      else s"$name[$scalaType]($length)"
+
+  def DECIMAL(accuracy: Int, scale: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "DECIMAL"
+    override val jdbcType: JdbcType = JdbcType.Decimal
     override val scalaType: ScalaType = ScalaType.BigDecimal
 
     override def toString: String =
-      if unsigned then s"DECIMAL[$scalaType]($accuracy, $scale).UNSIGNED"
-      else s"DECIMAL[$scalaType]($accuracy, $scale)"
+      if unsigned then s"$name[$scalaType]($accuracy, $scale).UNSIGNED"
+      else s"$name[$scalaType]($accuracy, $scale)"
 
-  case class CFloat(accuracy: Int, unsigned: Boolean, zerofill: Boolean) extends DataType:
-    override val jdbcType:  JdbcType  = JdbcType.Float
+  def FLOAT(accuracy: Int, unsigned: Boolean, zerofill: Boolean): DataType = new DataType:
+    override val name: String = "FLOAT"
+    override val jdbcType: JdbcType = JdbcType.Float
     override val scalaType: ScalaType = ScalaType.Float
 
     override def toString: String =
-      if unsigned then s"FLOAT[$scalaType]($accuracy).UNSIGNED"
-      else s"FLOAT[$scalaType]($accuracy)"
+      if unsigned then s"$name[$scalaType]($accuracy).UNSIGNED"
+      else s"$name[$scalaType]($accuracy)"
