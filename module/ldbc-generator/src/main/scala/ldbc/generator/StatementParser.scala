d0d4754d2546c0f5917aee3e2f35d588d2bb3ed2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-20 01:57:40 +0900

    Fixing StatementParser

--- module/ldbc-generator/src/main/scala/ldbc/generator/StatementParser.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/StatementParser.scala
@@ -8,10 +8,11 @@ import ldbc.generator.model.*
 
 trait StatementParser extends KeyParser:
 
-  private def create:      Parser[String] = "CREATE" ^^ (_.toUpperCase)
-  private def temporary:   Parser[String] = "TEMPORARY" ^^ (_.toUpperCase)
-  private def table:       Parser[String] = "TABLE" ^^ (_.toUpperCase)
-  private def ifNotExists: Parser[String] = "IF NOT EXISTS" ^^ (_.toUpperCase)
+  private def create:    Parser[String] = caseSensitivity("create") ^^ (_.toUpperCase)
+  private def temporary: Parser[String] = caseSensitivity("temporary") ^^ (_.toUpperCase)
+  private def table:     Parser[String] = caseSensitivity("table") ^^ (_.toUpperCase)
+  private def ifNotExists: Parser[String] =
+    caseSensitivity("if") ~> caseSensitivity("not") ~> caseSensitivity("exists") ^^ (_.toUpperCase)
 
   protected def createStatement: Parser[CreateStatement] =
     create ~> opt(comment) ~> opt(temporary) ~> opt(comment) ~> table ~>
