03364404d23f567029ee9684bcff19bf0f88b0f6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-21 00:07:48 +0900

    Create DatabaseStatement

---
+++ module/ldbc-generator/src/main/scala/ldbc/generator/model/DatabaseStatement.scala
@@ -0,0 +1,12 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.generator.model
+
+case class DatabaseStatement(
+  name:    String,
+  charset: Option[String],
+  collate: Option[String],
+  encryption: Option[String]
+)
