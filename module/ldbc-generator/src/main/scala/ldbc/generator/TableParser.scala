c659c7880e6a47f4a6731a0c66d5e6ebee7eb848
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-26 01:26:24 +0900

    Add dropStatement and tableStatements parser

--- module/ldbc-generator/src/main/scala/ldbc/generator/TableParser.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/TableParser.scala
@@ -11,12 +11,19 @@ trait TableParser extends KeyParser:
   private def temporary: Parser[String] = caseSensitivity("temporary") ^^ (_.toUpperCase)
   private def table:     Parser[String] = caseSensitivity("table") ^^ (_.toUpperCase)
 
-  protected def createStatement: Parser[CreateStatement] =
+  protected def tableStatements: Parser[Table.CreateStatement | Table.DropStatement] = createStatement | dropStatement
+
+  private def createStatement: Parser[Table.CreateStatement] =
     opt(comment) ~> create ~> opt(comment) ~> opt(temporary) ~> opt(comment) ~> table ~>
       opt(comment) ~> opt(ifNotExists) ~> opt(comment) ~> sqlIdent ~ opt(comment) ~
       "(" ~ repsep(columnDefinition | keyDefinitions, ",") ~ opt(comment) <~ ")" ~ ";" ^^ {
         case tableName ~ _ ~ _ ~ objects ~ _ =>
           val columnDefs = objects.filter(_.isInstanceOf[ColumnDefinition]).asInstanceOf[List[ColumnDefinition]]
           val keyDefs    = objects.filter(_.isInstanceOf[Key]).asInstanceOf[List[Key]]
-          CreateStatement(tableName, columnDefs, keyDefs)
+          Table.CreateStatement(tableName, columnDefs, keyDefs)
       }
+
+  private def dropStatement: Parser[Table.DropStatement] =
+    opt(comment) ~> drop ~> opt(comment) ~> opt(ifNotExists) ~> opt(comment) ~> sqlIdent <~ opt(comment) <~ ";" ^^ {
+      tableName => Table.DropStatement(tableName)
+    }
