8b35be3f8c030dd66d88de952c08dfbb7623a991
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-26 22:34:52 +0900

    Action sbt scalafmtAll

--- module/ldbc-generator/src/main/scala/ldbc/generator/model/Table.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/model/Table.scala
@@ -5,38 +5,39 @@
 package ldbc.generator.model
 
 object Table:
-  
+
   trait Options
 
   object Options:
 
-    case class AutoExtendSize(value: String) extends Options
-    case class AutoIncrement(value: Int) extends Options
-    case class AVGRowLength(value: String) extends Options
-    case class Character(value: String) extends Options
-    case class CheckSum(value: 0 | 1) extends Options
-    case class Collate(value: String) extends Options
-    case class Comment(value: String) extends Options
-    case class Compression(value: "ZLIB" | "LZ4" | "NONE") extends Options
-    case class Connection(value: String) extends Options
-    case class Directory(value: String) extends Options
-    case class DelayKeyWrite(value: 0 | 1) extends Options
-    case class Encryption(value: "Y" | "N") extends Options
-    case class Engine(value: String) extends Options
-    case class EngineAttribute(value: String) extends Options
+    case class AutoExtendSize(value: String)                extends Options
+    case class AutoIncrement(value: Int)                    extends Options
+    case class AVGRowLength(value: String)                  extends Options
+    case class Character(value: String)                     extends Options
+    case class CheckSum(value: 0 | 1)                       extends Options
+    case class Collate(value: String)                       extends Options
+    case class Comment(value: String)                       extends Options
+    case class Compression(value: "ZLIB" | "LZ4" | "NONE")  extends Options
+    case class Connection(value: String)                    extends Options
+    case class Directory(value: String)                     extends Options
+    case class DelayKeyWrite(value: 0 | 1)                  extends Options
+    case class Encryption(value: "Y" | "N")                 extends Options
+    case class Engine(value: String)                        extends Options
+    case class EngineAttribute(value: String)               extends Options
     case class InsertMethod(value: "NO" | "FIRST" | "LAST") extends Options
-    case class KeyBlockSize(value: String) extends Options
-    case class MaxRows(value: Int) extends Options
-    case class MinRows(value: Int) extends Options
-    case class PackKeys(value: "0" | "1" | "DEFAULT") extends Options
-    case class Password(value: String) extends Options
-    case class RowFormat(value: "DEFAULT" | "DYNAMIC" | "FIXED" | "COMPRESSED" | "REDUNDANT" | "COMPACT") extends Options
-    case class SecondaryEngineAttribute(value: String) extends Options
-    case class StatsAutoRecalc(value: "0" | "1" | "DEFAULT") extends Options
-    case class StatsPersistent(value: "0" | "1" | "DEFAULT") extends Options
-    case class StatsSamplePages(value: String) extends Options
+    case class KeyBlockSize(value: String)                  extends Options
+    case class MaxRows(value: Int)                          extends Options
+    case class MinRows(value: Int)                          extends Options
+    case class PackKeys(value: "0" | "1" | "DEFAULT")       extends Options
+    case class Password(value: String)                      extends Options
+    case class RowFormat(value: "DEFAULT" | "DYNAMIC" | "FIXED" | "COMPRESSED" | "REDUNDANT" | "COMPACT")
+      extends Options
+    case class SecondaryEngineAttribute(value: String)                      extends Options
+    case class StatsAutoRecalc(value: "0" | "1" | "DEFAULT")                extends Options
+    case class StatsPersistent(value: "0" | "1" | "DEFAULT")                extends Options
+    case class StatsSamplePages(value: String)                              extends Options
     case class Tablespace(name: String, storage: Option["DISK" | "MEMORY"]) extends Options
-    case class Union(tableNames: List[String]) extends Options
+    case class Union(tableNames: List[String])                              extends Options
 
   case class CreateStatement(
     tableName:         String,
