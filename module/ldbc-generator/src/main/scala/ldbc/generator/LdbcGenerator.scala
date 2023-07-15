7b4b95ffdb77163153308d8c9aafcfac6c2f10f6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-15 19:39:21 +0900

    Create TableModelGenerator

--- module/ldbc-generator/src/main/scala/ldbc/generator/LdbcGenerator.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/LdbcGenerator.scala
@@ -8,11 +8,8 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.charset.Charset
 
-import scala.io.Codec
-
 import ldbc.generator.formatter.Naming
 import ldbc.generator.parser.Parser
-import ldbc.generator.model.*
 
 private[ldbc] object LdbcGenerator:
 
@@ -37,60 +34,7 @@ private[ldbc] object LdbcGenerator:
         case Parser.Success(parsed, _) =>
           parsed.flatMap { (name, statements) =>
             statements.map(statement =>
-              val className = classNameFormatter.format(statement.tableName)
-              val properties = statement.columnDefinitions.map(column =>
-                propertyGenerator(className, column, propertyNameFormatter, classNameFormatter)
-              )
-
-              val objects =
-                statement.columnDefinitions.map(column => enumGenerator(column, classNameFormatter)).filter(_.nonEmpty)
-
-              val directory = sourceManaged.toPath.resolve(name)
-              val output = if !directory.toFile.exists() then
-                directory.toFile.getParentFile.mkdirs()
-                Files.createDirectory(directory)
-              else directory
-
-              val outputFile = new File(output.toFile, s"$className.scala")
-
-              if !outputFile.exists() then
-                outputFile.getParentFile.mkdirs()
-                outputFile.createNewFile()
-
-              val keyDefinitions = statement.keyDefinitions.map(key =>
-                s".keySet(table => ${ key.toCode("table", classNameFormatter, propertyNameFormatter) })"
-              )
-
-              val packageName = if name.nonEmpty then s"ldbc.generated.$name" else "ldbc.generated"
-
-              val columns =
-                statement.columnDefinitions.map((column: ColumnDefinition) =>
-                  column.dataType.scalaType match
-                    case ScalaType.Enum(types) => column.copy(name = classNameFormatter.format(column.name))
-                    case _                     => column
-                )
-
-              val scalaSource =
-                s"""
-                 |package $packageName
-                 |
-                 |import ldbc.core.*
-                 |
-                 |case class $className(
-                 |  ${ properties.mkString(",\n  ") }
-                 |)
-                 |
-                 |object $className:
-                 |
-                 |  ${ objects.mkString("\n  ") }
-                 |  val table: TABLE[$className] = Table[$className]("${ statement.tableName }")(
-                 |    ${ columns.map(_.toCode).mkString(",\n    ") }
-                 |  )
-                 |  ${ keyDefinitions.mkString("\n  ") }
-                 |""".stripMargin
-
-              Files.write(outputFile.toPath, scalaSource.getBytes(summon[Codec].name))
-              outputFile
+              TableModelGenerator.generate(name, statement, classNameFormatter, propertyNameFormatter, sourceManaged)
             )
           }
         case Parser.NoSuccess(errorMessage, _) =>
@@ -103,28 +47,3 @@ private[ldbc] object LdbcGenerator:
           println(s"Error: $errorMessage")
           List.empty
     )
-
-  private def propertyGenerator(
-    className:             String,
-    column:                ColumnDefinition,
-    propertyNameFormatter: Naming,
-    classNameFormatter:    Naming
-  ): String =
-
-    val name = propertyNameFormatter.format(column.name)
-
-    (column.attributes.forall(_.constraint), column.dataType.scalaType) match
-      case (true, _: ScalaType.Enum)  => s"$name: Option[$className.${ classNameFormatter.format(column.name) }]"
-      case (false, _: ScalaType.Enum) => s"$name: $className.${ classNameFormatter.format(column.name) }"
-      case (true, _)                  => s"$name: Option[${ column.dataType.scalaType.code }]"
-      case (false, _)                 => s"$name: ${ column.dataType.scalaType.code }"
-
-  private def enumGenerator(column: ColumnDefinition, formatter: Naming): String =
-    column.dataType.scalaType match
-      case ScalaType.Enum(types) =>
-        val enumName = formatter.format(column.name)
-        s"""enum $enumName extends ldbc.core.model.Enum:
-           |    case ${ types.mkString(", ") }
-           |  object $enumName extends ldbc.core.model.EnumDataType[$enumName]
-           |""".stripMargin
-      case _ => ""
