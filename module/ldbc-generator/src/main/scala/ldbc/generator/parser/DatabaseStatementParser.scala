7aed149e1e6df837fab23d2cfcf032846a209d1f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-17 21:50:02 +0900

    Fix DatabaseStatementParser

--- module/ldbc-generator/src/main/scala/ldbc/generator/parser/DatabaseStatementParser.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/parser/DatabaseStatementParser.scala
@@ -35,15 +35,7 @@ trait DatabaseStatementParser extends TableParser:
         opt(comment) ~> opt(ifNotExists) ~> opt(comment) ~> sqlIdent <~ ";" ^^ { name =>
           Database.DropStatement(name)
         },
-      input => s"""
-        |======================================================
-        |There is an error in the if drop database statement format.
-        |Please correct the format according to the following.
-        |
-        |${ input.pos.longString } ($fileName:${ input.pos.line }:${ input.pos.column })
-        |example: DROP {DATABASE | SCHEMA} [IF EXISTS] `database_name`
-        |======================================================
-        |""".stripMargin
+      failureMessage("drop database statement", "DROP {DATABASE | SCHEMA} [IF EXISTS] `database_name`")
     )
 
   /** Parser for parsing Database use statement.
@@ -53,15 +45,7 @@ trait DatabaseStatementParser extends TableParser:
       opt(comment) ~> caseSensitivity("use") ~> opt(comment) ~> sqlIdent <~ opt(comment) <~ ";" ^^ { name =>
         Database.DropStatement(name)
       },
-      input => s"""
-        |======================================================
-        |There is an error in the if use database statement format.
-        |Please correct the format according to the following.
-        |
-        |${ input.pos.longString } ($fileName:${ input.pos.line }:${ input.pos.column })
-        |example: USE `database_name`
-        |======================================================
-        |""".stripMargin
+      failureMessage("use database statement", "USE `database_name`")
     )
 
   protected def databaseStatements: Parser[Database.CreateStatement | Database.DropStatement | Database.UseStatement] =
