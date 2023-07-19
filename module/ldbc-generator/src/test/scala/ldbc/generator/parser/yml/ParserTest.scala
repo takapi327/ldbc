24a9aafc30e8b779b0c10bd7d38214d181208e75
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-19 20:35:58 +0900

    Create yml ParserTest

---
+++ module/ldbc-generator/src/test/scala/ldbc/generator/parser/yml/ParserTest.scala
@@ -0,0 +1,102 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.generator.parser.yml
+
+import org.specs2.mutable.Specification
+
+object ParserTest extends Specification:
+
+  "Testing the Custom" should {
+
+    "The model generated from the Yaml string matches the specified model." in {
+      val parser = Parser(
+        Parser.Database(
+          "test",
+          Seq(
+            Parser.Table(
+              "test",
+              Some(Seq(Parser.Column("id", "Long"))),
+              Some(Parser.Extend(Seq("ldbc.custom.extend"))),
+              Some(Parser.Extend(Seq("ldbc.custom.extend")))
+            )
+          )
+        )
+      )
+      val parsed = Parser.parse(
+        """
+          |database:
+          |  name: 'test'
+          |  tables:
+          |    - name: 'test'
+          |      columns:
+          |        - name: 'id'
+          |          type: 'Long'
+          |      object:
+          |        extends:
+          |          - ldbc.custom.extend
+          |      class:
+          |        extends:
+          |          - ldbc.custom.extend
+          |""".stripMargin)
+      parsed === parser
+    }
+
+    "The model generated from the Yaml string matches the specified model." in {
+      val parser = Parser(
+        Parser.Database(
+          "test",
+          Seq(
+            Parser.Table(
+              "test",
+              Some(Seq(Parser.Column("id", "Long"))),
+              None,
+              None
+            )
+          )
+        )
+      )
+      val parsed = Parser.parse(
+        """
+          |database:
+          |  name: 'test'
+          |  tables:
+          |    - name: 'test'
+          |      columns:
+          |        - name: 'id'
+          |          type: 'Long'
+          |""".stripMargin)
+      parsed === parser
+    }
+
+    "The model generated from the Yaml string matches the specified model." in {
+      val parser = Parser(
+        Parser.Database(
+          "test",
+          Seq(
+            Parser.Table(
+              "test",
+              None,
+              Some(Parser.Extend(Seq("ldbc.custom.extend"))),
+              Some(Parser.Extend(Seq("ldbc.custom.extend")))
+            )
+          )
+        )
+      )
+      val parsed = Parser.parse(
+        """
+          |database:
+          |  name: 'test'
+          |  tables:
+          |    - name: 'test'
+          |      object:
+          |        extends:
+          |          - ldbc.custom.extend
+          |      class:
+          |        extends:
+          |          - ldbc.custom.extend
+          |""".stripMargin)
+      parsed === parser
+    }
+  }
