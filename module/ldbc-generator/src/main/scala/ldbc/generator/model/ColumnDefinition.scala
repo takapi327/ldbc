d820b844d94990f8713d54f22e860a2b44fb7f32
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-23 01:24:28 +0900

    Change in Attribute definition model

--- module/ldbc-generator/src/main/scala/ldbc/generator/model/ColumnDefinition.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/model/ColumnDefinition.scala
@@ -4,26 +4,21 @@
 
 package ldbc.generator.model
 
-case class Attributes(
-  constraint:               Boolean,
-  default:                  Option[Default],
-  visible:                  Option[String],
-  key:                      Option[List[String]],
-  comment:                  Option[Comment],
-  collate:                  Option[String],
-  columnFormat:             Option[String],
-  engineAttribute:          Option[Key.EngineAttribute],
-  secondaryEngineAttribute: Option[Key.SecondaryEngineAttribute],
-  storage:                  Option[String]
-)
-
 case class ColumnDefinition(
   name:       String,
   dataType:   DataType,
-  attributes: Option[Attributes]
+  attributes: Option[List[ColumnDefinition.Attribute | CommentSet | Key.EngineAttribute | Key.SecondaryEngineAttribute | Comment]]
 ):
 
-  val isOptional = attributes.forall(_.constraint)
+  val isOptional: Boolean = attributes.fold(true)(_.map {
+    case v: ColumnDefinition.Attribute.Condition => v.bool
+    case _ => true
+  }.headOption.getOrElse(true))
+
+  private val defaultAttribute: Option[ColumnDefinition.Attribute.Default] = attributes.fold(None)(_.flatMap {
+    case v: ColumnDefinition.Attribute.Default => Some(v)
+    case _ => None
+  }.headOption)
 
   private val scalaType =
     dataType.scalaType match
@@ -34,23 +29,28 @@ case class ColumnDefinition(
     attributes.fold("")(attribute =>
       dataType.scalaType match
         case ScalaType.Enum(types) =>
-          attribute.default
+          defaultAttribute
             .map {
-              case Default.Value(value) =>
-                if attribute.constraint then s".DEFAULT(Some($name.$value))"
+              case ColumnDefinition.Attribute.Default.Value(value) =>
+                if isOptional then s".DEFAULT(Some($name.$value))"
                 else s".DEFAULT($name.$value)"
-              case default => default.toCode(attribute.constraint)
+              case default => default.toCode(isOptional)
             }
             .getOrElse("")
-        case _ => attribute.default.map(_.toCode(attribute.constraint)).getOrElse("")
+        case _ => defaultAttribute.map(_.toCode(isOptional)).getOrElse("")
     )
 
-  private val _attributes = attributes.fold("")(attribute =>
-    val attributes = Seq(
-      attribute.comment.map(comment => s"\"${ comment.message }\""),
-      attribute.key.flatMap(keys => if keys.nonEmpty then Some(s"${ keys.mkString(", ") }") else None)
-    ).flatten
-    if attributes.nonEmpty then ", " + attributes.mkString(", ") else ""
+  private val _attributes = attributes.fold("")(attributes =>
+    /** Only comments need to be brought to the top because the definition method is different. */
+    val comment = attributes.flatMap {
+      case comment: CommentSet => Some(s"\"${comment.message}\"")
+      case _ => None
+    }
+    val result = comment ++ attributes.flatMap {
+      case key: ColumnDefinition.Attribute.Key => Some(s"${ key.kind }")
+      case _ => None
+    }
+    if result.nonEmpty then ", " + result.mkString(", ") else ""
   )
 
   def toCode: String =
@@ -64,3 +64,60 @@ case class ColumnDefinition(
     val `type` =
       if isOptional then s"Option[${ dataType.getTypeMatches(customType) }]" else dataType.getTypeMatches(customType)
     s"column(\"$name\", ${ dataType.toCode(`type`) }" + default + _attributes + ")"
+
+object ColumnDefinition:
+
+  trait Attribute
+
+  object Attribute:
+
+    case class Condition(bool: Boolean) extends Attribute
+
+    /** Trait for setting SQL Default values
+     */
+    trait Default extends Attribute:
+
+      def toCode(isOptional: Boolean): String
+
+    object Default:
+
+      /** Model to be used when a value matching the DataType type is set.
+       *
+       * @param value
+       * Value set as the default value for DataType
+       */
+      case class Value(value: String | Int) extends Default:
+        private val str = value match
+          case v: String => s"\"$v\""
+          case v: Int => s"$v"
+
+        override def toCode(isOptional: Boolean): String =
+          if isOptional then s".DEFAULT(Some($str))"
+          else s".DEFAULT($str)"
+
+      /** Object for setting NULL as the Default value when the SQL DataType is NULL-allowed.
+       */
+      object Null extends Default:
+        override def toCode(isOptional: Boolean): String =
+          if isOptional then ".DEFAULT(None)"
+          else throw new IllegalArgumentException("NULL cannot be set as the default value for non-null-allowed columns.")
+
+      /** Model for setting TimeStamp-specific Default values.
+       *
+       * @param onUpdate
+       * Value to determine whether to set additional information
+       */
+      case class CurrentTimestamp(onUpdate: Boolean) extends Default:
+        override def toCode(isOptional: Boolean): String =
+          if onUpdate then ".DEFAULT_CURRENT_TIMESTAMP(true)"
+          else ".DEFAULT_CURRENT_TIMESTAMP(false)"
+
+    case class Visible(kind: String) extends Attribute
+
+    case class Key(kind: String) extends Attribute
+
+    case class Collate(set: String) extends Attribute
+
+    case class ColumnFormat(format: String) extends Attribute
+
+    case class Storage(kind: String) extends Attribute
