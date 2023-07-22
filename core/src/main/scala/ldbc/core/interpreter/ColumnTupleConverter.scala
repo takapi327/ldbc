0ca4b0c9d5d64bc96d077349fc86ac32df105dbb
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-22 20:32:38 +0900

    Delete unused

--- core/src/main/scala/ldbc/core/interpreter/ColumnTupleConverter.scala
+++ core/src/main/scala/ldbc/core/interpreter/ColumnTupleConverter.scala
@@ -24,11 +24,8 @@ object ColumnTupleConverter:
 
   given [T1, T2, F[_]]: ColumnTupleConverter[(T1, T2), F] = identity
 
-  given [T1, T2, TN <: NonEmptyTuple, F[_]](using
-    converter: ColumnTupleConverter[T2 *: TN, F]
-  ): ColumnTupleConverter[T1 *: T2 *: TN, F] =
-    (columns: F[T1] *: ColumnTuples[T2 *: TN, F]) =>
-      columns.head *: converter(columns.tail).asInstanceOf[Tuple.Map[T2 *: TN, F]]
+  given [T1, T2, TN <: NonEmptyTuple, F[_]](using converter: ColumnTupleConverter[T2 *: TN, F]): ColumnTupleConverter[T1 *: T2 *: TN, F] =
+    (columns: F[T1] *: ColumnTuples[T2 *: TN, F]) => columns.head *: converter(columns.tail)
 
   /** Method for converting Column Tuple to Tuple Map.
     *
@@ -41,6 +38,5 @@ object ColumnTupleConverter:
     * @tparam F
     *   The effect type
     */
-  def convert[Types <: Tuple, F[_]](using
-    converter:    ColumnTupleConverter[Types, F]
-  )(columnTuples: ColumnTuples[Types, F]): Tuple.Map[Types, F] = converter(columnTuples)
+  def convert[Types <: Tuple, F[_]](columnTuples: ColumnTuples[Types, F])(using converter: ColumnTupleConverter[Types, F]): Tuple.Map[Types, F] =
+    converter(columnTuples)
