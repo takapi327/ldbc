69425bddffbd6c4e2b720dcd62b9d41a8f4b4239
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-22 20:35:59 +0900

    Add scalaTypes property

--- module/ldbc-generator/src/main/scala/ldbc/generator/model/DataType.scala
+++ module/ldbc-generator/src/main/scala/ldbc/generator/model/DataType.scala
@@ -14,12 +14,17 @@ enum ScalaType(val code: String):
   case Long                      extends ScalaType("Long")
   case BigDecimal                extends ScalaType("BigDecimal")
   case Float                     extends ScalaType("Float")
+  case Double                    extends ScalaType("Double")
   case BigInt                    extends ScalaType("BigInt")
   case ArrayByte                 extends ScalaType("Array[Byte]")
+  case Instant                   extends ScalaType("java.time.Instant")
   case LocalDate                 extends ScalaType("java.time.LocalDate")
   case LocalDateTime             extends ScalaType("java.time.LocalDateTime")
   case LocalTime                 extends ScalaType("java.time.LocalTime")
-  case YEAR                      extends ScalaType("java.time.Year")
+  case OffsetTime                extends ScalaType("java.time.OffsetTime")
+  case OffsetDateTime            extends ScalaType("java.time.OffsetDateTime")
+  case ZonedDateTime             extends ScalaType("java.time.ZonedDateTime")
+  case Year                      extends ScalaType("java.time.Year")
   case Enum(types: List[String]) extends ScalaType("Enum")
 
 trait DataType:
@@ -29,6 +34,11 @@ trait DataType:
   val jdbcType: JdbcType
 
   val scalaType: ScalaType
+
+  val scalaTypes: Seq[ScalaType]
+
+  def getTypeMatches(custom: String): String =
+    scalaTypes.find(_.toString == custom).getOrElse(scalaType).code
 
   def propertyType(isOptional: Boolean): String =
     if isOptional then s"Option[${ scalaType.code }]" else scalaType.code
@@ -41,6 +51,8 @@ object DataType:
     override val name:      String    = "BIT"
     override val jdbcType:  JdbcType  = JdbcType.Bit
     override val scalaType: ScalaType = ScalaType.Byte
+    override val scalaTypes: Seq[ScalaType] =
+      Seq(ScalaType.Short, ScalaType.Int, ScalaType.Long)
 
     override def toCode(typeParam: String): String = length.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
 
@@ -48,6 +60,7 @@ object DataType:
     override val name:      String    = "TINYINT"
     override val jdbcType:  JdbcType  = JdbcType.TinyInt
     override val scalaType: ScalaType = if unsigned then ScalaType.Short else ScalaType.Byte
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Byte, ScalaType.Short)
 
     override def toCode(typeParam: String): String =
       val dataType = length.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
@@ -61,6 +74,7 @@ object DataType:
     override val name:      String    = "SMALLINT"
     override val jdbcType:  JdbcType  = JdbcType.SmallInt
     override val scalaType: ScalaType = if unsigned then ScalaType.Int else ScalaType.Short
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Short, ScalaType.Int)
 
     override def toCode(typeParam: String): String =
       val dataType = length.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
@@ -74,6 +88,7 @@ object DataType:
     override val name:      String    = "MEDIUMINT"
     override val jdbcType:  JdbcType  = JdbcType.Integer
     override val scalaType: ScalaType = ScalaType.Int
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       val dataType = length.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
@@ -87,6 +102,7 @@ object DataType:
     override val name:      String    = "INT"
     override val jdbcType:  JdbcType  = JdbcType.Integer
     override val scalaType: ScalaType = if unsigned then ScalaType.Long else ScalaType.Int
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Int, ScalaType.Long)
 
     override def toCode(typeParam: String): String =
       val dataType = length.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
@@ -100,6 +116,7 @@ object DataType:
     override val name:      String    = "BIGINT"
     override val jdbcType:  JdbcType  = JdbcType.BigInt
     override val scalaType: ScalaType = if unsigned then ScalaType.BigInt else ScalaType.Long
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Long, ScalaType.BigInt)
 
     override def toCode(typeParam: String): String =
       val dataType = length.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
@@ -113,6 +130,7 @@ object DataType:
     override val name:      String    = "DECIMAL"
     override val jdbcType:  JdbcType  = JdbcType.Decimal
     override val scalaType: ScalaType = ScalaType.BigDecimal
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (unsigned, zerofill) match
@@ -125,6 +143,7 @@ object DataType:
     override val name:      String    = "FLOAT"
     override val jdbcType:  JdbcType  = JdbcType.Float
     override val scalaType: ScalaType = ScalaType.Float
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Float, ScalaType.Double)
 
     override def toCode(typeParam: String): String =
       (unsigned, zerofill) match
@@ -137,6 +156,7 @@ object DataType:
     override val name:      String    = "CHAR"
     override val jdbcType:  JdbcType  = JdbcType.Char
     override val scalaType: ScalaType = ScalaType.String
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (character, collate) match
@@ -149,6 +169,7 @@ object DataType:
     override val name:      String    = "VARCHAR"
     override val jdbcType:  JdbcType  = JdbcType.VarChar
     override val scalaType: ScalaType = ScalaType.String
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (character, collate) match
@@ -161,6 +182,7 @@ object DataType:
     override val name:      String    = "BINARY"
     override val jdbcType:  JdbcType  = JdbcType.Binary
     override val scalaType: ScalaType = ScalaType.ArrayByte
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]($length)"
 
@@ -168,6 +190,7 @@ object DataType:
     override val name:      String    = "VARBINARY"
     override val jdbcType:  JdbcType  = JdbcType.VarBinary
     override val scalaType: ScalaType = ScalaType.ArrayByte
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]($length)"
 
@@ -175,6 +198,7 @@ object DataType:
     override val name:      String    = "TINYBLOB"
     override val jdbcType:  JdbcType  = JdbcType.VarBinary
     override val scalaType: ScalaType = ScalaType.ArrayByte
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]()"
 
@@ -182,6 +206,7 @@ object DataType:
     override val name:      String    = "TINYTEXT"
     override val jdbcType:  JdbcType  = JdbcType.VarChar
     override val scalaType: ScalaType = ScalaType.String
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (character, collate) match
@@ -194,6 +219,10 @@ object DataType:
     override val name:      String    = "ENUM"
     override val jdbcType:  JdbcType  = JdbcType.Char
     override val scalaType: ScalaType = ScalaType.Enum(types)
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
+
+    override def getTypeMatches(custom: String): String =
+      scalaTypes.find(_.toString == custom).map(_.code).getOrElse(custom)
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]"
 
@@ -201,6 +230,7 @@ object DataType:
     override val name:      String    = "BLOB"
     override val jdbcType:  JdbcType  = JdbcType.Blob
     override val scalaType: ScalaType = ScalaType.ArrayByte
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = length.fold(s"$name[$typeParam]()")(n => s"$name[$typeParam]($n)")
 
@@ -208,6 +238,7 @@ object DataType:
     override val name:      String    = "TEXT"
     override val jdbcType:  JdbcType  = JdbcType.LongVarChar
     override val scalaType: ScalaType = ScalaType.String
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (character, collate) match
@@ -220,6 +251,7 @@ object DataType:
     override val name:      String    = "MEDIUMBLOB"
     override val jdbcType:  JdbcType  = JdbcType.LongVarBinary
     override val scalaType: ScalaType = ScalaType.ArrayByte
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]()"
 
@@ -227,6 +259,7 @@ object DataType:
     override val name:      String    = "MEDIUMTEXT"
     override val jdbcType:  JdbcType  = JdbcType.LongVarChar
     override val scalaType: ScalaType = ScalaType.String
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (character, collate) match
@@ -239,6 +272,7 @@ object DataType:
     override val name:      String    = "LONGBLOB"
     override val jdbcType:  JdbcType  = JdbcType.LongVarBinary
     override val scalaType: ScalaType = ScalaType.ArrayByte
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]()"
 
@@ -246,6 +280,7 @@ object DataType:
     override val name:      String    = "LONGTEXT"
     override val jdbcType:  JdbcType  = JdbcType.LongVarChar
     override val scalaType: ScalaType = ScalaType.String
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String =
       (character, collate) match
@@ -258,6 +293,7 @@ object DataType:
     override val name:      String    = "DATE"
     override val jdbcType:  JdbcType  = JdbcType.Date
     override val scalaType: ScalaType = ScalaType.LocalDate
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = s"$name[$typeParam]"
 
@@ -265,6 +301,7 @@ object DataType:
     override val name:      String    = "DATETIME"
     override val jdbcType:  JdbcType  = JdbcType.Timestamp
     override val scalaType: ScalaType = ScalaType.LocalDateTime
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Instant, ScalaType.OffsetTime)
 
     override def toCode(typeParam: String): String = fsp.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
 
@@ -272,6 +309,7 @@ object DataType:
     override val name:      String    = "TIMESTAMP"
     override val jdbcType:  JdbcType  = JdbcType.Timestamp
     override val scalaType: ScalaType = ScalaType.LocalDateTime
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Instant, ScalaType.OffsetDateTime, ScalaType.ZonedDateTime)
 
     override def toCode(typeParam: String): String = fsp.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
 
@@ -279,13 +317,15 @@ object DataType:
     override val name:      String    = "TIME"
     override val jdbcType:  JdbcType  = JdbcType.Time
     override val scalaType: ScalaType = ScalaType.LocalTime
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def toCode(typeParam: String): String = fsp.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
 
   def YEAR(digit: Option[4]): DataType = new DataType:
     override val name:      String    = "YEAR"
     override val jdbcType:  JdbcType  = JdbcType.Date
-    override val scalaType: ScalaType = ScalaType.YEAR
+    override val scalaType: ScalaType = ScalaType.Year
+    override val scalaTypes: Seq[ScalaType] = Seq(ScalaType.Instant, ScalaType.LocalDate)
 
     override def toCode(typeParam: String): String = digit.fold(s"$name[$typeParam]")(n => s"$name[$typeParam]($n)")
 
@@ -293,6 +333,7 @@ object DataType:
     override val name:      String    = "SERIAL"
     override val jdbcType:  JdbcType  = JdbcType.BigInt
     override val scalaType: ScalaType = ScalaType.BigInt
+    override val scalaTypes: Seq[ScalaType] = Seq.empty
 
     override def propertyType(isOptional: Boolean): String = scalaType.code
 
