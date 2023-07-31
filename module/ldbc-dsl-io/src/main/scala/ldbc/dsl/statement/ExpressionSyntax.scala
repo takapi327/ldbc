1f5a68c3ebef2d18f3dadbf2749074db62cab7b1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-07-31 23:23:45 +0900

    Fix ExpressionSyntax

--- module/ldbc-dsl-io/src/main/scala/ldbc/dsl/statement/ExpressionSyntax.scala
+++ module/ldbc-dsl-io/src/main/scala/ldbc/dsl/statement/ExpressionSyntax.scala
@@ -4,6 +4,7 @@
 
 package ldbc.dsl.statement
 
+import ldbc.core.interpreter.Extract
 import ldbc.dsl.Parameter
 
 /**
@@ -31,172 +32,186 @@ private[ldbc] trait ExpressionSyntax[F[_], T]:
   /**
    * Trait for setting Scala and Java values to PreparedStatement.
    */
-  def parameter: Option[Parameter[F, T]]
+  def parameter: Option[Parameter[F, Extract[T]]]
 
 object ExpressionSyntax:
 
   private[ldbc] trait SingleValue[F[_], T] extends ExpressionSyntax[F, T]:
-    def value: T
-
-    def NOT: Not[F, T] = Not[F, T]("NOT", this)
-    def ! : Not[F, T] = Not[F, T]("!", this)
-    def |(expr: SingleValue[F, T]) : BitOr[F, T] = BitOr[F, T](this, expr)
+    def value: Extract[T]
 
   private[ldbc] trait MultiValue[F[_], T] extends ExpressionSyntax[F, T]:
-    def value: Seq[T]
+    def value: Seq[Extract[T]]
 
   /** comparison operator */
-  private[ldbc] case class MatchCondition[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+  private[ldbc] case class MatchCondition[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "="
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class OrMore[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends ExpressionSyntax[F, T]:
+    def NOT: MatchCondition[F, T] = MatchCondition[F, T](this.column, true, this.value)
+
+  private[ldbc] case class OrMore[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = ">="
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s" $flag NULL"
-      case _ => s" $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class Over[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: OrMore[F, T] = OrMore[F, T](this.column, true, this.value)
+
+  private[ldbc] case class Over[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = ">"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class LessThanOrEqualTo[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: Over[F, T] = Over[F, T](this.column, true, this.value)
+
+  private[ldbc] case class LessThanOrEqualTo[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "<="
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s" $flag NULL"
-      case _ => s" $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class LessThan[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: LessThanOrEqualTo[F, T] = LessThanOrEqualTo[F, T](this.column, true, this.value)
+
+  private[ldbc] case class LessThan[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "<"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s" $flag NULL"
-      case _ => s" $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class NotEqual[F[_], T](flag: String, column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s" $flag NULL"
-      case _ => s" $flag ?"
+    def NOT: LessThan[F, T] = LessThan[F, T](this.column, true, this.value)
 
-  private[ldbc] case class Not[F[_], T](flag: String, expr: SingleValue[F, T]) extends SingleValue[F, T]:
-    override def column: String = expr.column
-    override def value: T = expr.value
-    override def parameter: Option[Parameter[F, T]] = expr.parameter
-    override def statement: String = s"$flag ${ expr.statement }"
+  private[ldbc] case class NotEqual[F[_], T](flag: String, column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class Is[F[_], T <: "TRUE" | "FALSE" | "UNKNOWN" | "NULL"](column: String, value: T) extends SingleValue[F, T]:
+    def NOT: NotEqual[F, T] = NotEqual[F, T](this.flag, this.column, true, this.value)
+
+  private[ldbc] case class Is[F[_], T <: "TRUE" | "FALSE" | "UNKNOWN" | "NULL"](column: String, isNot: Boolean, value: T) extends SingleValue[F, T]:
     override def flag: String = "IS"
     override def parameter: Option[Parameter[F, T]] = None
-    override def statement: String = s"$column $flag $value"
+    override def statement: String =
+      val not = if isNot then " NOT" else ""
+      s"$column $flag$not $value"
 
-  private[ldbc] case class IsNot[F[_], T <: "TRUE" | "FALSE" | "UNKNOWN" | "NULL"](column: String, value: T) extends SingleValue[F, T]:
-    override def flag: String = "IS NOT"
-    override def parameter: Option[Parameter[F, T]] = None
-    override def statement: String = s"$column $flag $value"
+    def NOT: Is[F, T] = Is[F, T](this.column, true, this.value)
 
-  private[ldbc] case class NullSafeEqual[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+  private[ldbc] case class NullSafeEqual[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "<=>"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class In[F[_], T](column: String, value: T*)(using _parameter: Parameter[F, T]) extends MultiValue[F, T]:
+    def NOT: NullSafeEqual[F, T] = NullSafeEqual[F, T](this.column, true, this.value)
+
+  private[ldbc] case class In[F[_], T](column: String, isNot: Boolean, value: Extract[T]*)(using _parameter: Parameter[F, Extract[T]]) extends MultiValue[F, T]:
     override def flag: String = "IN"
-    override def parameter: Option[Parameter[F, T]] = Some(_parameter)
-    override def statement: String = s"$column $flag (${ value.map(_ => "?").mkString(", ") })"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$column $not$flag (${value.map(_ => "?").mkString(", ")})"
 
-  private[ldbc] case class Between[F[_], T](column: String, value: T*)(using _parameter: Parameter[F, T]) extends MultiValue[F, T]:
+    def NOT: In[F, T] = In[F, T](this.column, true, this.value: _*)
+
+  private[ldbc] case class Between[F[_], T](column: String, isNot: Boolean, value: Extract[T]*)(using _parameter: Parameter[F, Extract[T]]) extends MultiValue[F, T]:
     override def flag: String = "BETWEEN"
-    override def parameter: Option[Parameter[F, T]] = Some(_parameter)
-    override def statement: String = s"$column $flag ? AND ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$column $not$flag ? AND ?"
 
-  private[ldbc] case class Like[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: Between[F, T] = Between[F, T](this.column, true, this.value: _*)
+
+  private[ldbc] case class Like[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "LIKE"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class LikeEscape[F[_], T](column: String, value: T*)(using _parameter: Parameter[F, T]) extends MultiValue[F, T]:
+    def NOT: Like[F, T] = Like[F, T](this.column, true, this.value)
+
+
+  private[ldbc] case class LikeEscape[F[_], T](column: String, isNot: Boolean, value: Extract[T]*)(using _parameter: Parameter[F, Extract[T]]) extends MultiValue[F, T]:
     override def flag: String = "LIKE"
-    override def parameter: Option[Parameter[F, T]] = Some(_parameter)
-    override def statement: String = s"$column $flag ? ESCAPE ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ? ESCAPE ?"
 
-  private[ldbc] case class Regexp[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: LikeEscape[F, T] = LikeEscape[F, T](this.column, true, this.value: _*)
+
+  private[ldbc] case class Regexp[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "REGEXP"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class LeftShift[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: Regexp[F, T] = Regexp[F, T](this.column, true, this.value)
+
+  private[ldbc] case class LeftShift[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "<<"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class RightShift[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+    def NOT: LeftShift[F, T] = LeftShift[F, T](this.column, true, this.value)
+
+  private[ldbc] case class RightShift[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = ">>"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
 
-  private[ldbc] case class Div[F[_], T](column: String, value: T*)(using _parameter: Parameter[F, T]) extends MultiValue[F, T]:
+    def NOT: RightShift[F, T] = RightShift[F, T](this.column, true, this.value)
+
+  private[ldbc] case class Div[F[_], T](column: String, isNot: Boolean, value: Extract[T]*)(using _parameter: Parameter[F, Extract[T]]) extends MultiValue[F, T]:
     override def flag: String = "DIV"
-    override def parameter: Option[Parameter[F, T]] = Some(_parameter)
-    override def statement: String = s"$column $flag ? = ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ? = ?"
 
-  private[ldbc] case class Mod[F[_], T](flag: String, column: String, value: T*)(using _parameter: Parameter[F, T]) extends MultiValue[F, T]:
-    override def parameter: Option[Parameter[F, T]] = Some(_parameter)
-    override def statement: String = s"$column $flag ? = ?"
+    def NOT: Div[F, T] = Div[F, T](this.column, true, this.value: _*)
 
-  private[ldbc] case class BitXOR[F[_], T](column: String, value: T)(using _parameter: Parameter[F, T]) extends SingleValue[F, T]:
+  private[ldbc] case class Mod[F[_], T](flag: String, column: String, isNot: Boolean, value: Extract[T]*)(using _parameter: Parameter[F, Extract[T]]) extends MultiValue[F, T]:
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ? = ?"
+
+    def NOT: Mod[F, T] = Mod[F, T](this.flag, this.column, true, this.value: _*)
+
+  private[ldbc] case class BitXOR[F[_], T](column: String, isNot: Boolean, value: Extract[T])(using _parameter: Parameter[F, Extract[T]]) extends SingleValue[F, T]:
     override def flag: String = "^"
-    override def parameter: Option[Parameter[F, T]] = value match
-      case None => None
-      case _ => Some(_parameter)
-    override def statement: String = value match
-      case None => s"$column $flag NULL"
-      case _ => s"$column $flag ?"
+    override def parameter: Option[Parameter[F, Extract[T]]] = Some(_parameter)
 
-  private[ldbc] case class BitOr[F[_], T](expr1: SingleValue[F, T], expr2: SingleValue[F, T]) extends MultiValue[F, T]:
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not$column $flag ?"
+
+    def NOT: BitXOR[F, T] = BitXOR[F, T](this.column, true, this.value)
+
+  private[ldbc] case class BitOr[F[_], T](isNot: Boolean, expr1: SingleValue[F, T], expr2: SingleValue[F, T]) extends MultiValue[F, T]:
     override def flag: String = "|"
     override def column: String = expr1.column
-    override def value: Seq[T] = Seq(expr1.value, expr2.value)
-    override def parameter: Option[Parameter[F, T]] = expr2.parameter
+    override def value: Seq[Extract[T]] = Seq(expr1.value, expr2.value)
+    override def parameter: Option[Parameter[F, Extract[T]]] = expr2.parameter
+    override def statement: String =
+      val not = if isNot then "NOT " else ""
+      s"$not${ expr1.statement } $flag ${ expr2.statement }"
 
-    override def statement: String = s"${ expr1.statement } $flag ${ expr2.statement }"
+    def NOT: BitOr[F, T] = BitOr[F, T](true, this.expr1, this.expr2)
