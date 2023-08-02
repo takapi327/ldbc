b79ddb1dd9c2535992acb8f1494365bdb3a0dbd6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-02 23:55:55 +0900

    Create Where statement model

---
+++ module/ldbc-dsl-io/src/main/scala/ldbc/dsl/statement/Where.scala
@@ -0,0 +1,55 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.dsl.statement
+
+import ldbc.core.Table
+import ldbc.dsl.ParameterBinder
+
+/**
+ * A model for constructing WHERE statements in MySQL.
+ *
+ * @param table
+ * Trait for generating SQL table information.
+ * @param statement
+ * SQL statement string
+ * @param columns
+ * Union-type column list
+ * @param params
+ * A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index only.
+ * @tparam F
+ *   The effect type
+ * @tparam P
+ *   Base trait for all products
+ * @tparam T
+ *   Union type of column
+ */
+private[ldbc] case class Where[F[_], P <: Product, T](
+  table:       Table[P],
+  statement:   String,
+  columns:     T,
+  params:      Seq[ParameterBinder[F]]
+) extends Query[F, T]:
+
+  /**
+   * A method for combining WHERE statements.
+   *
+   * @param label
+   * A conjunctive expression to join WHERE statements together.
+   * @param expressionSyntax
+   * Trait for the syntax of expressions available in MySQL.
+   */
+  private def union(label: String, expressionSyntax: ExpressionSyntax[F]): Where[F, P, T] =
+    Where[F, P, T](
+      table = table,
+      statement = statement ++ s" $label ${expressionSyntax.statement}",
+      columns = columns,
+      params = params ++ expressionSyntax.parameter
+    )
+
+  def and(func: Table[P] => ExpressionSyntax[F]): Where[F, P, T] = union("AND", func(table))
+  def or(func: Table[P] => ExpressionSyntax[F]): Where[F, P, T] = union("OR", func(table))
+  def ||(func: Table[P] => ExpressionSyntax[F]): Where[F, P, T] = union("||", func(table))
+  def xor(func: Table[P] => ExpressionSyntax[F]): Where[F, P, T] = union("XOR", func(table))
+  def &&(func: Table[P] => ExpressionSyntax[F]): Where[F, P, T] = union("&&", func(table))
