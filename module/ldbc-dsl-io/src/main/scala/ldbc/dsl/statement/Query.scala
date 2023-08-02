9e113f5d7b781ddece33cd22caa5db9538134e96
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-02 23:58:20 +0900

    Action sbt scalafmtAll

--- module/ldbc-dsl-io/src/main/scala/ldbc/dsl/statement/Query.scala
+++ module/ldbc-dsl-io/src/main/scala/ldbc/dsl/statement/Query.scala
@@ -1,31 +1,27 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.dsl.statement
 
 import ldbc.dsl.ParameterBinder
 
-/**
- *
- * @tparam F
- *   The effect type
- * @tparam T
- *   Union type of column
- */
+/** @tparam F
+  *   The effect type
+  * @tparam T
+  *   Union type of column
+  */
 private[ldbc] trait Query[F[_], T]:
 
-  /**
-   * SQL statement string
-   */
+  /** SQL statement string
+    */
   def statement: String
 
-  /**
-   * Union-type column list
-   */
+  /** Union-type column list
+    */
   def columns: T
 
-  /**
-   * A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index only.
-   */
+  /** A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index
+    * only.
+    */
   def params: Seq[ParameterBinder[F]]
--- module/ldbc-dsl-io/src/main/scala/ldbc/dsl/syntax/QuerySyntax.scala
+++ module/ldbc-dsl-io/src/main/scala/ldbc/dsl/syntax/QuerySyntax.scala
@@ -1,6 +1,6 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.dsl.syntax
 
@@ -14,36 +14,39 @@ import ldbc.dsl.logging.{ LogEvent, LogHandler }
 
 trait QuerySyntax[F[_]: Sync]:
 
-  extension [T] (query: Query[F, T])
+  extension [T](query: Query[F, T])
 
     private def connection[A](consumer: ResultSetConsumer[F, A])(using
-      logHandler: LogHandler[F]
+      logHandler:                       LogHandler[F]
     ): Kleisli[F, Connection[F], A] =
       Kleisli { connection =>
         for
           statement <- connection.prepareStatement(query.statement)
           resultSet <- query.params.zipWithIndex.traverse {
-            case (param, index) => param.bind(statement, index + 1)
-          } >> statement
-            .executeQuery()
-            .onError(ex =>
-              logHandler.run(LogEvent.ExecFailure(query.statement, query.params.map(_.parameter).toList, ex))
-            )
-          result <- consumer
-            .consume(resultSet)
-            .onError(ex =>
-              logHandler.run(LogEvent.ProcessingFailure(query.statement, query.params.map(_.parameter).toList, ex))
-            )
-            <* statement.close()
-            <* logHandler.run(LogEvent.Success(query.statement, query.params.map(_.parameter).toList))
+                         case (param, index) => param.bind(statement, index + 1)
+                       } >> statement
+                         .executeQuery()
+                         .onError(ex =>
+                           logHandler.run(
+                             LogEvent.ExecFailure(query.statement, query.params.map(_.parameter).toList, ex)
+                           )
+                         )
+          result <-
+            consumer
+              .consume(resultSet)
+              .onError(ex =>
+                logHandler.run(LogEvent.ProcessingFailure(query.statement, query.params.map(_.parameter).toList, ex))
+              )
+              <* statement.close()
+              <* logHandler.run(LogEvent.Success(query.statement, query.params.map(_.parameter).toList))
         yield result
       }
 
-    def headOption[A] (func: T => Kleisli[F, ResultSet[F], A] ) (using
+    def headOption[A](func: T => Kleisli[F, ResultSet[F], A])(using
       LogHandler[F]
     ): Kleisli[F, Connection[F], Option[A]] =
       given Kleisli[F, ResultSet[F], A] = func(query.columns)
-      connection[Option[A]] (summon[ResultSetConsumer[F, Option[A]]] )
+      connection[Option[A]](summon[ResultSetConsumer[F, Option[A]]])
 
     def toList[A](func: T => Kleisli[F, ResultSet[F], A])(using LogHandler[F]): Kleisli[F, Connection[F], List[A]] =
       given Kleisli[F, ResultSet[F], A] = func(query.columns)
