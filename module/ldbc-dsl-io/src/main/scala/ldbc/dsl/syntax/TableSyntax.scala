5487fb6bec2b0124b4d941b0fbc21bc5eea49e0e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-05 17:35:33 +0900

    Action sbt scalafmtAll

--- module/ldbc-dsl-io/src/main/scala/ldbc/dsl/syntax/TableSyntax.scala
+++ module/ldbc-dsl-io/src/main/scala/ldbc/dsl/syntax/TableSyntax.scala
@@ -22,12 +22,15 @@ trait TableSyntax[F[_]]:
         Seq.empty
       )
 
-    def select[T <: Tuple.Union[Tuple.Map[mirror.MirroredElemTypes, Column]] | Tuple.Union[Tuple.Map[mirror.MirroredElemTypes, Column]] *: NonEmptyTuple](
+    def select[
+      T <: Tuple.Union[Tuple.Map[mirror.MirroredElemTypes, Column]] |
+        Tuple.Union[Tuple.Map[mirror.MirroredElemTypes, Column]] *: NonEmptyTuple
+    ](
       func: Table[P] => T
     ): Select[F, P, T] =
       val columns = func(table)
       val str = columns match
         case v: Tuple => v.toArray.distinct.mkString(", ")
-        case v => v
-      val statement = s"SELECT $str FROM ${table._name}"
+        case v        => v
+      val statement = s"SELECT $str FROM ${ table._name }"
       Select[F, P, T](table, statement, columns, Seq.empty)
