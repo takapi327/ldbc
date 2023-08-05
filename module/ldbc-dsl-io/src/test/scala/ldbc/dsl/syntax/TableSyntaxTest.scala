1451f5726fc00c65719080ad23df4204d81c3dbe
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-05 21:55:56 +0900

    Action sbt scalafmtAll

--- module/ldbc-dsl-io/src/test/scala/ldbc/dsl/syntax/TableSyntaxTest.scala
+++ module/ldbc-dsl-io/src/test/scala/ldbc/dsl/syntax/TableSyntaxTest.scala
@@ -1,6 +1,6 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.dsl.syntax
 
@@ -22,11 +22,26 @@ object TableSyntaxTest extends Specification, TableSyntax[Id], ColumnSyntax[Id]:
     "The query statement generated from Table is equal to the specified query statement." in {
       table.select[Column[Long]](_.p1).statement === "SELECT `p1` FROM test" and
         table.select[Column[Long]](_.p1).where(_.p1 > 1).statement === "SELECT `p1` FROM test WHERE p1 > ?" and
-        table.select[Column[Long]](_.p1).where(_.p1 >= 1).and(_.p2 === "test").statement === "SELECT `p1` FROM test WHERE p1 >= ? AND p2 = ?" and
-        table.select[Column[Long]](_.p1).where(v => v.p1 > 1 || v.p2 === "test").statement === "SELECT `p1` FROM test WHERE (p1 > ? OR p2 = ?)" and
-        table.select[Column[Long]](_.p1).where(v => v.p1 > 1 && v.p2 === "test").or(_.p3 === "test").statement === "SELECT `p1` FROM test WHERE (p1 > ? AND p2 = ?) OR p3 = ?" and
+        table
+          .select[Column[Long]](_.p1)
+          .where(_.p1 >= 1)
+          .and(_.p2 === "test")
+          .statement === "SELECT `p1` FROM test WHERE p1 >= ? AND p2 = ?" and
+        table
+          .select[Column[Long]](_.p1)
+          .where(v => v.p1 > 1 || v.p2 === "test")
+          .statement === "SELECT `p1` FROM test WHERE (p1 > ? OR p2 = ?)" and
+        table
+          .select[Column[Long]](_.p1)
+          .where(v => v.p1 > 1 && v.p2 === "test")
+          .or(_.p3 === "test")
+          .statement === "SELECT `p1` FROM test WHERE (p1 > ? AND p2 = ?) OR p3 = ?" and
         table.select[Column[Long]](_.p1).groupBy(p1 => p1).statement === "SELECT `p1` FROM test GROUP BY p1" and
-        table.select[Column[Long]](_.p1).groupBy(p1 => p1).having(_ < 1).statement === "SELECT `p1` FROM test GROUP BY p1 HAVING p1 < ?" and
+        table
+          .select[Column[Long]](_.p1)
+          .groupBy(p1 => p1)
+          .having(_ < 1)
+          .statement === "SELECT `p1` FROM test GROUP BY p1 HAVING p1 < ?" and
         table.select[Column[Long]](_.p1).orderBy(_.p1.desc).statement === "SELECT `p1` FROM test ORDER BY p1 DESC" and
         table.select[Column[Long]](_.p1).limit(10).statement === "SELECT `p1` FROM test LIMIT ?" and
         table.select[Column[Long]](_.p1).limit(10).offset(0).statement === "SELECT `p1` FROM test LIMIT ? OFFSET ?"
