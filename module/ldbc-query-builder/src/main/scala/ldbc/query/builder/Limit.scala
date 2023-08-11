d7e16de4cbe9ed2765653f6c203436214220fff8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-11 21:29:54 +0900

    Replace Limit dsl -> query.builder

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/Limit.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/Limit.scala
@@ -1,33 +1,33 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
-  * distributed with this source code.
-  */
+ * distributed with this source code.
+ */
 
-package ldbc.dsl.statement
+package ldbc.query.builder
 
-import ldbc.dsl.{ Parameter, ParameterBinder }
+import ldbc.sql.{ Parameter, ParameterBinder }
 
 /** A model for constructing LIMIT statements in MySQL.
-  *
-  * @param statement
-  *   SQL statement string
-  * @param columns
-  *   Union-type column list
-  * @param params
-  *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index
-  *   only.
-  * @tparam F
-  *   The effect type
-  * @tparam T
-  *   Union type of column
-  */
-private[ldbc] case class Limit[F[_], T](
+ *
+ * @param statement
+ *   SQL statement string
+ * @param columns
+ *   Union-type column list
+ * @param params
+ *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index
+ *   only.
+ * @tparam F
+ *   The effect type
+ * @tparam T
+ *   Union type of column
+ */
+private[ldbc] case class Limit[F[_], T <: Tuple](
   statement: String,
   columns:   T,
   params:    Seq[ParameterBinder[F]]
 ) extends Query[F, T]:
 
   /** A method for setting the OFFSET condition in a statement.
-    */
+   */
   def offset(length: Long): Parameter[F, Long] ?=> Limit[F, T] =
     Limit(
       statement = statement ++ " OFFSET ?",
@@ -36,17 +36,17 @@ private[ldbc] case class Limit[F[_], T](
     )
 
 /** Transparent Trait to provide limit method.
-  *
-  * @tparam F
-  *   The effect type
-  * @tparam T
-  *   Union type of column
-  */
-private[ldbc] transparent trait LimitProvider[F[_], T]:
+ *
+ * @tparam F
+ *   The effect type
+ * @tparam T
+ *   Union type of column
+ */
+private[ldbc] transparent trait LimitProvider[F[_], T <: Tuple]:
   self: Query[F, T] =>
 
   /** A method for setting the LIMIT condition in a statement.
-    */
+   */
   def limit(length: Long): Parameter[F, Long] ?=> Limit[F, T] =
     Limit(
       statement = statement ++ " LIMIT ?",
