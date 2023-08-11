f8316cffc423d7a38cbe310aa529b6f460f601b7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-11 22:49:36 +0900

    Fix bug

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/TableQuery.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/TableQuery.scala
@@ -4,15 +4,15 @@
 
 package ldbc.query.builder
 
-import ldbc.core.{ Table, Column }
+import ldbc.core.Table
 import ldbc.query.builder.statement.Select
 import ldbc.query.builder.interpreter.Tuples
 
 case class TableQuery[F[_], P <: Product](table: Table[P]):
 
   def select[T <: Tuple](
-    func: Table[P] => Tuples.MapToColumn[T, Column]
-  ): Select[F, P, Tuples.MapToColumn[T, Column]] =
+    func: Table[P] => Tuples.MapToColumn[T, F]
+  ): Select[F, P, Tuples.MapToColumn[T, F]] =
     val columns = func(table)
     val statement = s"SELECT ${columns.toArray.distinct.mkString(", ")} FROM ${table._name}"
-    Select[F, P, Tuples.MapToColumn[T, Column]](table, statement, columns, Seq.empty)
+    Select[F, P, Tuples.MapToColumn[T, F]](table, statement, columns, Seq.empty)
