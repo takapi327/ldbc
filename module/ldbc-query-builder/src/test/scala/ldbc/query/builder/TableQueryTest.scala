fef677a9f43d6a59015cee202a3df8ba6beb105b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-12 00:40:54 +0900

    Replace test dsl -> query.builder

--- module/ldbc-query-builder/src/test/scala/ldbc/query/builder/TableQueryTest.scala
+++ module/ldbc-query-builder/src/test/scala/ldbc/query/builder/TableQueryTest.scala
@@ -1,15 +1,16 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
-  * distributed with this source code.
-  */
+ * distributed with this source code.
+ */
 
-package ldbc.dsl.syntax
+package ldbc.query.builder
 
 import org.scalatest.flatspec.AnyFlatSpec
 
 import cats.Id
 import ldbc.core.*
+import ldbc.query.builder.syntax.ColumnSyntax
 
-class TableSyntaxTest extends AnyFlatSpec, TableSyntax[Id], ColumnSyntax[Id]:
+class TableQueryTest extends AnyFlatSpec, ColumnSyntax[Id]:
 
   case class Test(p1: Long, p2: String, p3: Option[String])
   private val table = Table[Test]("test")(
@@ -18,52 +19,52 @@ class TableSyntaxTest extends AnyFlatSpec, TableSyntax[Id], ColumnSyntax[Id]:
     column("p3", VARCHAR(255))
   )
 
+  private val query = TableQuery[Id, Test](table)
+
   it should "The query statement generated from Table is equal to the specified query statement." in {
-    assert(table.select[Column[Long]](_.p1).statement === "SELECT `p1` FROM test")
-    assert(table.select[Column[Long]](_.p1).where(_.p1 > 1).statement === "SELECT `p1` FROM test WHERE p1 > ?")
+    assert(query.select[Long](_.p1).statement === "SELECT `p1` FROM test")
+    assert(query.select[Long](_.p1).where(_.p1 > 1).statement === "SELECT `p1` FROM test WHERE p1 > ?")
     assert(
-      table
-        .select[Column[Long]](_.p1)
+      query
+        .select[Long](_.p1)
         .where(_.p1 >= 1)
         .and(_.p2 === "test")
         .statement === "SELECT `p1` FROM test WHERE p1 >= ? AND p2 = ?"
     )
     assert(
-      table
-        .select[Column[Long]](_.p1)
+      query
+        .select[Long](_.p1)
         .where(v => v.p1 > 1 || v.p2 === "test")
         .statement === "SELECT `p1` FROM test WHERE (p1 > ? OR p2 = ?)"
     )
     assert(
-      table
-        .select[Column[Long]](_.p1)
+      query
+        .select[Long](_.p1)
         .where(v => v.p1 > 1 && v.p2 === "test")
         .or(_.p3 === "test")
         .statement === "SELECT `p1` FROM test WHERE (p1 > ? AND p2 = ?) OR p3 = ?"
     )
-    assert(table.select[Column[Long]](_.p1).groupBy(p1 => p1).statement === "SELECT `p1` FROM test GROUP BY p1")
+    assert(query.select[Long](_.p1).groupBy(p1 => p1).statement === "SELECT `p1` FROM test GROUP BY p1")
     assert(
-      table
-        .select[Column[Long]](_.p1)
+      query
+        .select[Long](_.p1)
         .groupBy(p1 => p1)
         .having(_ < 1)
         .statement === "SELECT `p1` FROM test GROUP BY p1 HAVING p1 < ?"
     )
-    assert(table.select[Column[Long]](_.p1).orderBy(_.p1.desc).statement === "SELECT `p1` FROM test ORDER BY p1 DESC")
-    assert(table.select[Column[Long]](_.p1).limit(10).statement === "SELECT `p1` FROM test LIMIT ?")
-    assert(table.select[Column[Long]](_.p1).limit(10).offset(0).statement === "SELECT `p1` FROM test LIMIT ? OFFSET ?")
+    assert(query.select[Long](_.p1).orderBy(_.p1.desc).statement === "SELECT `p1` FROM test ORDER BY p1 DESC")
+    assert(query.select[Long](_.p1).limit(10).statement === "SELECT `p1` FROM test LIMIT ?")
+    assert(query.select[Long](_.p1).limit(10).offset(0).statement === "SELECT `p1` FROM test LIMIT ? OFFSET ?")
     assert(
-      table
-        .select[Column[Long]](_.p1)
-        .where(_.p1 === table.select[Column[Long]](_.p1).where(_.p1 > 1))
+      query
+        .select[Long](_.p1)
+        .where(_.p1 === query.select[Long](_.p1).where(_.p1 > 1))
         .statement === "SELECT `p1` FROM test WHERE p1 = (SELECT `p1` FROM test WHERE p1 > ?)"
     )
     assert(
-      table
-        .select[Column[Long]](_.p1)
-        .where(v =>
-          (v.p1 >= table.select[Column[Long]](_.p1).where(_.p1 > 1)) || (v.p2 === table.select[Column[String]](_.p2))
-        )
+      query
+        .select[Long](_.p1)
+        .where(v => (v.p1 hoge query.select[Long](_.p1).where(_.p1 > 1)) || (v.p2 === query.select[String](_.p2)))
         .statement === "SELECT `p1` FROM test WHERE (p1 >= (SELECT `p1` FROM test WHERE p1 > ?) OR p2 = (SELECT `p2` FROM test))"
     )
   }
