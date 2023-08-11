c6302e5036693f2fbe6a6f4c24908c96813ea411
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-11 21:33:40 +0900

    Replace GroupBy dsl -> query.builder

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/GroupBy.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/GroupBy.scala
@@ -1,38 +1,38 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
-  * distributed with this source code.
-  */
+ * distributed with this source code.
+ */
 
-package ldbc.dsl.statement
+package ldbc.query.builder
 
 import ldbc.core.*
-import ldbc.dsl.ParameterBinder
+import ldbc.sql.ParameterBinder
 
 /** A model for constructing GROUP BY statements in MySQL.
-  *
-  * @param table
-  *   Trait for generating SQL table information.
-  * @param statement
-  *   SQL statement string
-  * @param columns
-  *   Union-type column list
-  * @param params
-  *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index
-  *   only.
-  * @tparam F
-  *   The effect type
-  * @tparam P
-  *   Base trait for all products
-  * @tparam T
-  *   Union type of column
-  */
+ *
+ * @param table
+ *   Trait for generating SQL table information.
+ * @param statement
+ *   SQL statement string
+ * @param columns
+ *   Union-type column list
+ * @param params
+ *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index
+ *   only.
+ * @tparam F
+ *   The effect type
+ * @tparam P
+ *   Base trait for all products
+ * @tparam T
+ *   Union type of column
+ */
 private[ldbc] case class GroupBy[F[_], P <: Product, T](
   table:     Table[P],
   statement: String,
   columns:   T,
   params:    Seq[ParameterBinder[F]]
 ) extends Query[F, T],
-          OrderByProvider[F, P, T],
-          LimitProvider[F, T]:
+  OrderByProvider[F, P, T],
+  LimitProvider[F, T]:
 
   def having[A](func: T => ExpressionSyntax[F]): Having[F, P, T] =
     val expressionSyntax = func(columns)
