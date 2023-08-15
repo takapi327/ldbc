224b96f1333a1486c3b0966052c7ba877aad5768
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-15 23:24:48 +0900

    Add selectAll method

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/TableQuery.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/TableQuery.scala
@@ -4,11 +4,35 @@
 
 package ldbc.query.builder
 
-import ldbc.core.Table
+import scala.deriving.Mirror
+import scala.compiletime.*
+
+import ldbc.core.*
+import ldbc.sql.ResultSetReader
 import ldbc.query.builder.statement.{ Select, Join }
 import ldbc.query.builder.interpreter.Tuples
 
 case class TableQuery[F[_], P <: Product](table: Table[P]):
+
+  private inline def inferResultSetReader[T]: ResultSetReader[F, T] =
+    summonFrom[ResultSetReader[F, T]] {
+      case reader: ResultSetReader[F, T] => reader
+      case _ => error("ResultSetReader cannot be inferred")
+    }
+
+  private inline def foldResultSetReader[T <: Tuple]: Tuples.MapToResultSetReader[F, T] =
+    inline erasedValue[T] match
+      case _: EmptyTuple => EmptyTuple
+      case _: (h *: t) => inferResultSetReader[h] *: foldResultSetReader[t]
+
+  inline def selectAll(using mirror: Mirror.ProductOf[P]): Select[F, P, Tuples.ToColumn[F, mirror.MirroredElemTypes]] =
+    val columns = table.*.zip(foldResultSetReader[mirror.MirroredElemTypes]).map(
+      [t] => (x: t) =>
+        val (column, reader) = x.asInstanceOf[(Column[t], ResultSetReader[F, t])]
+        ColumnReader(column, reader)
+    ).asInstanceOf[Tuples.ToColumn[F, mirror.MirroredElemTypes]]
+    val statement = s"SELECT ${ table.*.toList.mkString(", ") } FROM ${ table._name }"
+    new Select[F, P, Tuples.ToColumn[F, mirror.MirroredElemTypes]](table, statement, columns, Seq.empty)
 
   def select[T](func: Table[P] => Tuples.ToColumn[F, T]): Select[F, P, Tuples.ToColumn[F, T]] =
     val columns = func(table)
