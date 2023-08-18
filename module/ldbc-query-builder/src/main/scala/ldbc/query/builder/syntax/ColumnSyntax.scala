0c7606c85d59166a2a169091064d5112cdd726dc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-08-18 14:16:37 +0900

    Add alias methods

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/syntax/ColumnSyntax.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/syntax/ColumnSyntax.scala
@@ -4,6 +4,8 @@
 
 package ldbc.query.builder.syntax
 
+import scala.annotation.targetName
+
 import cats.data.Kleisli
 
 import ldbc.core.Column
@@ -53,24 +55,39 @@ trait ColumnSyntax[F[_]]:
 
     def desc: OrderBy.Desc = OrderBy.Desc(column)
 
-    def ===(value: Extract[T]): MatchCondition[F, T] = MatchCondition[F, T](buildColumnName(column), false, value)
+    def _equals(value: Extract[T]): MatchCondition[F, T] = MatchCondition[F, T](buildColumnName(column), false, value)
+    @targetName("matchCondition")
+    def ===(value: Extract[T]): MatchCondition[F, T] = _equals(value)
 
-    def >=(value: Extract[T]): OrMore[F, T] = OrMore[F, T](buildColumnName(column), false, value)
+    def orMore(value: Extract[T]): OrMore[F, T] = OrMore[F, T](buildColumnName(column), false, value)
+    @targetName("_orMore")
+    def >=(value: Extract[T]): OrMore[F, T] = orMore(value)
 
-    def >(value: Extract[T]): Over[F, T] = Over[F, T](buildColumnName(column), false, value)
+    def over(value: Extract[T]): Over[F, T] = Over[F, T](buildColumnName(column), false, value)
+    @targetName("_over")
+    def >(value: Extract[T]): Over[F, T] = over(value)
 
-    def <=(value: Extract[T]): LessThanOrEqualTo[F, T] = LessThanOrEqualTo[F, T](buildColumnName(column), false, value)
+    def lessThanOrEqual(value: Extract[T]): LessThanOrEqualTo[F, T] = LessThanOrEqualTo[F, T](buildColumnName(column), false, value)
+    @targetName("_lessThanOrEqual")
+    def <=(value: Extract[T]): LessThanOrEqualTo[F, T] = lessThanOrEqual(value)
 
-    def <(value: Extract[T]): LessThan[F, T] = LessThan[F, T](buildColumnName(column), false, value)
+    def lessThan(value: Extract[T]): LessThan[F, T] = LessThan[F, T](buildColumnName(column), false, value)
+    @targetName("_lessThan")
+    def <(value: Extract[T]): LessThan[F, T] = lessThan(value)
 
-    def <>(value: Extract[T]): NotEqual[F, T] = NotEqual[F, T]("<>", buildColumnName(column), false, value)
+    def notEqual(value: Extract[T]): NotEqual[F, T] = NotEqual[F, T]("<>", buildColumnName(column), false, value)
+    @targetName("_notEqual")
+    def <>(value: Extract[T]): NotEqual[F, T] = notEqual(value)
 
+    @targetName("_!equal")
     def !==(value: Extract[T]): NotEqual[F, T] = NotEqual[F, T]("!=", buildColumnName(column), false, value)
 
     def IS[A <: "TRUE" | "FALSE" | "UNKNOWN" | "NULL"](value: A): Is[F, A] =
       Is[F, A](buildColumnName(column), false, value)
 
-    def <=>(value: Extract[T]): NullSafeEqual[F, T] = NullSafeEqual[F, T](buildColumnName(column), false, value)
+    def nullSafeEqual(value: Extract[T]): NullSafeEqual[F, T] = NullSafeEqual[F, T](buildColumnName(column), false, value)
+    @targetName("_nullSafeEqual")
+    def <=>(value: Extract[T]): NullSafeEqual[F, T] = nullSafeEqual(value)
 
     def IN(value: Extract[T]*): In[F, T] = In[F, T](buildColumnName(column), false, value: _*)
 
@@ -84,86 +101,137 @@ trait ColumnSyntax[F[_]]:
 
     def REGEXP(value: Extract[T]): Regexp[F, T] = Regexp[F, T](buildColumnName(column), false, value)
 
-    def <<(value: Extract[T]): LeftShift[F, T] = LeftShift[F, T](buildColumnName(column), false, value)
+    def leftShift(value: Extract[T]): LeftShift[F, T] = LeftShift[F, T](buildColumnName(column), false, value)
+    @targetName("_leftShift")
+    def <<(value: Extract[T]): LeftShift[F, T] = leftShift(value)
 
-    def >>(value: Extract[T]): RightShift[F, T] = RightShift[F, T](buildColumnName(column), false, value)
+    def rightShift(value: Extract[T]): RightShift[F, T] = RightShift[F, T](buildColumnName(column), false, value)
+    @targetName("_rightShift")
+    def >>(value: Extract[T]): RightShift[F, T] = rightShift(value)
 
     def DIV(cond: Extract[T], result: Extract[T]): Div[F, T] = Div[F, T](buildColumnName(column), false, cond, result)
 
     def MOD(cond: Extract[T], result: Extract[T]): Mod[F, T] =
       Mod[F, T]("MOD", buildColumnName(column), false, cond, result)
 
-    def %(cond: Extract[T], result: Extract[T]): Mod[F, T] =
-      Mod[F, T]("%", buildColumnName(column), false, cond, result)
+    def mod(cond: Extract[T], result: Extract[T]): Mod[F, T] = Mod[F, T]("%", buildColumnName(column), false, cond, result)
+    @targetName("_mod")
+    def %(cond: Extract[T], result: Extract[T]): Mod[F, T] = mod(cond, result)
 
-    def ^(value: Extract[T]): BitXOR[F, T] = BitXOR[F, T](buildColumnName(column), false, value)
+    def bitXOR(value: Extract[T]): BitXOR[F, T] = BitXOR[F, T](buildColumnName(column), false, value)
+    @targetName("_bitXOR")
+    def ^(value: Extract[T]): BitXOR[F, T] = bitXOR(value)
 
-    def ~(value: Extract[T]): BitFlip[F, T] = BitFlip[F, T](buildColumnName(column), false, value)
+    def bitFlip(value: Extract[T]): BitFlip[F, T] = BitFlip[F, T](buildColumnName(column), false, value)
+    @targetName("_bitFlip")
+    def ~(value: Extract[T]): BitFlip[F, T] = bitFlip(value)
 
-    def ++(other: Column[T]): MultiColumn[T] = MultiColumn[T]("+", column, other)
+    def combine(other: Column[T]): MultiColumn[T] = MultiColumn[T]("+", column, other)
+    @targetName("_combine")
+    def ++(other: Column[T]): MultiColumn[T] = combine(other)
 
-    def --(other: Column[T]): MultiColumn[T] = MultiColumn[T]("-", column, other)
+    def deduct(other: Column[T]): MultiColumn[T] = MultiColumn[T]("-", column, other)
+    @targetName("_deduct")
+    def --(other: Column[T]): MultiColumn[T] = deduct(other)
 
-    def *(other: Column[T]): MultiColumn[T] = MultiColumn[T]("*", column, other)
+    def multiply(other: Column[T]): MultiColumn[T] = MultiColumn[T]("*", column, other)
+    @targetName("_multiply")
+    def *(other: Column[T]): MultiColumn[T] = multiply(other)
 
-    def /(other: Column[T]): MultiColumn[T] = MultiColumn[T]("/", column, other)
+    def smash(other: Column[T]): MultiColumn[T] = MultiColumn[T]("/", column, other)
+    @targetName("_smash")
+    def /(other: Column[T]): MultiColumn[T] = smash(other)
 
     /** List of sub query methods */
-    def ===(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
-      SubQuery[F, T]("=", buildColumnName(column), value)
+    def _equals(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("=", buildColumnName(column), value)
+    @targetName("subQueryEquals")
+    def ===(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = _equals(value)
 
-    def hoge(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
-      SubQuery[F, T](">=", buildColumnName(column), value)
+    def orMore(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T](">=", buildColumnName(column), value)
+    @targetName("subQueryOrMore")
+    def >=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = orMore(value)
 
-    def >(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
-      SubQuery[F, T](">", buildColumnName(column), value)
+    def over(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T](">", buildColumnName(column), value)
+    @targetName("subQueryOver")
+    def >(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = over(value)
 
-    def <=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
-      SubQuery[F, T]("<=", buildColumnName(column), value)
+    def lessThanOrEqual(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<=", buildColumnName(column), value)
+    @targetName("subQueryLessThanOrEqual")
+    def <=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = lessThanOrEqual(value)
 
-    def <(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
-      SubQuery[F, T]("<", buildColumnName(column), value)
+    def lessThan(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<", buildColumnName(column), value)
+    @targetName("subQueryLessThan")
+    def <(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = lessThan(value)
 
-    def <>(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
-      SubQuery[F, T]("<>", buildColumnName(column), value)
+    def notEqual(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<>", buildColumnName(column), value)
+    @targetName("subQueryNotEqual")
+    def <>(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = notEqual(value)
 
     def IN(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] =
       SubQuery[F, T]("IN", buildColumnName(column), value)
 
     /** List of join query methods */
-    def ===(other: Column[?]): ExpressionSyntax[F] = JoinQuery("=", column, other)
+    def _equals(other: Column[?]): ExpressionSyntax[F] = JoinQuery("=", column, other)
+    @targetName("joinEqual")
+    def ===(other: Column[?]): ExpressionSyntax[F] = _equals(other)
 
-    def >=(other: Column[?]): ExpressionSyntax[F] = JoinQuery(">=", column, other)
+    def orMore(other: Column[?]): ExpressionSyntax[F] = JoinQuery(">=", column, other)
+    @targetName("joinOrMore")
+    def >=(other: Column[?]): ExpressionSyntax[F] = orMore(other)
 
-    def >(other: Column[?]): ExpressionSyntax[F] = JoinQuery(">", column, other)
+    def over(other: Column[?]): ExpressionSyntax[F] = JoinQuery(">", column, other)
+    @targetName("joinOver")
+    def >(other: Column[?]): ExpressionSyntax[F] = over(other)
 
-    def <=(other: Column[?]): ExpressionSyntax[F] = JoinQuery("<=", column, other)
+    def lessThanOrEqual(other: Column[?]): ExpressionSyntax[F] = JoinQuery("<=", column, other)
+    @targetName("joinLessThanOrEqual")
+    def <=(other: Column[?]): ExpressionSyntax[F] = lessThanOrEqual(other)
 
-    def <(other: Column[?]): ExpressionSyntax[F] = JoinQuery("<", column, other)
+    def lessThan(other: Column[?]): ExpressionSyntax[F] = JoinQuery("<", column, other)
+    @targetName("joinLessThan")
+    def <(other: Column[?]): ExpressionSyntax[F] = lessThan(other)
 
-    def <>(other: Column[?]): ExpressionSyntax[F] = JoinQuery("<>", column, other)
+    def notEqual(other: Column[?]): ExpressionSyntax[F] = JoinQuery("<>", column, other)
+    @targetName("joinNotEqual")
+    def <>(other: Column[?]): ExpressionSyntax[F] = notEqual(other)
 
+    @targetName("join!Equal")
     def !==(other: Column[?]): ExpressionSyntax[F] = JoinQuery("!=", column, other)
 
   implicit class MultiColumnStatement[T](column: MultiColumn[T])(using Parameter[F, Extract[T]]):
 
-    def ===(value: Extract[T]): MatchCondition[F, T] = MatchCondition[F, T](column.label, false, value)
+    def _equals(value: Extract[T]): MatchCondition[F, T] = MatchCondition[F, T](column.label, false, value)
+    @targetName("multiColumnEquals")
+    def ===(value: Extract[T]): MatchCondition[F, T] = _equals(value)
 
-    def >=(value: Extract[T]): OrMore[F, T] = OrMore[F, T](column.label, false, value)
+    def orMore(value: Extract[T]): OrMore[F, T] = OrMore[F, T](column.label, false, value)
+    @targetName("multiColumnOrMore")
+    def >=(value: Extract[T]): OrMore[F, T] = orMore(value)
 
-    def >(value: Extract[T]): Over[F, T] = Over[F, T](column.label, false, value)
+    def over(value: Extract[T]): Over[F, T] = Over[F, T](column.label, false, value)
+    @targetName("multiColumnOver")
+    def >(value: Extract[T]): Over[F, T] = over(value)
 
-    def <=(value: Extract[T]): LessThanOrEqualTo[F, T] = LessThanOrEqualTo[F, T](column.label, false, value)
+    def lessThanOrEqual(value: Extract[T]): LessThanOrEqualTo[F, T] = LessThanOrEqualTo[F, T](column.label, false, value)
+    @targetName("multiColumnLessThanOrEqual")
+    def <=(value: Extract[T]): LessThanOrEqualTo[F, T] = lessThanOrEqual(value)
 
-    def <(value: Extract[T]): LessThan[F, T] = LessThan[F, T](column.label, false, value)
+    def lessThan(value: Extract[T]): LessThan[F, T] = LessThan[F, T](column.label, false, value)
+    @targetName("multiColumnLessThan")
+    def <(value: Extract[T]): LessThan[F, T] = lessThan(value)
 
-    def <>(value: Extract[T]): NotEqual[F, T] = NotEqual[F, T]("<>", column.label, false, value)
+    def notEqual(value: Extract[T]): NotEqual[F, T] = NotEqual[F, T]("<>", column.label, false, value)
+    @targetName("multiColumnNotEqual")
+    def <>(value: Extract[T]): NotEqual[F, T] = notEqual(value)
 
+    @targetName("multiColumn!Equal")
     def !==(value: Extract[T]): NotEqual[F, T] = NotEqual[F, T]("!=", column.label, false, value)
 
     def IS[A <: "TRUE" | "FALSE" | "UNKNOWN" | "NULL"](value: A): Is[F, A] = Is[F, A](column.label, false, value)
 
-    def <=>(value: Extract[T]): NullSafeEqual[F, T] = NullSafeEqual[F, T](column.label, false, value)
+    def nullSafeEqual(value: Extract[T]): NullSafeEqual[F, T] = NullSafeEqual[F, T](column.label, false, value)
+    @targetName("multiColumnNullSafeEqual")
+    def <=>(value: Extract[T]): NullSafeEqual[F, T] = nullSafeEqual(value)
 
     def IN(value: Extract[T]*): In[F, T] = In[F, T](column.label, false, value: _*)
 
@@ -176,31 +244,53 @@ trait ColumnSyntax[F[_]]:
 
     def REGEXP(value: Extract[T]): Regexp[F, T] = Regexp[F, T](column.label, false, value)
 
-    def <<(value: Extract[T]): LeftShift[F, T] = LeftShift[F, T](column.label, false, value)
+    def leftShift(value: Extract[T]): LeftShift[F, T] = LeftShift[F, T](column.label, false, value)
+    @targetName("multiColumnLeftShift")
+    def <<(value: Extract[T]): LeftShift[F, T] = leftShift(value)
 
-    def >>(value: Extract[T]): RightShift[F, T] = RightShift[F, T](column.label, false, value)
+    def rightShift(value: Extract[T]): RightShift[F, T] = RightShift[F, T](column.label, false, value)
+    @targetName("multiColumnRightShift")
+    def >>(value: Extract[T]): RightShift[F, T] = rightShift(value)
 
     def DIV(cond: Extract[T], result: Extract[T]): Div[F, T] = Div[F, T](column.label, false, cond, result)
 
     def MOD(cond: Extract[T], result: Extract[T]): Mod[F, T] = Mod[F, T]("MOD", column.label, false, cond, result)
 
-    def %(cond: Extract[T], result: Extract[T]): Mod[F, T] = Mod[F, T]("%", column.label, false, cond, result)
+    def mod(cond: Extract[T], result: Extract[T]): Mod[F, T] = Mod[F, T]("%", column.label, false, cond, result)
+    @targetName("multiColumnMOD")
+    def %(cond: Extract[T], result: Extract[T]): Mod[F, T] = mod(cond, result)
 
-    def ^(value: Extract[T]): BitXOR[F, T] = BitXOR[F, T](column.label, false, value)
+    def bitXOR(value: Extract[T]): BitXOR[F, T] = BitXOR[F, T](column.label, false, value)
+    @targetName("multiColumnBitXOR")
+    def ^(value: Extract[T]): BitXOR[F, T] = bitXOR(value)
 
-    def ~(value: Extract[T]): BitFlip[F, T] = BitFlip[F, T](column.label, false, value)
+    def bitFlip(value: Extract[T]): BitFlip[F, T] = BitFlip[F, T](column.label, false, value)
+    @targetName("multiColumnBitFlip")
+    def ~(value: Extract[T]): BitFlip[F, T] = bitFlip(value)
 
     /** List of sub query methods */
-    def ===(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("=", column.label, value)
+    def _equals(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("=", column.label, value)
+    @targetName("subQueryMultiColumnEquals")
+    def ===(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = _equals(value)
 
-    def >=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T](">=", column.label, value)
+    def orMore(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T](">=", column.label, value)
+    @targetName("subQueryMultiColumnOrMore")
+    def >=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = orMore(value)
 
-    def >(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T](">", column.label, value)
+    def over(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T](">", column.label, value)
+    @targetName("subQueryMultiColumnOver")
+    def >(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = over(value)
 
-    def <=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<=", column.label, value)
+    def lessThanOrEqual(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<=", column.label, value)
+    @targetName("subQueryMultiColumnLessThanOrEqual")
+    def <=(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = lessThanOrEqual(value)
 
-    def <(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<", column.label, value)
+    def lessThan(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<", column.label, value)
+    @targetName("subQueryMultiColumnLessThan")
+    def <(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = lessThan(value)
 
-    def <>(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<>", column.label, value)
+    def notEqual(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("<>", column.label, value)
+    @targetName("subQueryMultiColumnNotEqual")
+    def <>(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = notEqual(value)
 
     def IN(value: Query[F, ColumnReader[F, T] & Column[T]]): SubQuery[F, T] = SubQuery[F, T]("IN", column.label, value)
