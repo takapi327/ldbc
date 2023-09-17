85a6d8bfa35fef5205acee9734f16a6f05881bb7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-09-17 20:15:38 +0900

    Update cats-effect 3.5.0 -> 3.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ import sbt._
 object Dependencies {
 
   val cats = "org.typelevel" %% "cats-core" % "2.10.0"
-  val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.0"
+  val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.1"
 
   val schemaspy = "org.schemaspy" % "schemaspy" % "6.2.2"
 
