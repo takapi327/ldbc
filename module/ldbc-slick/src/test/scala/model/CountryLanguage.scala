4da0eac2e137db60092145873167066caa0a7677
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-09-18 17:08:55 +0900

    Create model for test

---
+++ module/ldbc-slick/src/test/scala/model/CountryLanguage.scala
@@ -0,0 +1,38 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package model
+
+import ldbc.core.model.*
+import ldbc.slick.jdbc.MySQLProfile.api.*
+
+case class CountryLanguage(
+                            countryCode: String,
+                            language:    String,
+                            isOfficial:  CountryLanguage.IsOfficial,
+                            percentage:  BigDecimal
+                          )
+
+object CountryLanguage:
+
+  enum IsOfficial extends Enum:
+    case T, F
+  object IsOfficial extends EnumDataType[IsOfficial]
+
+  given BaseColumnType[IsOfficial] = MappedColumnType.base[IsOfficial, String](
+    { _.toString },
+    { str => IsOfficial.valueOf(str) }
+  )
+
+  val table = Table[CountryLanguage]("countrylanguage")(
+    column("CountryCode", CHAR(3).DEFAULT("")),
+    column("Language", CHAR(30).DEFAULT("")),
+    column("IsOfficial", ENUM(using IsOfficial).DEFAULT(IsOfficial.F)),
+    column("Percentage", DECIMAL(4, 1).DEFAULT(0.0))
+  )
+    .keySet(v => PRIMARY_KEY(v.countryCode, v.language))
+    .keySet(v => INDEX_KEY(v.countryCode))
+    .keySet(v =>
+      CONSTRAINT("countryLanguage_ibfk_1", FOREIGN_KEY(v.countryCode, REFERENCE(Country.table, Country.table.code)))
+    )
