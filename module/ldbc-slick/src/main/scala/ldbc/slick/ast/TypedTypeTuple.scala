7f692d81a1b56e81b9edf8fb54ccc5b3eb34b28b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-09-18 16:35:49 +0900

    Create TypedTypeTuple

---
+++ module/ldbc-slick/src/main/scala/ldbc/slick/ast/TypedTypeTuple.scala
@@ -0,0 +1,24 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.slick.ast
+
+import slick.ast.TypedType
+
+type TypedTypeTuple[T <: Tuple] <: Tuple = T match
+  case EmptyTuple => EmptyTuple
+  case h *: ts => TypedType[h] *: TypedTypeTuple[ts]
+
+object TypedTypeTuple:
+
+  inline def infer[T]: TypedType[T] =
+    scala.compiletime.summonFrom[TypedType[T]] {
+      case typedType: TypedType[T] => typedType
+      case _ => scala.compiletime.error("error")
+    }
+
+  inline def fold[T <: Tuple]: TypedTypeTuple[T] =
+    inline scala.compiletime.erasedValue[T] match
+      case _: EmptyTuple => EmptyTuple
+      case _: (h *: ts) => infer[h] *: fold[ts]
