8b9f81be9a6577c233797a7867845c688d076ad0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-09-20 00:08:43 +0900

    Fix TableQueryTest

--- module/ldbc-slick/src/test/scala/ldbc/slick/lifted/TableQueryTest.scala
+++ module/ldbc-slick/src/test/scala/ldbc/slick/lifted/TableQueryTest.scala
@@ -4,108 +4,128 @@
 
 package ldbc.slick.lifted
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
-import com.mysql.cj.jdbc.MysqlDataSource
-
 import org.scalatest.flatspec.AnyFlatSpec
 
-import ldbc.core.model.*
 import ldbc.slick.jdbc.MySQLProfile.api.*
 
-case class Country(
-  code:           String,
-  name:           String,
-  continent:      Country.Continent,
-  region:         String,
-  surfaceArea:    BigDecimal,
-  indepYear:      Option[Short],
-  population:     Int,
-  lifeExpectancy: Option[BigDecimal],
-  gnp:            Option[BigDecimal],
-  gnpOld:         Option[BigDecimal],
-  localName:      String,
-  governmentForm: String,
-  headOfState:    Option[String],
-  capital:        Option[Int],
-  code2:          String
-)
-
-object Country:
-
-  enum Continent(val value: String) extends Enum:
-    case Asia          extends Continent("Asia")
-    case Europe        extends Continent("Europe")
-    case North_America extends Continent("North America")
-    case Africa        extends Continent("Africa")
-    case Oceania       extends Continent("Oceania")
-    case Antarctica    extends Continent("Antarctica")
-    case South_America extends Continent("South America")
-
-    override def toString: String = value
-  object Continent extends EnumDataType[Continent]
-
-  given BaseColumnType[Continent] = MappedColumnType.base[Continent, String](
-    { continent => continent.value },
-    { str => Continent.valueOf(str.replace(" ", "_")) }
-  )
-
-  val table = Table[Country]("country")(
-    column("Code", CHAR(3).DEFAULT(""), PRIMARY_KEY),
-    column("Name", CHAR(52).DEFAULT("")),
-    column("Continent", ENUM(using Continent).DEFAULT(Continent.Asia)),
-    column("Region", CHAR(26).DEFAULT("")),
-    column("SurfaceArea", DECIMAL(10, 2).DEFAULT(0.00)),
-    column("IndepYear", SMALLINT.DEFAULT(None)),
-    column("Population", INT.DEFAULT(0)),
-    column("LifeExpectancy", DECIMAL(3, 1).DEFAULT(None)),
-    column("GNP", DECIMAL(10, 2).DEFAULT(None)),
-    column("GNPOld", DECIMAL(10, 2).DEFAULT(None)),
-    column("LocalName", CHAR(45).DEFAULT("")),
-    column("GovernmentForm", CHAR(45).DEFAULT("")),
-    column("HeadOfState", CHAR(60).DEFAULT(None)),
-    column("Capital", INT.DEFAULT(None)),
-    column("Code2", CHAR(2).DEFAULT(""))
-  )
+import model.{ Country, City, CountryLanguage }
 
 class TableQueryTest extends AnyFlatSpec:
 
-  private val dataSource = new MysqlDataSource()
-  dataSource.setServerName("127.0.0.1")
-  dataSource.setPortNumber(13306)
-  dataSource.setDatabaseName("world")
-  dataSource.setUser("ldbc")
-  dataSource.setPassword("password")
-
-  private val table = SlickTableQuery[Country](Country.table)
-  private val db    = Database.forDataSource(dataSource, None)
-
-  it should "TableQuery Test" in {
-    val result = Await.result(
-      db.run(table.filter(_.population > 126713999).filter(_.population < 126714001).result.headOption),
-      Duration.Inf
-    )
-    db.close
+  it should "The select query statement generated from Table is equal to the specified query statement." in {
     assert(
-      result === Some(
-        Country(
-          "JPN",
-          "Japan",
-          Country.Continent.Asia,
-          "Eastern Asia",
-          BigDecimal.decimal(377829.00),
-          Some(-660),
-          126714000,
-          Some(BigDecimal.decimal(80.7)),
-          Some(BigDecimal.decimal(3787042.00)),
-          Some(BigDecimal.decimal(4192638.00)),
-          "Nihon/Nippon",
-          "Constitutional Monarchy",
-          Some("Akihito"),
-          Some(1532),
-          "JP"
-        )
-      )
+      TableQueryTest.countryQuery1 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country`"
     )
+    assert(
+      TableQueryTest.countryQuery2 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country`"
+    )
+    assert(
+      TableQueryTest.countryQuery3 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` where `Code` = 'JPN'"
+    )
+    assert(
+      TableQueryTest.countryQuery4 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` where (`Population` > 126713999) and (`Population` < 126714001)"
+    )
+    assert(
+      TableQueryTest.countryQuery5 === "select `Code` from `country`"
+    )
+    assert(
+      TableQueryTest.countryQuery6 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` limit 10,5"
+    )
+    assert(
+      TableQueryTest.countryQuery7 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` order by isnull(`IndepYear`) desc,`IndepYear` desc"
+    )
+    assert(
+      TableQueryTest.countryQuery8 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` where (`Code` = 'JPN') or (`Code` = 'JOR')"
+    )
+    assert(
+      TableQueryTest.countryQuery9 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` where `Code` = 'JPN'"
+    )
+    assert(
+      TableQueryTest.countryQuery10 === "select `Code`, `Name`, `Continent`, `Region`, `SurfaceArea`, `IndepYear`, `Population`, `LifeExpectancy`, `GNP`, `GNPOld`, `LocalName`, `GovernmentForm`, `HeadOfState`, `Capital`, `Code2` from `country` where `Code` = 'JPN'"
+    )
+    assert(TableQueryTest.cityQuery1 === "select min(`Population`) from `city`")
+    assert(TableQueryTest.cityQuery2 === "select max(`Population`) from `city`")
+    assert(TableQueryTest.cityQuery3 === "select sum(`Population`) from `city`")
+    assert(TableQueryTest.cityQuery4 === "select avg(`Population`) from `city`")
+    assert(TableQueryTest.cityQuery5 === "select count(1) from `city`")
+    assert(TableQueryTest.cityQuery6 === "select exists(select `District`, `CountryCode`, `Name`, `ID`, `Population` from `city`)")
+    assert(
+      TableQueryTest.cityQuery7 === "(select `Population` as x2 from `city`) union (select `Population` as x2 from `city`)"
+    )
+
+    //assert(
+    //  TableQueryTest.cityQuery1 === "select x2.`Name`, x3.`Name` from `country` x2, `city` x3"
+    //)
+    //assert(
+    //  TableQueryTest.cityQuery2 === "select x2.`Name`, x3.`Name` from `country` x2, `city` x3 where x2.`Code` = x3.`CountryCode`"
+    //)
+    //assert(
+    //  TableQueryTest.cityQuery3 === "select x2.`Name`, x3.`Name` from `country` x2, `city` x3 where x2.`Code` = x3.`CountryCode`"
+    //)
   }
+
+  it should "The insert query statement generated from Table is equal to the specified query statement." in {
+    assert(TableQueryTest.countryLanguageQuery1 === "insert into `countrylanguage` (`CountryCode`,`Language`,`IsOfficial`,`Percentage`)  values (?,?,?,?)")
+    assert(TableQueryTest.countryLanguageQuery2 === "insert into `countrylanguage` (`CountryCode`,`Language`,`IsOfficial`,`Percentage`)  values (?,?,?,?)")
+    assert(TableQueryTest.countryLanguageQuery3 === "insert into `countrylanguage` (`CountryCode`,`Language`)  values (?,?)")
+    assert(TableQueryTest.cityQuery8 === "insert into `city` (`ID`,`Name`,`CountryCode`,`District`,`Population`)  values (?,?,?,?,?)")
+  }
+
+  it should "The update query statement generated from Table is equal to the specified query statement." in {
+    assert(TableQueryTest.countryLanguageQuery4 === "update `countrylanguage` set `CountryCode` = ?, `Language` = ?")
+    assert(TableQueryTest.countryLanguageQuery5 === "update `countrylanguage` set `CountryCode` = ?, `Language` = ? where `countrylanguage`.`CountryCode` = 'JPN'")
+    //assert(TableQueryTest.countryLanguageQuery6 === "")
+  }
+
+object TableQueryTest:
+
+  private val countries = SlickTableQuery[Country](Country.table)
+  private val cities = SlickTableQuery[City](City.table)
+  private val countryLanguages = SlickTableQuery[CountryLanguage](CountryLanguage.table)
+
+  val countryQuery1 = countries.result.headOption.statements.head
+  val countryQuery2 = countries.result.statements.head
+  val countryQuery3 = countries.filter(_.code === "JPN").result.headOption.statements.head
+  val countryQuery4 = countries.filter(_.population > 126713999).filter(_.population < 126714001).result.headOption.statements.head
+  val countryQuery5 = countries.map(_.code).result.headOption.statements.head
+  val countryQuery6 = countries.drop(10).take(5).result.headOption.statements.head
+  val countryQuery7 = countries.sortBy(_.indepYear.desc.nullsFirst).result.headOption.statements.head
+  val countryQuery8 = countries.filter { country =>
+    List(
+      Some("JPN").map(country.code === _),
+      Some("JOR").map(country.code === _),
+      None,
+    )
+      .collect {
+        case Some(criteria)  => criteria
+      }
+      .reduceLeftOption(_ || _)
+      .getOrElse(true: Rep[Boolean])
+  }.result.statements.head
+  val none: Option[String] = None
+  val countryQuery9 = countries.filterOpt(Some("JPN"))(_.code === _).filterOpt(none)(_.code === _).result.statements.head
+  val countryQuery10 = countries.filterIf(true)(_.code === "JPN").filterIf(false)(_.code === "JOR").result.statements.head
+
+  val cityQuery1 = cities.map(_.population).min.result.statements.head
+  val cityQuery2 = cities.map(_.population).max.result.statements.head
+  val cityQuery3 = cities.map(_.population).sum.result.statements.head
+  val cityQuery4 = cities.map(_.population).avg.result.statements.head
+  val cityQuery5 = cities.length.result.statements.head
+  val cityQuery6 = cities.exists.result.statements.head
+  val cityQuery7 = (cities.map(_.population) union cities.map(_.population)).result.statements.head
+  val cityQuery8 = ((cities returning cities.map(_.id) into ((ciry, id) => ciry.copy(id = id))) += City(0, "", "", "", 0)).statements.head
+
+  //val cityQuery1 = (countries join cities).map((country, city) => (country.name, city.name)).result.headOption.statements.head
+  //val cityQuery2 = (countries join cities on (_.code === _.countryCode)).map((country, city) => (country.name, city.name)).result.headOption.statements.head
+  //val cityQuery3 = (countries joinLeft cities on (_.code === _.countryCode)).map((country, city) => (country.name, city.map(_.name))).result.headOption.statements.head
+  //val cityQuery4 = (countries joinRight cities on (_.code === _.countryCode)).map((country, city) => (country.map(_.name), city.name)).result.headOption.statements.head
+
+  val countryLanguageQuery1 = (countryLanguages += CountryLanguage("JPN", "", CountryLanguage.IsOfficial.T, BigDecimal.decimal(2.2))).statements.head
+  val countryLanguageQuery2 = (countryLanguages ++= Seq(
+    CountryLanguage("JPN", "", CountryLanguage.IsOfficial.T, BigDecimal.decimal(2.2)),
+    CountryLanguage("JPN", "", CountryLanguage.IsOfficial.T, BigDecimal.decimal(2.2))
+  )).statements.head
+  val countryLanguageQuery3 = (countryLanguages.map(c => (c.countryCode, c.language)) += ("JPN", "")).statements.head
+  val countryLanguageQuery4 = countryLanguages.map(c => (c.countryCode, c.language)).update(("JPN", "Update")).statements.head
+  val countryLanguageQuery5 = countryLanguages.filter(_.countryCode === "JPN").map(c => (c.countryCode, c.language)).update(("JPN", "Update")).statements.head
+  //val countryLanguageQuery6 = countryLanguages.insertOrUpdate(CountryLanguage("JPN", "", CountryLanguage.IsOfficial.T, BigDecimal.decimal(2.2))).statements.head
