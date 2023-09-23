cb20a6a95034e0ad353ea18342d6f343dd2c4f76
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-09-23 21:52:13 +0900

    Fix Parameter

--- module/ldbc-sql/src/main/scala/ldbc/sql/Parameter.scala
+++ module/ldbc-sql/src/main/scala/ldbc/sql/Parameter.scala
@@ -128,9 +128,10 @@ object Parameter:
     override def bind(statement: PreparedStatement[F], index: Int, value: Enum): F[Unit] =
       statement.setString(index, value.toString)
 
-  type MapToTuple[F[_], T <: Tuple] <: Tuple = T match
-    case EmptyTuple => EmptyTuple
-    case h *: t     => Parameter[F, h] *: MapToTuple[F, t]
+  type MapToTuple[F[_], T] <: Tuple = T match
+    case EmptyTuple      => EmptyTuple
+    case h *: EmptyTuple => Parameter[F, h] *: EmptyTuple
+    case h *: t          => Parameter[F, h] *: MapToTuple[F, t]
 
   inline def infer[F[_], T]: Parameter[F, T] =
     summonFrom[Parameter[F, T]] {
@@ -138,7 +139,8 @@ object Parameter:
       case _                          => error("Parameter cannot be inferred")
     }
 
-  inline def fold[F[_], T <: Tuple]: MapToTuple[F, T] =
+  inline def fold[F[_], T]: MapToTuple[F, T] =
     inline erasedValue[T] match
       case _: EmptyTuple => EmptyTuple
+      case _: (h *: EmptyTuple)   => infer[F, h] *: EmptyTuple
       case _: (h *: t)   => infer[F, h] *: fold[F, t]
