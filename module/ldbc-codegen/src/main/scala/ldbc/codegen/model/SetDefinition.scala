797df680d7fe60c81f96d1481d490b550e0deea1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-10-21 19:11:23 +0900

    Create SetDefinition

---
+++ module/ldbc-codegen/src/main/scala/ldbc/codegen/model/SetDefinition.scala
@@ -0,0 +1,21 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.codegen.model
+
+/**
+ * A model to hold the results of parsing the SET syntax for variable assignments.
+ *
+ * @param variable
+ *   Definition Variable Name
+ * @param expr
+ *   Variable value
+ */
+case class SetDefinition(
+  variable: String,
+  expr: String
+):
+
+  /** SET syntax for variable assignment */
+  val statement: String = s"SET $variable = $expr"
