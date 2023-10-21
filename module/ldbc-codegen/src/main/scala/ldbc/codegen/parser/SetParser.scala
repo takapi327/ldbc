9c63c9d43d11138e044ee0299e0b853641ac8f88
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-10-21 19:11:34 +0900

    Fix SetParser

--- module/ldbc-codegen/src/main/scala/ldbc/codegen/parser/SetParser.scala
+++ module/ldbc-codegen/src/main/scala/ldbc/codegen/parser/SetParser.scala
@@ -4,6 +4,8 @@
 
 package ldbc.codegen.parser
 
+import ldbc.codegen.model.SetDefinition
+
 /** Parser for parsing SET definitions.
  */
 trait SetParser extends SqlParser:
@@ -20,55 +22,55 @@ trait SetParser extends SqlParser:
 
   private def expr: Parser[String] = opt(global | persist | session | local | userVariable) ~> opt("'") ~> specialChars <~ opt("'")
 
-  private def globalVariableStatement: Parser[String] =
+  private def globalVariableStatement: Parser[SetDefinition] =
     customError(
       global ~> ident ~ "=" ~ expr ^^ {
-        case variable ~ _ ~ expr => expr
+        case variable ~ _ ~ expr => SetDefinition("GLOBAL", expr)
       },
       failureMessage("set global variable", "{GLOBAL | @@GLOBAL.} system_var_name = expr")
     )
 
-  private def persistVariableStatement: Parser[String] =
+  private def persistVariableStatement: Parser[SetDefinition] =
     customError(
       persist ~> ident ~ "=" ~ expr ^^ {
-        case variable ~ _ ~ expr => expr
+        case variable ~ _ ~ expr => SetDefinition("PERSIST", expr)
       },
       failureMessage("set persist variable", "{PERSIST | @@PERSIST.} system_var_name = expr")
     )
 
-  private def persistOnlyVariableStatement: Parser[String] =
+  private def persistOnlyVariableStatement: Parser[SetDefinition] =
     customError(
       persistOnly ~> ident ~ "=" ~ expr ^^ {
-        case variable ~ _ ~ expr => expr
+        case variable ~ _ ~ expr => SetDefinition("PERSIST_ONLY", expr)
       },
       failureMessage("set persist_only variable", "{PERSIST_ONLY | @@PERSIST_ONLY.} system_var_name = expr")
     )
 
-  private def sessionVariableStatement: Parser[String] =
+  private def sessionVariableStatement: Parser[SetDefinition] =
     customError(
       session ~> ident ~ "=" ~ expr ^^ {
-        case variable ~ _ ~ expr => expr
+        case variable ~ _ ~ expr => SetDefinition("SESSION", expr)
       },
       failureMessage("set session variable", "[SESSION | @@SESSION. | @@] system_var_name = expr")
     )
 
-  private def localVariableStatement: Parser[String] =
+  private def localVariableStatement: Parser[SetDefinition] =
     customError(
       local ~> ident ~ "=" ~ expr ^^ {
-        case variable ~ _ ~ expr => expr
+        case variable ~ _ ~ expr => SetDefinition("LOCAL", expr)
       },
       failureMessage("set local variable", "[LOCAL | @@LOCAL.] system_var_name = expr")
     )
 
-  private def variableStatement: Parser[String] =
+  private def variableStatement: Parser[SetDefinition] =
     customError(
-      opt("@") ~> ident ~ "=" ~ expr ^^ {
-        case variable ~ _ ~ expr => expr
+      opt("@") ~ ident ~ "=" ~ expr ^^ {
+        case symbol ~ variable ~ _ ~ expr => SetDefinition(symbol.getOrElse("") + variable, expr)
       },
       failureMessage("set variable", "user_var_name | param_name | local_var_name")
     )
 
-  protected def setStatement: Parser[List[String]] =
+  protected def setStatement: Parser[List[SetDefinition]] =
     customError(
       caseSensitivity("set") ~> rep1sep(globalVariableStatement | persistVariableStatement | persistOnlyVariableStatement | sessionVariableStatement | localVariableStatement | variableStatement, ",") <~ ";",
       failureMessage("set", "SET variable = expr [, variable = expr] ...")
