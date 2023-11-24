99cdddf1f9656cb26704a0f4aa291709d4dad5cd
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-11-24 18:59:43 +0900

    Create ldbc table CompileCreation Benchmark

---
+++ benchmark/src/main/scala/benchmark/ldbc/CompileCreation.scala
@@ -0,0 +1,41 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package benchmark.ldbc
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations.*
+
+import benchmark.Compiler
+
+@BenchmarkMode(Array(Mode.SingleShotTime))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+class CompileCreation:
+
+  @Param(Array("1", "5", "10", "20", "25"))
+  var size: Int = 0
+
+  var source: String = ""
+
+  var compiler: Compiler = null
+
+  @Setup(Level.Iteration)
+  def setup(): Unit =
+    compiler = new Compiler
+
+    val columns = (1 to size).map(i => s"column(\"c$i\", INT)").mkString(",\n")
+
+    source =
+      s"""
+         |import ldbc.core.*
+         |import benchmark.ldbc.Table$size
+         |val table = Table[Table$size](\"table$size\")(
+         |  $columns
+         |)
+         |""".stripMargin
+
+  @Benchmark
+  def createTableN = compiler.compile(source)
