7b7e8ba828561a745f23bfe87e86b4bfe6fbe227
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-11-24 20:26:03 +0900

    Create Model

---
+++ benchmark/src/main/scala/benchmark/caseclass/Model.scala
@@ -0,0 +1,33 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package benchmark.caseclass
+
+case class Model(
+  p1: Int,
+  p2: Int,
+  p3: Int,
+  p4: Int,
+  p5: Int,
+  p6: Int,
+  p7: Int,
+  p8: Int,
+  p9: Int,
+  p10: Int,
+  p11: Int,
+  p12: Int,
+  p13: Int,
+  p14: Int,
+  p15: Int,
+  p16: Int,
+  p17: Int,
+  p18: Int,
+  p19: Int,
+  p20: Int,
+  p21: Int,
+  p22: Int,
+  p23: Int,
+  p24: Int,
+  p25: Int,
+)
