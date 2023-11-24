ac528fb6b265a80fe4780626be6d1731c157c1d6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-11-24 18:59:30 +0900

    Create Table model

---
+++ benchmark/src/main/scala/benchmark/ldbc/Table.scala
@@ -0,0 +1,81 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package benchmark.ldbc
+
+case class Table1(
+  c1: Int
+)
+
+case class Table5(
+  c1: Int,
+  c2: Int,
+  c3: Int,
+  c4: Int,
+  c5: Int,
+)
+
+case class Table10(
+  c1: Int,
+  c2: Int,
+  c3: Int,
+  c4: Int,
+  c5: Int,
+  c6: Int,
+  c7: Int,
+  c8: Int,
+  c9: Int,
+  c10: Int,
+)
+
+case class Table20(
+  c1: Int,
+  c2: Int,
+  c3: Int,
+  c4: Int,
+  c5: Int,
+  c6: Int,
+  c7: Int,
+  c8: Int,
+  c9: Int,
+  c10: Int,
+  c11: Int,
+  c12: Int,
+  c13: Int,
+  c14: Int,
+  c15: Int,
+  c16: Int,
+  c17: Int,
+  c18: Int,
+  c19: Int,
+  c20: Int,
+)
+
+case class Table25(
+  c1: Int,
+  c2: Int,
+  c3: Int,
+  c4: Int,
+  c5: Int,
+  c6: Int,
+  c7: Int,
+  c8: Int,
+  c9: Int,
+  c10: Int,
+  c11: Int,
+  c12: Int,
+  c13: Int,
+  c14: Int,
+  c15: Int,
+  c16: Int,
+  c17: Int,
+  c18: Int,
+  c19: Int,
+  c20: Int,
+  c21: Int,
+  c22: Int,
+  c23: Int,
+  c24: Int,
+  c25: Int,
+)
