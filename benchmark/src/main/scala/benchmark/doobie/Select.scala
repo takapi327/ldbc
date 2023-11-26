846f72e4a851966824f80b8b161c99e75f8689bc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-11-26 15:46:57 +0900

    Fix doobie select Benchmark

--- benchmark/src/main/scala/benchmark/doobie/Select.scala
+++ benchmark/src/main/scala/benchmark/doobie/Select.scala
@@ -16,37 +16,29 @@ import doobie.implicits.*
 
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
 class Select:
 
-  def selectN(num: Int): Int =
-    sql"SELECT ID, Name, CountryCode FROM city LIMIT $num"
-      .query[(String, String, String)]
+  @volatile
+  var xa: Transactor[IO] = _
+
+  @Setup
+  def setup(): Unit =
+    xa = Transactor.fromDriverManager[IO](
+      "com.mysql.cj.jdbc.Driver",
+      "jdbc:mysql://127.0.0.1:13306/world",
+      "ldbc", 
+      "password",
+      None
+    )
+
+  @Param(Array("10", "100", "1000", "2000", "4000"))
+  var len: Int = _
+
+  @Benchmark
+  def selectN: List[(Int, String, String)] =
+    sql"SELECT ID, Name, CountryCode FROM city LIMIT $len"
+      .query[(Int, String, String)]
       .to[List]
-      .transact(Select.xa)
-      .map(_.length)
+      .transact(xa)
       .unsafeRunSync()
-
-  @Benchmark
-  @OperationsPerInvocation(10)
-  def select10: Int = selectN(10)
-
-  @Benchmark
-  @OperationsPerInvocation(100)
-  def select100: Int = selectN(100)
-
-  @Benchmark
-  @OperationsPerInvocation(1000)
-  def select1000: Int = selectN(1000)
-
-  @Benchmark
-  @OperationsPerInvocation(2000)
-  def select2000: Int = selectN(2000)
-
-  @Benchmark
-  @OperationsPerInvocation(4000)
-  def select4000: Int = selectN(4000)
-
-object Select:
-
-  @State(Scope.Benchmark)
-  val xa = Transactor.fromDriverManager[IO]("com.mysql.cj.jdbc.Driver", "jdbc:mysql://127.0.0.1:13306/world", "ldbc", "password", None)
