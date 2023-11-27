98a0d7fcc67db49b3a220aa5109076062f7eccab
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-11-28 00:50:48 +0900

    Create FactoryCompat

---
+++ module/ldbc-sql/src/main/scala/ldbc/sql/util/FactoryCompat.scala
@@ -0,0 +1,20 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.sql.util
+
+import scala.annotation.implicitNotFound
+import scala.collection.Factory
+import scala.collection.mutable
+
+@implicitNotFound(msg = "Cannot construct a factory of type ${C} with elements of type ${A}.")
+trait FactoryCompat[-A, +C]:
+  def newBuilder: mutable.Builder[A, C]
+
+object FactoryCompat:
+  //given [A, C]: Conversion[Factory[A, C], FactoryCompat[A, C]] =
+  //  factory => new FactoryCompat[A, C]:
+  //    override def newBuilder: mutable.Builder[A, C] = factory.newBuilder
+  given [A, C](using factory: Factory[A, C]): FactoryCompat[A, C] = new FactoryCompat[A, C]:
+      override def newBuilder: mutable.Builder[A, C] = factory.newBuilder
