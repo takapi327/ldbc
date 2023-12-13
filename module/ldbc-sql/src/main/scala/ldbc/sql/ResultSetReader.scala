3b945ed2942c0733ad7f0b887e08d27217a2ef2b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-12-13 23:45:37 +0900

    Fix ResultSetReader

--- module/ldbc-sql/src/main/scala/ldbc/sql/ResultSetReader.scala
+++ module/ldbc-sql/src/main/scala/ldbc/sql/ResultSetReader.scala
@@ -121,15 +121,18 @@ object ResultSetReader:
 
     override def read(resultSet: ResultSet[F], columnLabel: String): F[Option[A]] =
       for
-        result <- reader.read(resultSet, columnLabel)
         bool   <- resultSet.wasNull()
-      yield if bool then None else Some(result)
+        result <- if bool then
+          reader.read(resultSet, columnLabel).map(Some(_))
+        else Monad[F].pure(None)
+      yield result
 
     override def read(resultSet: ResultSet[F], index: Int): F[Option[A]] =
       for
-        result <- reader.read(resultSet, index)
-        bool   <- resultSet.wasNull()
-      yield if bool then None else Some(result)
+        bool <- resultSet.wasNull()
+        result <- if bool then reader.read(resultSet, index).map(Some(_))
+        else Monad[F].pure(None)
+      yield result
 
   type MapToTuple[F[_], T <: Tuple] <: Tuple = T match
     case EmptyTuple => EmptyTuple
