06dc20690b40bfd15756cf1c179f4baf35ab506c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-12-19 20:19:07 +0900

    Action sbt scalafmtAll

--- module/ldbc-dsl/src/main/scala/ldbc/dsl/DataSourceIO.scala
+++ module/ldbc-dsl/src/main/scala/ldbc/dsl/DataSourceIO.scala
@@ -1,12 +1,12 @@
 /** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
- * distributed with this source code.
- */
+  * distributed with this source code.
+  */
 
 package ldbc.dsl
 
 import cats.implicits.*
 import cats.effect.Sync
-import ldbc.sql.{Connection, DataSource}
+import ldbc.sql.{ Connection, DataSource }
 
 import java.io.PrintWriter
 
@@ -18,9 +18,9 @@ case class DataSourceIO[F[_]: Sync](dataSource: javax.sql.DataSource) extends Da
     Sync[F].blocking(dataSource.getConnection(username, password)).map(ConnectionIO(_))
 
   override def getLogWriter: F[PrintWriter] = Sync[F].blocking(dataSource.getLogWriter)
-  
+
   override def setLogWriter(out: PrintWriter): F[Unit] = Sync[F].blocking(dataSource.setLogWriter(out))
-  
+
   override def setLoginTimeout(seconds: Int): F[Unit] = Sync[F].blocking(dataSource.setLoginTimeout(seconds))
 
   override def getLoginTimeout: F[Int] = Sync[F].blocking(dataSource.getLoginTimeout)
