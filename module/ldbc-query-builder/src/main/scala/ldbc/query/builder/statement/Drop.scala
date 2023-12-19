9e7ced8986fd52a8426514a279a3e04d1f5ddf6e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-12-19 20:53:38 +0900

    Create Drop statement model

---
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/statement/Drop.scala
@@ -0,0 +1,24 @@
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+package ldbc.query.builder.statement
+import ldbc.sql.ParameterBinder
+
+/**
+ * A model for constructing Drop Table statements in MySQL.
+ * 
+ * @param tableQuery
+ *   Trait for generating SQL table information.
+ * @tparam F
+ *   The effect type
+ * @tparam P
+ *   Base trait for all products
+ */
+class Drop[F[_], P <: Product](
+  tableQuery: TableQuery[F, P]
+) extends Command[F]:
+
+  override def params: Seq[ParameterBinder[F]] = Seq.empty
+
+  override def statement: String = s"DROP TABLE ${tableQuery.table._name}"
