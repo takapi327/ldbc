eca45b2fca1db1042520fccddde98c9b76eb3c77
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-12-19 20:26:50 +0900

    Change javax DataSource -> ldbc DataSource

--- benchmark/src/main/scala/benchmark/ldbc/Insert.scala
+++ benchmark/src/main/scala/benchmark/ldbc/Insert.scala
@@ -15,6 +15,8 @@ import cats.effect.unsafe.implicits.global
 
 import ldbc.core.*
 import ldbc.query.builder.TableQuery
+import ldbc.sql.DataSource
+import ldbc.dsl.DataSourceIO
 import ldbc.dsl.io.*
 import ldbc.dsl.logging.*
 
@@ -24,7 +26,7 @@ import ldbc.dsl.logging.*
 class Insert:
 
   @volatile
-  var dataSource: MysqlDataSource = _
+  var dataSource: DataSource[IO] = _
 
   @volatile
   var noLog: LogHandler[IO] = _
@@ -37,12 +39,13 @@ class Insert:
 
   @Setup
   def setupDataSource(): Unit =
-    dataSource = new MysqlDataSource()
-    dataSource.setServerName("127.0.0.1")
-    dataSource.setPortNumber(13306)
-    dataSource.setDatabaseName("world")
-    dataSource.setUser("ldbc")
-    dataSource.setPassword("password")
+    val ds = new MysqlDataSource()
+    ds.setServerName("127.0.0.1")
+    ds.setPortNumber(13306)
+    ds.setDatabaseName("world")
+    ds.setUser("ldbc")
+    ds.setPassword("password")
+    dataSource = DataSourceIO[IO](ds)
 
     records = (1 to len).map(num => (num, s"record$num")).toList
 
