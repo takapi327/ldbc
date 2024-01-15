67b8135d809b97f6cce08d784cbf4714089b0048
Scala Steward <scala_steward@virtuslab.com>
2024-01-15 00:34:46 +0000

    Update scala-library from 2.12.18 to 2.13.12

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,11 +1,9 @@
-/**
- *  This file is part of the ldbc.
- *  For the full copyright and license information,
- *  please view the LICENSE file that was distributed with this source code.
- */
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+  * distributed with this source code.
+  */
 
 object ScalaVersions {
-  val scala2 = "2.12.18"
+  val scala2 = "2.13.12"
   val scala3 = "3.3.1"
 }
 
