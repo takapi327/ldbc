dbbdef2aa61665f7b4470de5c5176344329e04d9
takapi327 <t.takapi0327@gmail.com>
2024-01-19 18:20:58 +0900

    Action sbt scalafmtAll

--- core/src/main/scala/ldbc/core/syntax/OrderingTable.scala
+++ core/src/main/scala/ldbc/core/syntax/OrderingTable.scala
@@ -1,8 +1,6 @@
-/**
- * Copyright (c) 2023-2024 by Takahiko Tominaga
- * This software is licensed under the MIT License (MIT).
- * For more information see LICENSE or https://opensource.org/licenses/MIT
- */
+/** Copyright (c) 2023-2024 by Takahiko Tominaga This software is licensed under the MIT License (MIT). For more
+  * information see LICENSE or https://opensource.org/licenses/MIT
+  */
 
 package ldbc.core.syntax
 
