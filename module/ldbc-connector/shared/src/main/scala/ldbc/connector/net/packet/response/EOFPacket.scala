d28d54657f9bdeb494bf2e7237cd7cef65f84130
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-08 21:11:45 +0900

    Create EOFPacket

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/response/EOFPacket.scala
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package response
+
+import scodec.*
+import scodec.codecs.*
+
+/**
+ * If CLIENT_PROTOCOL_41 is enabled, the EOF packet contains a warning count and status flags.
+ * 
+ * The EOF_Packet packet may appear in places where a Protocol::LengthEncodedInteger may appear. You must check whether the packet length is less than 9 to make sure that it is a EOF_Packet packet.
+ *
+ * @param status
+ *   Type: int<1>
+ *   Name: header
+ *   Description: 0xFE EOF packet header
+ * @param warnings
+ *   Type: int<2>
+ *   Name: warnings
+ *   Description: number of warnings
+ * @param statusFlags
+ *   Type: int<2>
+ *   Name: status_flags
+ *   Description: SERVER_STATUS_flags_enum
+ */
+case class EOFPacket(
+  status:      Int,
+  warnings:    Int,
+  statusFlags: Int
+) extends GenericResponsePackets:
+
+  override def toString: String = "EOF_Packet"
+
+object EOFPacket:
+
+  val STATUS = 0xfe
+
+  val decoder: Decoder[EOFPacket] =
+    for
+      status      <- uint4
+      warnings    <- uint4
+      statusFlags <- uint4
+    yield EOFPacket(status, warnings, statusFlags)
