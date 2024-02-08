eadc6debc8be9aabb2832bd303e23ff6cef8dfff
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-09 01:52:15 +0900

    Create SSLNegotiation

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/SSLNegotiation.scala
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net
+
+import cats.*
+import cats.syntax.all.*
+
+import cats.effect.*
+
+import fs2.Chunk
+import fs2.io.net.*
+import fs2.io.net.tls.*
+
+import scodec.bits.BitVector
+
+import ldbc.connector.data.CapabilitiesFlags
+import ldbc.connector.net.packet.request.SSLRequestPacket
+
+object SSLNegotiation:
+
+  /** Parameters for `negotiateSSL`. */
+  case class Options[F[_]](
+    tlsContext:    TLSContext[F],
+    tlsParameters: TLSParameters,
+    fallbackOk:    Boolean,
+    logger:        Option[String => F[Unit]]
+  )
+
+  private def buildRequest(sequenceId: Byte, request: BitVector): BitVector =
+    val payloadSize = request.toByteArray.length
+    val header = Chunk(
+      payloadSize.toByte,
+      ((payloadSize >> 8) & 0xff).toByte,
+      ((payloadSize >> 16) & 0xff).toByte,
+      sequenceId
+    )
+    header.toBitVector ++ request
+
+  def negotiateSSL[F[_]](
+    socket:     Socket[F],
+    capabilityFlags: Seq[CapabilitiesFlags],
+    sslOptions: SSLNegotiation.Options[F],
+    sequenceIdRef: Ref[F, Byte]
+  ): Resource[F, Socket[F]] =
+    for
+      sequenceId <- Resource.eval(sequenceIdRef.get)
+      request <- Resource.pure(buildRequest(sequenceId, SSLRequestPacket(capabilityFlags).encode))
+      _ <- Resource.eval(socket.write(Chunk.byteVector(request.bytes)))
+      socket$ <- sslOptions.tlsContext
+          .clientBuilder(socket)
+          .withParameters(sslOptions.tlsParameters)
+          .withLogger(
+            sslOptions.logger.fold[TLSLogger[F]](TLSLogger.Disabled)(logger => TLSLogger.Enabled(x => logger(x)))
+          )
+          .build
+      _ <- Resource.eval(sequenceIdRef.update(sequenceId => ((sequenceId + 1) % 256).toByte))
+    yield socket$
