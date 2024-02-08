07df5f855c48294580572f18394c2569b1b91802
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-08 21:02:47 +0900

    Create GenericResponsePackets

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/response/GenericResponsePackets.scala
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package response
+
+import scala.annotation.switch
+
+import scodec.*
+import scodec.codecs.*
+
+trait GenericResponsePackets extends ResponsePacket
