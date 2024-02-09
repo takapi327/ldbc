7f1850f2a898732136d539a1c5ec3aa019ca11c0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-10 01:36:55 +0900

    Create HandshakeResponsePacket

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/request/HandshakeResponsePacket.scala
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package request
+
+import ldbc.connector.data.CapabilitiesFlags
+
+trait HandshakeResponsePacket extends RequestPacket:
+
+  def capabilitiesFlags: Seq[CapabilitiesFlags]
+  def user: String
+  def hashedPassword: Array[Byte]
+  def pluginName: String
+
+object HandshakeResponsePacket:
+  
+  def apply(
+             capabilitiesFlags: Seq[CapabilitiesFlags],
+             user:            String,
+             hashedPassword:  Array[Byte],
+             pluginName:      String
+           ): HandshakeResponsePacket =
+    if capabilitiesFlags.contains(CapabilitiesFlags.CLIENT_PROTOCOL_41) then
+      HandshakeResponse41Packet(capabilitiesFlags, user, hashedPassword, pluginName)
+    else
+      HandshakeResponse320Packet(capabilitiesFlags, user, hashedPassword, pluginName)
