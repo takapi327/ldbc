51df30c87eed6ff443a78f9a0d0578b6885a62ea
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-10 01:53:51 +0900

    Create ComQuitPacket

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/request/ComQuitPacket.scala
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package request
+
+import scodec.*
+import scodec.bits.BitVector
+
+import ldbc.connector.data.CommandId
+
+case class ComQuitPacket() extends RequestPacket:
+
+  override protected def encodeBody: Attempt[BitVector] =
+    ComQuitPacket.encoder.encode(this)
+
+  override def encode: BitVector = encodeBody.require
+
+  override def toString: String = "COM_QUIT Request"
+
+object ComQuitPacket:
+
+  val encoder: Encoder[ComQuitPacket] = Encoder(_ => Attempt.successful(BitVector(CommandId.COM_QUIT)))
