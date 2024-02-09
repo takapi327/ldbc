d9d0132e6414b9cb6986dd651ba8fc41b8e494c7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-10 01:11:21 +0900

    Rename MySQLSocket -> MySQLProtocol

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/MySQLProtocol.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/MySQLProtocol.scala
@@ -21,30 +21,31 @@ import ldbc.connector.net.*
 import ldbc.connector.net.packet.*
 import ldbc.connector.net.packet.response.InitialPacket
 
-trait MySQLSocket[F[_]] extends PacketSocket[F]:
+trait MySQLProtocol[F[_]]:
+
+  def initialPacket: InitialPacket
 
   def changeCommandPhase: F[Unit]
 
-object MySQLSocket:
+object MySQLProtocol:
 
   def apply[F[_]: Temporal: Console](
     sockets:     Resource[F, Socket[F]],
     debug:       Boolean,
     sslOptions:  Option[SSLNegotiation.Options[F]],
     readTimeout: Duration
-  ): Resource[F, MySQLSocket[F]] =
+  ): Resource[F, MySQLProtocol[F]] =
     for
       sequenceIdRef <- Resource.eval(Ref[F].of[Byte](0x01))
       socket        <- sockets
       ps            <- Resource.eval(PacketSocket[F](debug, socket, sequenceIdRef, readTimeout))
-      initialPacket <- Resource.eval(ps.receive(InitialPacket.decoder))
+      initialPacket$ <- Resource.eval(ps.receive(InitialPacket.decoder))
       socket$ <- sslOptions.fold(Resource.pure(ps))(option =>
                    SSLNegotiation
-                     .negotiateSSL(socket, initialPacket.capabilityFlags, option, sequenceIdRef)
+                     .negotiateSSL(socket, initialPacket$.capabilityFlags, option, sequenceIdRef)
                      .flatMap(ssl => Resource.eval(PacketSocket[F](debug, ssl, sequenceIdRef, readTimeout)))
                  )
     yield new MySQLSocket[F]:
-      override def receive[P <: ResponsePacket](decoder: Decoder[P]):    F[P]    = socket$.receive[P](decoder)
-      override def send(request:                         RequestPacket): F[Unit] = socket$.send(request)
+      override def initialPacket: InitialPacket = initialPacket$
       override def changeCommandPhase: F[Unit] =
         sequenceIdRef.update(_ => 0.toByte)
