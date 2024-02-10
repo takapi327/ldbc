b3a71443793f6a0e9791c99f29a8b4c015892099
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-10 21:50:03 +0900

    Action sbt scalafmtAll

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/Connection.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/Connection.scala
@@ -39,52 +39,52 @@ object Connection:
      * system defaults. Note that this is usually more work than you need to do. If your application
      * isn't running arbitrary statements then `minimal` might be more efficient.
      */
-    def full[F[_] : Monad]: Recycler[F, Connection[F]] =
+    def full[F[_]: Monad]: Recycler[F, Connection[F]] =
       Recycler.success[F, Connection[F]]
 
-  def single[F[_] : Temporal : Tracer : Network : Console](
-                                                            host: String,
-                                                            port: Int,
-                                                            user: String,
-                                                            password: Option[String] = None,
-                                                            debug: Boolean = false,
-                                                            ssl: SSL = SSL.None,
-                                                            readTimeout: Duration = Duration.Inf
-                                                          ): Resource[F, Connection[F]] =
+  def single[F[_]: Temporal: Tracer: Network: Console](
+    host:        String,
+    port:        Int,
+    user:        String,
+    password:    Option[String] = None,
+    debug:       Boolean = false,
+    ssl:         SSL = SSL.None,
+    readTimeout: Duration = Duration.Inf
+  ): Resource[F, Connection[F]] =
     singleTracer(host, port, user, password, debug, ssl, readTimeout).apply(Tracer[F])
 
-  def singleTracer[F[_] : Temporal : Network : Console](
-                                                         host: String,
-                                                         port: Int,
-                                                         user: String,
-                                                         password: Option[String] = None,
-                                                         debug: Boolean = false,
-                                                         ssl: SSL = SSL.None,
-                                                         readTimeout: Duration = Duration.Inf
-                                                       ): Tracer[F] => Resource[F, Connection[F]] =
+  def singleTracer[F[_]: Temporal: Network: Console](
+    host:        String,
+    port:        Int,
+    user:        String,
+    password:    Option[String] = None,
+    debug:       Boolean = false,
+    ssl:         SSL = SSL.None,
+    readTimeout: Duration = Duration.Inf
+  ): Tracer[F] => Resource[F, Connection[F]] =
     Kleisli((_: Tracer[F]) =>
       pooled[F](
-        host = host,
-        port = port,
-        user = user,
-        password = password,
-        max = 1,
-        debug = debug,
-        ssl = ssl,
+        host        = host,
+        port        = port,
+        user        = user,
+        password    = password,
+        max         = 1,
+        debug       = debug,
+        ssl         = ssl,
         readTimeout = readTimeout
       )
     )
       .flatMap(f => Kleisli { implicit T: Tracer[F] => f(T) })
       .run
 
-  def fromSockets[F[_] : Temporal : Tracer : Console](
-    sockets: Resource[F, Socket[F]],
-    host: String,
-    port: Int,
-    user: String,
-    password: Option[String] = None,
-    debug: Boolean = false,
-    sslOptions: Option[SSLNegotiation.Options[F]],
+  def fromSockets[F[_]: Temporal: Tracer: Console](
+    sockets:     Resource[F, Socket[F]],
+    host:        String,
+    port:        Int,
+    user:        String,
+    password:    Option[String] = None,
+    debug:       Boolean = false,
+    sslOptions:  Option[SSLNegotiation.Options[F]],
     readTimeout: Duration = Duration.Inf
   ): Resource[F, Connection[F]] =
     for
@@ -92,16 +92,16 @@ object Connection:
       _        <- Resource.eval(protocol.authenticate(user, password.getOrElse("")))
     yield new Connection[F] {}
 
-  def fromSocketGroup[F[_] : Tracer : Console](
-    socketGroup: SocketGroup[F],
-    host: String,
-    port: Int,
-    user: String,
-    password: Option[String] = None,
-    debug: Boolean = false,
+  def fromSocketGroup[F[_]: Tracer: Console](
+    socketGroup:   SocketGroup[F],
+    host:          String,
+    port:          Int,
+    user:          String,
+    password:      Option[String] = None,
+    debug:         Boolean = false,
     socketOptions: List[SocketOption],
-    sslOptions: Option[SSLNegotiation.Options[F]],
-    readTimeout: Duration = Duration.Inf
+    sslOptions:    Option[SSLNegotiation.Options[F]],
+    readTimeout:   Duration = Duration.Inf
   )(using ev: Temporal[F]): Resource[F, Connection[F]] =
 
     def fail[A](msg: String): Resource[F, A] =
@@ -116,24 +116,26 @@ object Connection:
 
     fromSockets(sockets, host, port, user, password, debug, sslOptions, readTimeout)
 
-  def pooled[F[_] : Temporal : Network : Console](
-    host: String,
-    port: Int,
-    user: String,
-    password: Option[String] = None,
-    max: Int,
-    debug: Boolean = false,
-    ssl: SSL = SSL.None,
+  def pooled[F[_]: Temporal: Network: Console](
+    host:          String,
+    port:          Int,
+    user:          String,
+    password:      Option[String] = None,
+    max:           Int,
+    debug:         Boolean = false,
+    ssl:           SSL = SSL.None,
     socketOptions: List[SocketOption] = Connection.defaultSocketOptions,
-    readTimeout: Duration = Duration.Inf
+    readTimeout:   Duration = Duration.Inf
   ): Resource[F, Tracer[F] => Resource[F, Connection[F]]] =
 
     val logger: String => F[Unit] = s => Console[F].println(s"TLS: $s")
 
-    def connection(socketGroup: SocketGroup[F], sslOp: Option[SSLNegotiation.Options[F]])(using Tracer[F]): Resource[F, Connection[F]] =
+    def connection(socketGroup: SocketGroup[F], sslOp: Option[SSLNegotiation.Options[F]])(using
+      Tracer[F]
+    ): Resource[F, Connection[F]] =
       fromSocketGroup(socketGroup, host, port, user, password, debug, socketOptions, sslOp, readTimeout)
 
     for
       sslOp <- ssl.toSSLNegotiationOptions(if debug then logger.some else none)
-      pool <- Pool.ofF({ implicit T: Tracer[F] => connection(Network[F], sslOp) }, max)(Recyclers.full)
+      pool  <- Pool.ofF({ implicit T: Tracer[F] => connection(Network[F], sslOp) }, max)(Recyclers.full)
     yield pool
