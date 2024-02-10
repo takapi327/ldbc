afb7ed47b188a5caf8bf0e14ce69b416f783f44b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-10 23:42:56 +0900

    Fix MySQLProtocol

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/MySQLProtocol.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/MySQLProtocol.scala
@@ -6,7 +6,7 @@
 
 package ldbc.connector.net
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.*
 
 import cats.*
 import cats.syntax.all.*
@@ -17,6 +17,7 @@ import cats.effect.std.*
 import fs2.io.net.Socket
 
 import ldbc.connector.authenticator.*
+import ldbc.connector.exception.MySQLException
 import ldbc.connector.net.packet.request.*
 import ldbc.connector.net.packet.response.*
 import ldbc.connector.net.protocol.Exchange
@@ -41,54 +42,52 @@ object MySQLProtocol:
   ): Resource[F, MySQLProtocol[F]] =
     for
       sequenceIdRef  <- Resource.eval(Ref[F].of[Byte](0x01))
-      socket         <- sockets
-      ps             <- Resource.eval(PacketSocket[F](debug, socket, sequenceIdRef, readTimeout))
-      initialPacket$ <- Resource.eval(ps.receive(InitialPacket.decoder))
-      socket$ <- sslOptions.fold(Resource.pure(ps))(option =>
-                   SSLNegotiation
-                     .negotiateSSL(socket, initialPacket$.capabilityFlags, option, sequenceIdRef)
-                     .flatMap(ssl => Resource.eval(PacketSocket[F](debug, ssl, sequenceIdRef, readTimeout)))
-                 )
-      protocol <- Resource.make(fromPacketSocket(socket$, initialPacket$, sequenceIdRef))(_.close())
+      initialPacketRef <- Resource.eval(Ref[F].of[Option[InitialPacket]](None))
+      ps <- PacketSocket[F](debug, sockets, sslOptions, sequenceIdRef, initialPacketRef, readTimeout)
+      protocol <- Resource.make(fromPacketSocket(ps, sequenceIdRef, initialPacketRef))(_.close())
     yield protocol
 
   def fromPacketSocket[F[_]: Temporal: Console](
     packetSocket:  PacketSocket[F],
-    packet:        InitialPacket,
-    sequenceIdRef: Ref[F, Byte]
+    sequenceIdRef: Ref[F, Byte],
+    initialPacketRef: Ref[F, Option[InitialPacket]]
   ): F[MySQLProtocol[F]] =
-    Exchange[F].map { implicit ex =>
-      new MySQLProtocol[F]:
-
-        override def initialPacket: InitialPacket = packet
-
-        private def readUntilOk(): F[Unit] =
-          packetSocket.receive(AuthenticationPacket.decoder(initialPacket.capabilityFlags)).flatMap {
-            case _: AuthMoreDataPacket => readUntilOk()
-            case _: OKPacket           => Concurrent[F].unit
-            case error: ERRPacket =>
-              Concurrent[F].raiseError(error.toException("Connection error"))
-          }
-
-        override def authenticate(user: String, password: String): F[Unit] =
-          val plugin = initialPacket.authPlugin match
-            case "mysql_native_password" => new MysqlNativePasswordPlugin
-            case "caching_sha2_password" => CachingSha2PasswordPlugin(Some(password), None)
-            case _                       => throw new Exception(s"Unknown plugin: ${ initialPacket.authPlugin }")
-
-          val hashedPassword = plugin.hashPassword(password, initialPacket.scrambleBuff)
-
-          val handshakeResponse = HandshakeResponsePacket(
-            initialPacket.capabilityFlags,
-            user,
-            Array(hashedPassword.length.toByte) ++ hashedPassword,
-            plugin.name
-          )
-
-          packetSocket.send(handshakeResponse) <* readUntilOk()
-
-        override def resetSequenceId: F[Unit] =
-          sequenceIdRef.update(_ => 0.toByte)
-
-        override def close(): F[Unit] = resetSequenceId *> packetSocket.send(ComQuitPacket())
-    }
+    for
+      given Exchange[F] <- Exchange[F]
+      initialPacketOpt <- initialPacketRef.get
+    yield initialPacketOpt match
+      case Some(initial) =>
+        new MySQLProtocol[F]:
+  
+          override def initialPacket: InitialPacket = initial
+  
+          private def readUntilOk(): F[Unit] =
+            packetSocket.receive(AuthenticationPacket.decoder(initialPacket.capabilityFlags)).flatMap {
+              case _: AuthMoreDataPacket => readUntilOk()
+              case _: OKPacket           => Concurrent[F].unit
+              case error: ERRPacket =>
+                Concurrent[F].raiseError(error.toException("Connection error"))
+            }
+  
+          override def authenticate(user: String, password: String): F[Unit] =
+            val plugin = initialPacket.authPlugin match
+              case "mysql_native_password" => new MysqlNativePasswordPlugin
+              case "caching_sha2_password" => CachingSha2PasswordPlugin(Some(password), None)
+              case _                       => throw new Exception(s"Unknown plugin: ${ initialPacket.authPlugin }")
+  
+            val hashedPassword = plugin.hashPassword(password, initialPacket.scrambleBuff)
+  
+            val handshakeResponse = HandshakeResponsePacket(
+              initialPacket.capabilityFlags,
+              user,
+              Array(hashedPassword.length.toByte) ++ hashedPassword,
+              plugin.name
+            )
+  
+            packetSocket.send(handshakeResponse) <* readUntilOk()
+  
+          override def resetSequenceId: F[Unit] =
+            sequenceIdRef.update(_ => 0.toByte)
+  
+          override def close(): F[Unit] = resetSequenceId *> packetSocket.send(ComQuitPacket())
+      case None => throw new MySQLException(None, "Initial packet is not set")
