9444966f6ccd42f835b2aff711fdf841ff8a2a45
takapi327 <t.takapi0327@gmail.com>
2024-02-16 11:03:33 +0900

    Use munit-cats-effect

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/ConnectionTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/ConnectionTest.scala
@@ -6,59 +6,58 @@
 
 package ldbc.connector
 
-import org.scalatest.flatspec.AnyFlatSpec
-
 import org.typelevel.otel4s.trace.Tracer
 
 import cats.effect.*
-import cats.effect.unsafe.implicits.global
+
+import munit.CatsEffectSuite
 
 import ldbc.connector.exception.MySQLException
 
-class ConnectionTest extends AnyFlatSpec:
+class ConnectionTest extends CatsEffectSuite:
 
   given Tracer[IO] = Tracer.noop[IO]
 
-  it should "Passing an empty string to host causes MySQLException" in {
+  test("Passing an empty string to host causes MySQLException") {
     val connection = Connection.single[IO](
       host = "",
       port = 13306,
       user = "root"
     )
-    assertThrows[MySQLException] {
-      connection.use(_ => IO.unit).unsafeRunSync()
+    interceptIO[MySQLException] {
+      connection.use(_ => IO.unit)
     }
   }
 
-  it should "UnknownHostException occurs when invalid host is passed" in {
+  test("UnknownHostException occurs when invalid host is passed") {
     val connection = Connection.single[IO](
       host = "host",
       port = 13306,
       user = "root"
     )
-    assertThrows[java.net.UnknownHostException] {
-      connection.use(_ => IO.unit).unsafeRunSync()
+    interceptIO[java.net.UnknownHostException] {
+      connection.use(_ => IO.unit)
     }
   }
 
-  it should "Passing a negative value to Port causes MySQLException" in {
+  test("Passing a negative value to Port causes MySQLException") {
     val connection = Connection.single[IO](
       host = "127.0.0.1",
       port = -1,
       user = "root"
     )
-    assertThrows[MySQLException] {
-      connection.use(_ => IO.unit).unsafeRunSync()
+    interceptIO[MySQLException] {
+      connection.use(_ => IO.unit)
     }
   }
 
-  it should "MySQLException occurs when passing more than 65535 values to Port" in {
+  test("MySQLException occurs when passing more than 65535 values to Port") {
     val connection = Connection.single[IO](
       host = "127.0.0.1",
       port = 65536,
       user = "root"
     )
-    assertThrows[MySQLException] {
-      connection.use(_ => IO.unit).unsafeRunSync()
+    interceptIO[MySQLException] {
+      connection.use(_ => IO.unit)
     }
   }
