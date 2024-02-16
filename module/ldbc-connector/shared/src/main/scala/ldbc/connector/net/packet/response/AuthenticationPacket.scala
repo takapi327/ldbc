3b0ac1028b7fb2088d04c993e3eda7ca17acb24c
takapi327 <t.takapi0327@gmail.com>
2024-02-16 13:34:52 +0900

    Fix AuthenticationPacket

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/response/AuthenticationPacket.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/response/AuthenticationPacket.scala
@@ -7,21 +7,20 @@
 package ldbc.connector.net.packet
 package response
 
-import scala.annotation.switch
-
 import scodec.*
 import scodec.codecs.*
 
 import ldbc.connector.data.CapabilitiesFlags
+import ldbc.connector.exception.MySQLException
 
 trait AuthenticationPacket extends ResponsePacket
 
 object AuthenticationPacket:
 
   def decoder(capabilityFlags: Seq[CapabilitiesFlags]): Decoder[AuthenticationPacket | GenericResponsePackets] =
-    int8.flatMap { status =>
-      (status: @switch) match
-        case AuthMoreDataPacket.STATUS => AuthMoreDataPacket.decoder
-        case OKPacket.STATUS           => OKPacket.decoder(capabilityFlags)
-        case ERRPacket.STATUS          => ERRPacket.decoder(capabilityFlags)
+    int8.flatMap {
+      case OKPacket.STATUS           => OKPacket.decoder(capabilityFlags)
+      case ERRPacket.STATUS          => ERRPacket.decoder(capabilityFlags)
+      case AuthMoreDataPacket.STATUS => AuthMoreDataPacket.decoder
+      case unknown                   => throw new MySQLException(None, "Error during database operation", Some(s"Unknown status: $unknown"))
     }
