b6043eaf626b0d3ed1ac560d9818e07528b49ee6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-17 15:56:03 +0900

    Create protocol package object

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/protocol/package.scala
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net
+
+import org.typelevel.otel4s.trace.Span
+import org.typelevel.otel4s.trace.Tracer
+
+package object protocol:
+
+  def exchange[F[_] : Tracer, A](label: String)(f: Span[F] => F[A])(
+    using exchange: Exchange[F]
+  ): F[A] = Tracer[F].span(label).use(span => exchange(f(span)))
