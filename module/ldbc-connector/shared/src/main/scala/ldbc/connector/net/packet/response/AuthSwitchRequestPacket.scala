00197e26856cb4f9549506af72bdec5553c412a4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-17 14:16:35 +0900

    Create AuthSwitchRequestPacket

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/response/AuthSwitchRequestPacket.scala
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package response
+
+import scodec.*
+import scodec.codecs.*
+
+import cats.syntax.all.*
+
+/**
+ * Authentication method Switch Request Packet
+ *
+ * If both server and the client support [[ldbc.connector.data.CapabilitiesFlags.CLIENT_PLUGIN_AUTH]] capability,
+ * server can send this packet tp ask client to use another authentication method.
+ * 
+ * @param status
+ *   Type: int<1>
+ *   Name: 0xFE (254)	
+ *   Description: status tag
+ * @param pluginName
+ *   Type: string<NUL>
+ *   Name: plugin name
+ *   Description: name of the client authentication plugin to switch to
+ * @param pluginProvidedData
+ *   Type: string<EOF>
+ *   Name: plugin provided data
+ *   Description: Initial authentication data for that client plugin
+ */
+case class AuthSwitchRequestPacket(
+                                    status:                   Int,
+                                    pluginName: String,
+                                    pluginProvidedData: String
+                                  ) extends AuthenticationPacket:
+
+  override def toString: String = "Protocol::AuthSwitchRequest"
+
+object AuthSwitchRequestPacket:
+  
+  val STATUS = 254
+  
+  val decoder: Decoder[AuthSwitchRequestPacket] =
+    for
+      status <- uint4
+      pluginName <- nullTerminatedStringCodec.asDecoder
+      pluginProvidedData <- nullTerminatedStringCodec.asDecoder
+    yield AuthSwitchRequestPacket(status, pluginName, pluginProvidedData)
