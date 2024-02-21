064fdbda4737d10e94f611a399edd3860349493c
takapi327 <t.takapi0327@gmail.com>
2024-02-21 13:50:58 +0900

    Fix Openssl

--- module/ldbc-connector/native/src/main/scala/ldbc/connector/authenticator/Openssl.scala
+++ module/ldbc-connector/native/src/main/scala/ldbc/connector/authenticator/Openssl.scala
@@ -9,6 +9,7 @@ package ldbc.connector.authenticator
 import org.typelevel.scalaccompat.annotation.*
 
 import scala.scalanative.unsafe.*
+import scala.scalanative.unsigned.*
 
 @nowarn212
 @link("crypto")
@@ -20,6 +21,11 @@ private[ldbc] object Openssl:
   type EVP_MD
   type EVP_MD_CTX
   type ENGINE
+  type BIO
+  type EVP_PKEY
+  type EVP_PKEY_CTX
+
+  type pem_password_cb = CFuncPtr4[Ptr[Byte], CInt, CInt, Ptr[Byte], CInt]
 
   def EVP_get_digestbyname(name: Ptr[CChar]): Ptr[EVP_MD] = extern
 
@@ -30,4 +36,20 @@ private[ldbc] object Openssl:
     size:   Ptr[CUnsignedInt],
     `type`: Ptr[EVP_MD],
     impl:   Ptr[ENGINE]
+  ): CInt = extern
+
+  def BIO_new_mem_buf(buf: Ptr[Byte], len: CInt): Ptr[BIO] = extern
+
+  def PEM_read_bio_PUBKEY(bp: Ptr[BIO], x: Ptr[Ptr[EVP_PKEY]], cb: pem_password_cb, u: Ptr[Byte]): Ptr[EVP_PKEY] = extern
+
+  def EVP_PKEY_CTX_new(pkey: Ptr[EVP_PKEY], e: Ptr[ENGINE]): Ptr[EVP_PKEY_CTX] = extern
+
+  def EVP_PKEY_encrypt_init(ctx: Ptr[EVP_PKEY_CTX]): CInt = extern
+
+  def EVP_PKEY_encrypt(
+    ctx: Ptr[EVP_PKEY_CTX],
+    out: Ptr[UByte],
+    outlen: Ptr[CSize],
+    in: Ptr[UByte],
+    inlen: CSize
   ): CInt = extern
