99c584960eacafb9cf5fa3f1713f3cfd35b89ca8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-25 19:44:10 +0900

    Create StatementTest

---
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/StatementTest.scala
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector
+
+import org.typelevel.otel4s.trace.Tracer
+
+import cats.effect.*
+
+import munit.CatsEffectSuite
+
+import ldbc.connector.net.packet.response.ResultSetRowPacket
+
+class StatementTest extends CatsEffectSuite:
+
+  given Tracer[IO] = Tracer.noop[IO]
+  
+  test("Statement should be able to execute a query") {
+    val connection = Connection[IO](
+      host = "127.0.0.1",
+      port = 13306,
+      user = "ldbc",
+      password = Some("password"),
+      ssl = SSL.Trusted
+    )
+    assertIO(connection.use(_.statement("SELECT 1").executeQuery()), List(ResultSetRowPacket(List(Some("1")))))
+  }
