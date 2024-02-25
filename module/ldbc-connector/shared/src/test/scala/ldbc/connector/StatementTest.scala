9b981df29e3274d67f3b085ae07e45d08aeff33f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-25 19:44:19 +0900

    Action sbt scalafmtAll

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/StatementTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/StatementTest.scala
@@ -17,14 +17,14 @@ import ldbc.connector.net.packet.response.ResultSetRowPacket
 class StatementTest extends CatsEffectSuite:
 
   given Tracer[IO] = Tracer.noop[IO]
-  
+
   test("Statement should be able to execute a query") {
     val connection = Connection[IO](
-      host = "127.0.0.1",
-      port = 13306,
-      user = "ldbc",
+      host     = "127.0.0.1",
+      port     = 13306,
+      user     = "ldbc",
       password = Some("password"),
-      ssl = SSL.Trusted
+      ssl      = SSL.Trusted
     )
     assertIO(connection.use(_.statement("SELECT 1").executeQuery()), List(ResultSetRowPacket(List(Some("1")))))
   }
