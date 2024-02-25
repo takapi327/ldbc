1e772ea8b070e00355a7bc6b0854429d6a848a16
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-25 18:51:14 +0900

    Create ColumnsNumberPacket

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/response/ColumnsNumberPacket.scala
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package response
+
+import scala.annotation.switch
+
+import scodec.*
+import scodec.codecs.*
+
+import ldbc.connector.data.CapabilitiesFlags
+
+/**
+ * A packet that contains the number of columns in the result set.
+ * 
+ * @param size
+ *   the number of columns in the result set
+ */
+case class ColumnsNumberPacket(size: Int) extends ResponsePacket:
+
+  override def toString: String = "ColumnsNumber Packet"
+  
+object ColumnsNumberPacket:
+
+  def decoder(capabilityFlags: Seq[CapabilitiesFlags]): Decoder[ColumnsNumberPacket | ERRPacket] =
+    uint8.flatMap { status =>
+      (status: @switch) match
+        case ERRPacket.STATUS => ERRPacket.decoder(capabilityFlags)
+        case value            => Decoder.pure(ColumnsNumberPacket(value))
+    }
