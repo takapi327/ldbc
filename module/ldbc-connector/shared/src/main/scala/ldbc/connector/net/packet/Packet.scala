ca6ae8f11fe75d5262756db12967b76031ea194d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-25 17:01:49 +0900

    Add comment in Packet

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/Packet.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/Packet.scala
@@ -9,11 +9,14 @@ package ldbc.connector.net.packet
 import scodec.*
 import scodec.bits.BitVector
 
+/** A trait that represents a packet. */
 transparent trait Packet
 
+/** A trait that represents a request packet. */
 trait RequestPacket extends Packet:
   protected def encodeBody: Attempt[BitVector]
 
   def encode: BitVector
 
+/** A trait that represents a response packet. */
 trait ResponsePacket extends Packet
