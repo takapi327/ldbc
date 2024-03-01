f83ddc228c723a22c5ad05271a8c62111eb5e570
Scala Steward <scala_steward@virtuslab.com>
2024-03-01 00:03:31 +0000

    Update doobie-core from 1.0.0-RC4 to 1.0.0-RC5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,6 @@
-/**
- *  This file is part of the ldbc.
- *  For the full copyright and license information,
- *  please view the LICENSE file that was distributed with this source code.
- */
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+  * distributed with this source code.
+  */
 
 import sbt.*
 
@@ -10,18 +8,18 @@ import ScalaVersions.*
 
 object Dependencies {
 
-  val cats = "org.typelevel" %% "cats-core" % "2.10.0"
+  val cats       = "org.typelevel" %% "cats-core"   % "2.10.0"
   val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.2"
 
   val schemaspy = "org.schemaspy" % "schemaspy" % "6.2.4"
 
   val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.3.0"
 
-  val circeYaml = "io.circe" %% "circe-yaml" % "0.15.1"
+  val circeYaml    = "io.circe" %% "circe-yaml"    % "0.15.1"
   val circeGeneric = "io.circe" %% "circe-generic" % "0.14.6"
 
   val mysqlVersion = "8.0.33"
-  val mysql = "mysql" % "mysql-connector-java" % mysqlVersion
+  val mysql        = "mysql" % "mysql-connector-java" % mysqlVersion
 
   val typesafeConfig = "com.typesafe" % "config" % "1.4.3"
 
@@ -29,14 +27,14 @@ object Dependencies {
 
   val scala3Compiler = "org.scala-lang" %% "scala3-compiler" % scala3
 
-  val doobie = "org.tpolecat" %% "doobie-core" % "1.0.0-RC4"
+  val doobie = "org.tpolecat" %% "doobie-core" % "1.0.0-RC5"
 
   val slick = "com.typesafe.slick" %% "slick" % "3.5.0-M5"
 
   val specs2Version = "5.4.0"
   val specs2: Seq[ModuleID] = Seq(
     "specs2-core",
-    "specs2-junit",
+    "specs2-junit"
   ).map("org.specs2" %% _ % specs2Version % Test)
 
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17" % Test
