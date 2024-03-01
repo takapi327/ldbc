71d84c6d7ce46a8376f1f66ae9aa5fe0c59327fc
takapi327 <t.takapi0327@gmail.com>
2024-03-01 18:15:26 +0900

    Create codec package object

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/codec/package.scala
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector
+
+package object codec:
+  
+  trait AllCodecs extends BooleanCodec, NumericCodecs, TextCodecs, TemporalCodecs
+  
+  object all extends AllCodecs
