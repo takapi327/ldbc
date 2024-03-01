021353c84546d78e1456751c0f12737d80ae7866
Scala Steward <scala_steward@virtuslab.com>
2024-03-01 00:03:46 +0000

    Update cats-effect from 3.5.2 to 3.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,6 @@
-/**
- *  This file is part of the ldbc.
- *  For the full copyright and license information,
- *  please view the LICENSE file that was distributed with this source code.
- */
+/** This file is part of the ldbc. For the full copyright and license information, please view the LICENSE file that was
+  * distributed with this source code.
+  */
 
 import sbt.*
 
@@ -10,18 +8,18 @@ import ScalaVersions.*
 
 object Dependencies {
 
-  val cats = "org.typelevel" %% "cats-core" % "2.10.0"
-  val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.2"
+  val cats       = "org.typelevel" %% "cats-core"   % "2.10.0"
+  val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.3"
 
   val schemaspy = "org.schemaspy" % "schemaspy" % "6.2.4"
 
   val parserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % "2.3.0"
 
-  val circeYaml = "io.circe" %% "circe-yaml" % "0.15.1"
+  val circeYaml    = "io.circe" %% "circe-yaml"    % "0.15.1"
   val circeGeneric = "io.circe" %% "circe-generic" % "0.14.6"
 
   val mysqlVersion = "8.0.33"
-  val mysql = "mysql" % "mysql-connector-java" % mysqlVersion
+  val mysql        = "mysql" % "mysql-connector-java" % mysqlVersion
 
   val typesafeConfig = "com.typesafe" % "config" % "1.4.3"
 
@@ -36,7 +34,7 @@ object Dependencies {
   val specs2Version = "5.4.0"
   val specs2: Seq[ModuleID] = Seq(
     "specs2-core",
-    "specs2-junit",
+    "specs2-junit"
   ).map("org.specs2" %% _ % specs2Version % Test)
 
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17" % Test
