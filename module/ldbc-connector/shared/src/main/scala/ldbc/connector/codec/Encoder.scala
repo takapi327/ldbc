c61a58c053307a80e1af7a0205ddf1c6b545819a
takapi327 <t.takapi0327@gmail.com>
2024-03-01 17:37:35 +0900

    Create Encoder

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/codec/Encoder.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.codec
+
+import org.typelevel.twiddles.TwiddleSyntax
+
+import ldbc.connector.data.{ Type, Encoded }
+
+/**
+ * Encoder of MySQL text-format data from Scala types.
+ */
+trait Encoder[A] extends TwiddleSyntax[Encoder]:
+  outer =>
+
+  /**
+   * Encode a value of type `A`, yielding a list of MySQL text-formatted strings, lifted to
+   * `Option` to handle `NULL` values. Encoding failures raise unrecoverable errors.
+   */
+  def encode(a: A): List[Option[Encoded]]
+
+  /** Types of encoded fields, in order. */
+  def types: List[Type]
+
+  /** `Encoder` is semigroupal: a pair of encoders make a encoder for a pair. */
+  def product[B](fb: Encoder[B]): Encoder[(A, B)] =
+    new Encoder[(A, B)]:
+      override def encode(ab: (A, B)): List[Option[Encoded]] = outer.encode(ab._1) ++ fb.encode(ab._2)
+
+      override val types: List[Type] = outer.types ++ fb.types
