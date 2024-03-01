5a97492403f823cefa428f1a12e0579b05e27d81
takapi327 <t.takapi0327@gmail.com>
2024-03-01 17:57:32 +0900

    Create TextCodecs

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/codec/TextCodecs.scala
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.codec
+
+import cats.syntax.all.*
+
+import scodec.bits.ByteVector
+
+import ldbc.connector.data.Type
+
+trait TextCodecs:
+
+  def char(length: Int): Codec[String] = Codec.simple(s => s, _.asRight, Type.char(length))
+  def varchar(length: Int): Codec[String] = Codec.simple(s => s, _.asRight, Type.varchar(length))
+
+  def binary(length: Int): Codec[Array[Byte]] = Codec.simple(
+    "\\x" + ByteVector.view(_).toHex,
+    str => ByteVector.fromHex(str.substring(2)).map(_.toArray).fold("Cannot decode bytes from HEX String".asLeft[Array[Byte]])(_.asRight[String]),
+    Type.binary(length)
+  )
+  def varbinary(length: Int): Codec[Array[Byte]] = Codec.simple(
+    "\\x" + ByteVector.view(_).toHex,
+    str => ByteVector.fromHex(str.substring(2)).map(_.toArray).fold("Cannot decode bytes from HEX String".asLeft[Array[Byte]])(_.asRight[String]),
+    Type.varbinary(length)
+  )
+
+  def tinyblob(length: Int): Codec[Array[Byte]] = Codec.simple(
+    "\\x" + ByteVector.view(_).toHex,
+    str => ByteVector.fromHex(str.substring(2)).map(_.toArray).fold("Cannot decode bytes from HEX String".asLeft[Array[Byte]])(_.asRight[String]),
+    Type.tinyblob
+  )
+  def blob(length: Int): Codec[Array[Byte]] = Codec.simple(
+    "\\x" + ByteVector.view(_).toHex,
+    str => ByteVector.fromHex(str.substring(2)).map(_.toArray).fold("Cannot decode bytes from HEX String".asLeft[Array[Byte]])(_.asRight[String]),
+    Type.blob
+  )
+  def mediumblob(length: Int): Codec[Array[Byte]] = Codec.simple(
+    "\\x" + ByteVector.view(_).toHex,
+    str => ByteVector.fromHex(str.substring(2)).map(_.toArray).fold("Cannot decode bytes from HEX String".asLeft[Array[Byte]])(_.asRight[String]),
+    Type.mediumblob
+  )
+  def longblob(length: Int): Codec[Array[Byte]] = Codec.simple(
+    "\\x" + ByteVector.view(_).toHex,
+    str => ByteVector.fromHex(str.substring(2)).map(_.toArray).fold("Cannot decode bytes from HEX String".asLeft[Array[Byte]])(_.asRight[String]),
+    Type.longblob
+  )
+
+  val tinytext: Codec[String] = Codec.simple(s => s, _.asRight, Type.tinytext)
+  val text: Codec[String] = Codec.simple(s => s, _.asRight, Type.text)
+  val mediumtext: Codec[String] = Codec.simple(s => s, _.asRight, Type.mediumtext)
+  val longtext: Codec[String] = Codec.simple(s => s, _.asRight, Type.longtext)
+
+  def `enum`(values: List[String]): Codec[String] = Codec.simple(s => s, _.asRight, Type.`enum`(values))
+
+object text extends TextCodecs
