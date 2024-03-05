74350d59567b2bbf18cbc9e427de7fe75a2bfc82
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-03-05 21:08:54 +0900

    Fix NumericCodecsTest for js

--- module/ldbc-connector/js/src/test/scala/ldbc/connector/codec/NumericCodecsTest.scala
+++ module/ldbc-connector/js/src/test/scala/ldbc/connector/codec/NumericCodecsTest.scala
@@ -462,7 +462,7 @@ class NumericCodecsTest extends FunSuite:
   test("decimal decode error") {
     assertEquals(
       decimal().decode(0, List(Some(""))),
-      Left(Decoder.Error(0, 1, "Invalid decimal(10, 0)  null", Type.decimal()))
+      Left(Decoder.Error(0, 1, "Invalid decimal(10, 0)  Bad offset/length: offset=0 len=0 in.length=0", Type.decimal()))
     )
     assertEquals(
       decimal().decode(0, List(Some("invalid"))),
@@ -515,7 +515,7 @@ class NumericCodecsTest extends FunSuite:
     )
     assertEquals(
       float.decode(0, List(Some("1.1.1"))),
-      Left(Decoder.Error(0, 1, "Invalid float 1.1.1 multiple points", Type.float))
+      Left(Decoder.Error(0, 1, "Invalid float 1.1.1 For input string: \"1.1.1\"", Type.float))
     )
   }
 
@@ -546,6 +546,6 @@ class NumericCodecsTest extends FunSuite:
     )
     assertEquals(
       double.decode(0, List(Some("1.1.1"))),
-      Left(Decoder.Error(0, 1, "Invalid double 1.1.1 multiple points", Type.double))
+      Left(Decoder.Error(0, 1, "Invalid double 1.1.1 For input string: \"1.1.1\"", Type.double))
     )
   }
