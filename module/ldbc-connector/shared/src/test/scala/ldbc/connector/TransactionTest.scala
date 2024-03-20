94a3242b3d77c26747918d18d1cdbfc219e9ea17
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-03-20 14:11:25 +0900

    Create TransactionTest

---
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/TransactionTest.scala
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector
+
+import cats.effect.*
+
+import org.typelevel.otel4s.trace.Tracer
+
+import munit.CatsEffectSuite
+
+class TransactionTest extends CatsEffectSuite:
+
+  given Tracer[IO] = Tracer.noop[IO]
+
+  test("Transactions initiated in a session are read-only.") {
+    val connection = Connection[IO](
+      host = "127.0.0.1",
+      port = 13306,
+      user = "ldbc",
+      password = Some("password"),
+      ssl = SSL.Trusted
+    )
+    assertIOBoolean(connection.use { conn =>
+      for
+        _ <- conn.setReadOnly(true)
+        readOnly <- conn.isReadOnly
+      yield readOnly
+    })
+  }
+
+  test("Transactions initiated in a session are write-only.") {
+    val connection = Connection[IO](
+      host = "127.0.0.1",
+      port = 13306,
+      user = "ldbc",
+      password = Some("password"),
+      ssl = SSL.Trusted
+    )
+    assertIOBoolean(connection.use { conn =>
+      for
+        _ <- conn.setReadOnly(false)
+        readOnly <- conn.isReadOnly
+      yield !readOnly
+    })
+  }
