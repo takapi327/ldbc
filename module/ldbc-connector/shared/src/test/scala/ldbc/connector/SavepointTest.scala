e53604d3f3022253a6d3f8f29c891977196e4939
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-03-23 16:06:27 +0900

    Action sbt scalafmtAll

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/SavepointTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/SavepointTest.scala
@@ -19,78 +19,90 @@ class SavepointTest extends CatsEffectSuite:
   given Tracer[IO] = Tracer.noop[IO]
 
   private val connection = Connection[IO](
-    host = "127.0.0.1",
-    port = 13306,
-    user = "ldbc",
+    host     = "127.0.0.1",
+    port     = 13306,
+    user     = "ldbc",
     password = Some("password"),
     database = Some("connector_test"),
-    ssl = SSL.Trusted
+    ssl      = SSL.Trusted
   )
 
   override def beforeEach(context: BeforeEach): Unit =
-    connection.use { conn =>
-      for
-        s1 <- conn.clientPreparedStatement("CREATE TABLE IF NOT EXISTS `savepoint_test` (`c1` BIGINT)")
-        s2 <- conn.clientPreparedStatement("TRUNCATE TABLE `savepoint_test`")
-        _ <- s1.executeUpdate()
-        _ <- s2.executeUpdate()
-      yield ()
-    }.unsafeRunSync()
+    connection
+      .use { conn =>
+        for
+          s1 <- conn.clientPreparedStatement("CREATE TABLE IF NOT EXISTS `savepoint_test` (`c1` BIGINT)")
+          s2 <- conn.clientPreparedStatement("TRUNCATE TABLE `savepoint_test`")
+          _  <- s1.executeUpdate()
+          _  <- s2.executeUpdate()
+        yield ()
+      }
+      .unsafeRunSync()
 
   override def afterEach(context: AfterEach): Unit =
-    connection.use { conn =>
-      for
-        s1 <- conn.clientPreparedStatement("DROP TABLE IF EXISTS `savepoint_test`")
-        _ <- s1.executeUpdate()
-      yield ()
-    }.unsafeRunSync()
+    connection
+      .use { conn =>
+        for
+          s1 <- conn.clientPreparedStatement("DROP TABLE IF EXISTS `savepoint_test`")
+          _  <- s1.executeUpdate()
+        yield ()
+      }
+      .unsafeRunSync()
 
   test("A Savepoint with the specified name can be set.") {
-    assertIO(connection.use { conn =>
-      for
-        savepoint <- conn.setSavepoint("test_savepoint")
-      yield savepoint.getSavepointName
-    }, "test_savepoint")
+    assertIO(
+      connection.use { conn =>
+        for savepoint <- conn.setSavepoint("test_savepoint")
+        yield savepoint.getSavepointName
+      },
+      "test_savepoint"
+    )
   }
 
   test("A Savepoint can be set and rolled back.") {
-    assertIO(connection.use { conn =>
-      for
-        _ <- conn.setAutoCommit(false)
-        statement1 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
-        _ <- statement1.setLong(1, 1L)
-        _         <- statement1.executeUpdate()
-        savepoint <- conn.setSavepoint("test_savepoint")
-        statement2 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
-        _ <- statement2.setLong(1, 2L)
-        _         <- statement2.executeUpdate()
-        _ <- conn.rollback(savepoint)
-        _ <- conn.commit()
-        statement3 <- conn.clientPreparedStatement("SELECT * FROM `savepoint_test` WHERE `c1` IN (?, ?)")
-        _ <- statement3.setLong(1, 1L)
-        _ <- statement3.setLong(2, 2L)
-        resultSet <- statement3.executeQuery()
-      yield resultSet.decode(bigint).length
-    }, 1)
+    assertIO(
+      connection.use { conn =>
+        for
+          _          <- conn.setAutoCommit(false)
+          statement1 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
+          _          <- statement1.setLong(1, 1L)
+          _          <- statement1.executeUpdate()
+          savepoint  <- conn.setSavepoint("test_savepoint")
+          statement2 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
+          _          <- statement2.setLong(1, 2L)
+          _          <- statement2.executeUpdate()
+          _          <- conn.rollback(savepoint)
+          _          <- conn.commit()
+          statement3 <- conn.clientPreparedStatement("SELECT * FROM `savepoint_test` WHERE `c1` IN (?, ?)")
+          _          <- statement3.setLong(1, 1L)
+          _          <- statement3.setLong(2, 2L)
+          resultSet  <- statement3.executeQuery()
+        yield resultSet.decode(bigint).length
+      },
+      1
+    )
   }
 
   test("A Savepoint can be set and released.") {
-    assertIO(connection.use { conn =>
-      for
-        _ <- conn.setAutoCommit(false)
-        statement1 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
-        _ <- statement1.setLong(1, 1L)
-        _         <- statement1.executeUpdate()
-        savepoint <- conn.setSavepoint("test_savepoint")
-        statement2 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
-        _ <- statement2.setLong(1, 2L)
-        _         <- statement2.executeUpdate()
-        _ <- conn.releaseSavepoint(savepoint)
-        _ <- conn.commit()
-        statement3 <- conn.clientPreparedStatement("SELECT * FROM `savepoint_test` WHERE `c1` IN (?, ?)")
-        _ <- statement3.setLong(1, 1L)
-        _ <- statement3.setLong(2, 2L)
-        resultSet <- statement3.executeQuery()
-      yield resultSet.decode(bigint).length
-    }, 2)
+    assertIO(
+      connection.use { conn =>
+        for
+          _          <- conn.setAutoCommit(false)
+          statement1 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
+          _          <- statement1.setLong(1, 1L)
+          _          <- statement1.executeUpdate()
+          savepoint  <- conn.setSavepoint("test_savepoint")
+          statement2 <- conn.clientPreparedStatement("INSERT INTO `savepoint_test` VALUES (?)")
+          _          <- statement2.setLong(1, 2L)
+          _          <- statement2.executeUpdate()
+          _          <- conn.releaseSavepoint(savepoint)
+          _          <- conn.commit()
+          statement3 <- conn.clientPreparedStatement("SELECT * FROM `savepoint_test` WHERE `c1` IN (?, ?)")
+          _          <- statement3.setLong(1, 1L)
+          _          <- statement3.setLong(2, 2L)
+          resultSet  <- statement3.executeQuery()
+        yield resultSet.decode(bigint).length
+      },
+      2
+    )
   }
