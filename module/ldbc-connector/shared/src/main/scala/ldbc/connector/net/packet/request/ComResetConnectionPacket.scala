8669af6a690b887195db8620c2bf70873930b9d0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-04-01 22:51:19 +0900

    Create ComResetConnectionPacket

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/request/ComResetConnectionPacket.scala
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+package request
+
+import scodec.*
+import scodec.bits.BitVector
+
+import ldbc.connector.data.CommandId
+
+case class ComResetConnectionPacket() extends RequestPacket:
+
+  override protected def encodeBody: Attempt[BitVector] =
+    ComResetConnectionPacket.encoder.encode(this)
+
+  override def encode: BitVector = encodeBody.require
+
+  override def toString: String = "COM_RESET_CONNECTION Request"
+
+object ComResetConnectionPacket:
+
+  val encoder: Encoder[ComResetConnectionPacket] = Encoder(_ => Attempt.successful(BitVector(CommandId.COM_RESET_CONNECTION)))
