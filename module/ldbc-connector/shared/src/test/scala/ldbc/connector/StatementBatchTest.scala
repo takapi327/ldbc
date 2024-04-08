92e4ca4328d7282f218debd9071f700f2b611fbd
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-04-09 01:37:22 +0900

    Add executeBatch for preparedStatement test

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/StatementBatchTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/StatementBatchTest.scala
@@ -22,7 +22,7 @@ class StatementBatchTest extends CatsEffectSuite:
     user     = "ldbc",
     password = Some("password"),
     database = Some("connector_test"),
-    ssl      = SSL.Trusted
+    allowPublicKeyRetrieval = true,
   )
 
   test("It will be the same as the list of updates specified as a result of executing the batch process.") {
@@ -66,5 +66,76 @@ class StatementBatchTest extends CatsEffectSuite:
         yield result
       },
       List.empty
+    )
+  }
+
+  test("If the batch execution of Client PreparedStatement is successful, the result will be a -2 array of the number of queries that were batch executed.") {
+    assertIO(
+      connection.use { conn =>
+        for
+          statement <- conn.createStatement()
+          _ <- statement.executeUpdate("CREATE TABLE `batch_test2` (`c1` INT, `c2` INT)")
+          preparedStatement <- conn.clientPreparedStatement("INSERT INTO `batch_test2` VALUES (?, ?)")
+          _         <- preparedStatement.setInt(1, 1) *> preparedStatement.setInt(2, 1) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 2) *> preparedStatement.setInt(2, 2) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 3) *> preparedStatement.setInt(2, 3) *> preparedStatement.addBatch()
+          result    <- preparedStatement.executeBatch()
+          _ <- statement.executeUpdate("DROP TABLE `batch_test2`")
+        yield result
+      },
+      List(-2, -2, -2)
+    )
+  }
+
+  test("Placeholder(?) and the number of parameters do not match, an IllegalArgumentException is raised.") {
+    interceptMessageIO[IllegalArgumentException](
+      "requirement failed: The number of parameters does not match the number of placeholders"
+    )(
+      connection.use { conn =>
+        for
+          preparedStatement <- conn.clientPreparedStatement("INSERT INTO `batch_test2` VALUES (?)")
+          _         <- preparedStatement.setInt(1, 1) *> preparedStatement.setInt(2, 1) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 2) *> preparedStatement.setInt(2, 2) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 3) *> preparedStatement.setInt(2, 3) *> preparedStatement.addBatch()
+          result    <- preparedStatement.executeBatch()
+        yield result
+      }
+    )
+  }
+
+  test("If the batch execution of Server PreparedStatement is successful, the result will be a -2 array of the number of queries that were batch executed.") {
+    assertIO(
+      connection.use { conn =>
+        for
+          statement <- conn.createStatement()
+          _ <- statement.executeUpdate("CREATE TABLE `batch_test3` (`c1` INT, `c2` INT)")
+          preparedStatement <- conn.serverPreparedStatement("INSERT INTO `batch_test3` VALUES (?, ?)")
+          _         <- preparedStatement.setInt(1, 1) *> preparedStatement.setInt(2, 1) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 2) *> preparedStatement.setInt(2, 2) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 3) *> preparedStatement.setInt(2, 3) *> preparedStatement.addBatch()
+          result    <- preparedStatement.executeBatch()
+          _ <- statement.executeUpdate("DROP TABLE `batch_test3`")
+        yield result
+      },
+      List(-2, -2, -2)
+    )
+  }
+
+  test("Placeholder(?) and the number of parameters do not match, an IllegalArgumentException is raised.") {
+    interceptMessageIO[IllegalArgumentException](
+      "requirement failed: The number of parameters does not match the number of placeholders"
+    )(
+      connection.use { conn =>
+        for
+          statement <- conn.createStatement()
+          _ <- statement.executeUpdate("CREATE TABLE `batch_test4` (`c1` INT)")
+          preparedStatement <- conn.serverPreparedStatement("INSERT INTO `batch_test4` VALUES (?)")
+          _         <- preparedStatement.setInt(1, 1) *> preparedStatement.setInt(2, 1) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 2) *> preparedStatement.setInt(2, 2) *> preparedStatement.addBatch()
+          _         <- preparedStatement.setInt(1, 3) *> preparedStatement.setInt(2, 3) *> preparedStatement.addBatch()
+          result    <- preparedStatement.executeBatch()
+          _ <- statement.executeUpdate("DROP TABLE `batch_test4`")
+        yield result
+      }
     )
   }
