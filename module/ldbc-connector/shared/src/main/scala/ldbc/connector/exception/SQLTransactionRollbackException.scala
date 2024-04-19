ad4186e407dee652ce2bcf04d726d20432a023ff
takapi327 <t.takapi0327@gmail.com>
2024-04-19 18:01:28 +0900

    Create SQLTransactionRollbackException

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/exception/SQLTransactionRollbackException.scala
@@ -0,0 +1,17 @@
+package ldbc.connector.exception
+
+/**
+ * The subclass of {@link SQLException} thrown when the SQLState class value
+ * is '<i>40</i>', or under vendor-specified conditions. This indicates that the
+ * current statement was automatically rolled back by the database because
+ * of deadlock or other transaction serialization failures.
+ */
+class SQLTransactionRollbackException(
+                                       sqlState: String,
+                                       vendorCode: Int,
+                                       message:          String,
+                                       sql:              Option[String] = None,
+                                       detail:           Option[String] = None,
+                                       hint:             Option[String] = None,
+                                       originatedPacket: Option[String] = None
+                                     ) extends SQLTransientException(sqlState, vendorCode, message, sql, detail, hint, originatedPacket)
