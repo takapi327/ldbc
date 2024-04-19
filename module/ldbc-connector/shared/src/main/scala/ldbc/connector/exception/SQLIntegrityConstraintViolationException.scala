b76a0684d0663f8f0f2a5bd5c95cfa634f9c1a9a
takapi327 <t.takapi0327@gmail.com>
2024-04-19 17:52:02 +0900

    Create SQLIntegrityConstraintViolationException

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/exception/SQLIntegrityConstraintViolationException.scala
@@ -0,0 +1,20 @@
+package ldbc.connector.exception
+
+/**
+ * The subclass of {@link SQLException} thrown when the SQLState class value
+ * is '<i>23</i>', or under vendor-specified conditions.
+ * This indicates that an integrity
+ * constraint (foreign key, primary key or unique key) has been violated.
+ * <p>
+ * Please consult your driver vendor documentation for the vendor-specified
+ * conditions for which this <code>Exception</code> may be thrown.
+ */
+class SQLIntegrityConstraintViolationException(
+                                                sqlState: String,
+                                                vendorCode: Int,
+                                                message:          String,
+                                                sql:              Option[String] = None,
+                                                detail:           Option[String] = None,
+                                                hint:             Option[String] = None,
+                                                originatedPacket: Option[String] = None
+                                              ) extends SQLException(sqlState, vendorCode, message, sql, detail, hint, originatedPacket)
