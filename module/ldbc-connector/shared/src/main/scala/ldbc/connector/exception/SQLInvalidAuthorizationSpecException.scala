54a1ac6443dd805911b38dcf842b5a544ece255f
takapi327 <t.takapi0327@gmail.com>
2024-04-19 17:54:01 +0900

    Create SQLInvalidAuthorizationSpecException

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/exception/SQLInvalidAuthorizationSpecException.scala
@@ -0,0 +1,18 @@
+package ldbc.connector.exception
+
+/**
+ * The subclass of {@link SQLException} thrown when the SQLState class value
+ * is '<i>28</i>', or under vendor-specified conditions. This indicates that
+ * the authorization credentials presented during connection establishment
+ * are not valid.
+ */
+class SQLInvalidAuthorizationSpecException(
+                                            sqlState: String,
+                                            vendorCode: Int,
+                                            message:          String,
+                                            sql:              Option[String] = None,
+                                            detail:           Option[String] = None,
+                                            hint:             Option[String] = None,
+                                            originatedPacket: Option[String] = None
+                                          ) extends SQLException(sqlState, vendorCode, message, sql, detail, hint, originatedPacket)
+
