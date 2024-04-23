beb48a7bdf98cb5e758c9d1ef986cd546a26fd91
takapi327 <t.takapi0327@gmail.com>
2024-04-23 18:23:27 +0900

    Add ResultSet relative test

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/ResultSetTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/ResultSetTest.scala
@@ -781,6 +781,36 @@ class ResultSetTest extends CatsEffectSuite:
     interceptMessage[SQLException]("Message: Operation not allowed for a result set of type ResultSet.TYPE_FORWARD_ONLY.")(resultSet.absolute(0))
   }
 
+  test("If the cursor in the ResultSet is relative position, the result at the cursor position matches the specified value.") {
+    val resultSet = ResultSet(
+      Vector(column("c1", ColumnDataType.MYSQL_TYPE_TIMESTAMP)),
+      Vector(ResultSetRowPacket(List(Some("1"))), ResultSetRowPacket(List(Some("2")))),
+      Version(0, 0, 0),
+      ResultSet.TYPE_SCROLL_INSENSITIVE
+    )
+    assertEquals(resultSet.getRow(), 0)
+    assertEquals(resultSet.relative(0), false)
+    assertEquals(resultSet.getRow(), 0)
+    assertEquals(resultSet.getInt(1), 1)
+    assertEquals(resultSet.relative(1), true)
+    assertEquals(resultSet.getRow(), 1)
+    assertEquals(resultSet.getInt(1), 1)
+    assertEquals(resultSet.relative(2), false)
+    assertEquals(resultSet.getRow(), 0)
+    assertEquals(resultSet.relative(-1), false)
+    assertEquals(resultSet.getRow(), 0)
+    assertEquals(resultSet.getInt(1), 1)
+  }
+
+  test("When the type of ResultSet is TYPE_FORWARD_ONLY, the cursor position operation by relative throws SQLException.") {
+    val resultSet = ResultSet(
+      Vector.empty,
+      Vector.empty,
+      Version(0, 0, 0),
+    )
+    interceptMessage[SQLException]("Message: Operation not allowed for a result set of type ResultSet.TYPE_FORWARD_ONLY.")(resultSet.relative(0))
+  }
+
   private def column(
     columnName: String,
     `type`:     ColumnDataType,
