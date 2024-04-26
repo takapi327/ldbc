8be16cb1985e834bc696197ba270356e1eede70b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-04-27 00:13:48 +0900

    Fix UtilityCommands protocol

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/protocol/UtilityCommands.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/protocol/UtilityCommands.scala
@@ -6,15 +6,7 @@
 
 package ldbc.connector.net.protocol
 
-import cats.*
-import cats.syntax.all.*
-
-import org.typelevel.otel4s.Attribute
-import org.typelevel.otel4s.trace.{ Tracer, Span }
-
 import ldbc.connector.data.EnumMySQLSetOption
-import ldbc.connector.net.PacketSocket
-import ldbc.connector.net.packet.request.*
 import ldbc.connector.net.packet.response.*
 
 /**
@@ -60,70 +52,3 @@ trait UtilityCommands[F[_]]:
    *   the option operation
    */
   def comSetOption(optionOperation: EnumMySQLSetOption): F[Unit]
-
-object UtilityCommands:
-
-  def apply[F[_]: Exchange: Tracer](
-    socket:        PacketSocket[F],
-    initialPacket: InitialPacket
-  )(using ev: MonadError[F, Throwable]): UtilityCommands[F] =
-    new UtilityCommands[F]:
-
-      private val attributes = initialPacket.attributes ++ List(
-        Attribute("type", "Utility Commands")
-      )
-
-      override def comQuit(): F[Unit] =
-        exchange[F, Unit]("utility_commands") { (span: Span[F]) =>
-          span.addAttributes((attributes :+ Attribute("command", "COM_QUIT"))*) *> socket.send(ComQuitPacket())
-        }
-
-      override def comInitDB(schema: String): F[Unit] =
-        exchange[F, Unit]("utility_commands") { (span: Span[F]) =>
-          span.addAttributes((attributes ++ List(Attribute("command", "COM_INIT_DB"), Attribute("schema", schema)))*) *>
-            socket.send(ComInitDBPacket(schema)) *>
-            socket.receive(GenericResponsePackets.decoder(initialPacket.capabilityFlags)).flatMap {
-              case error: ERRPacket => ev.raiseError(error.toException("Failed to execute change schema"))
-              case ok: OKPacket     => ev.unit
-            }
-        }
-
-      override def comStatistics(): F[StatisticsPacket] =
-        exchange[F, StatisticsPacket]("utility_commands") { (span: Span[F]) =>
-          span.addAttributes((attributes :+ Attribute("command", "COM_STATISTICS"))*) *>
-            socket.send(ComStatisticsPacket()) *>
-            socket.receive(StatisticsPacket.decoder)
-        }
-
-      override def comPing(): F[Boolean] =
-        exchange[F, Boolean]("utility_commands") { (span: Span[F]) =>
-          span.addAttributes((attributes :+ Attribute("command", "COM_PING"))*) *>
-            socket.send(ComPingPacket()) *>
-            socket.receive(GenericResponsePackets.decoder(initialPacket.capabilityFlags)).flatMap {
-              case error: ERRPacket => ev.pure(false)
-              case ok: OKPacket     => ev.pure(true)
-            }
-        }
-
-      override def comResetConnection(): F[Unit] =
-        exchange[F, Unit]("utility_commands") { (span: Span[F]) =>
-          span.addAttributes((attributes :+ Attribute("command", "COM_RESET_CONNECTION"))*) *>
-            socket.send(ComResetConnectionPacket()) *>
-            socket.receive(GenericResponsePackets.decoder(initialPacket.capabilityFlags)).flatMap {
-              case error: ERRPacket => ev.raiseError(error.toException("Failed to execute reset connection"))
-              case ok: OKPacket     => ev.unit
-            }
-        }
-
-      override def comSetOption(optionOperation: EnumMySQLSetOption): F[Unit] =
-        exchange[F, Unit]("utility_commands") { (span: Span[F]) =>
-          span.addAttributes(
-            (attributes ++ List(Attribute("command", "COM_SET_OPTION"), Attribute("option", optionOperation.toString)))*
-          ) *>
-            socket.send(ComSetOptionPacket(optionOperation)) *>
-            socket.receive(GenericResponsePackets.decoder(initialPacket.capabilityFlags)).flatMap {
-              case error: ERRPacket => ev.raiseError(error.toException("Failed to execute set option"))
-              case eof: EOFPacket   => ev.unit
-              case ok: OKPacket     => ev.unit
-            }
-        }
