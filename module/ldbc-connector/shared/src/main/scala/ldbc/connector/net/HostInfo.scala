1645a424fd86ac8d296052903b31c1d52d693029
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-04-30 14:26:25 +0900

    Create HostInfo

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/net/HostInfo.scala
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net
+
+case class HostInfo(
+  host: String,
+  port: Int,
+  user: String,
+  password: Option[String],
+  database: Option[String],
+):
+  
+  val url: String = s"jdbc:mysql://$host:$port" + database.map("/" + _).getOrElse("")
