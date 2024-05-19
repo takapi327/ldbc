ce133e32f422d76d906cc974fab21b561ca8aa44
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-19 20:41:31 +0900

    Create Connection object

--- module/ldbc-sql/src/main/scala/ldbc/sql/Connection.scala
+++ module/ldbc-sql/src/main/scala/ldbc/sql/Connection.scala
@@ -445,3 +445,53 @@ trait Connection[F[_]]:
    *   true if the connection is valid, false otherwise
    */
   def isValid(timeout: Int): F[Boolean]
+
+object Connection:
+
+  /**
+   * A constant indicating that transactions are not supported.
+   */
+  val TRANSACTION_NONE: Int = 0
+
+  /**
+   * A constant indicating that
+   * dirty reads, non-repeatable reads and phantom reads can occur.
+   * This level allows a row changed by one transaction to be read
+   * by another transaction before any changes in that row have been
+   * committed (a "dirty read").  If any of the changes are rolled back,
+   * the second transaction will have retrieved an invalid row.
+   */
+  val TRANSACTION_READ_UNCOMMITTED: Int = 1
+
+  /**
+   * A constant indicating that
+   * dirty reads are prevented; non-repeatable reads and phantom
+   * reads can occur.  This level only prohibits a transaction
+   * from reading a row with uncommitted changes in it.
+   */
+  val TRANSACTION_READ_COMMITTED: Int = 2
+
+  /**
+   * A constant indicating that
+   * dirty reads and non-repeatable reads are prevented; phantom
+   * reads can occur.  This level prohibits a transaction from
+   * reading a row with uncommitted changes in it, and it also
+   * prohibits the situation where one transaction reads a row,
+   * a second transaction alters the row, and the first transaction
+   * rereads the row, getting different values the second time
+   * (a "non-repeatable read").
+   */
+  val TRANSACTION_REPEATABLE_READ: Int = 4
+
+  /**
+   * A constant indicating that
+   * dirty reads, non-repeatable reads and phantom reads are prevented.
+   * This level includes the prohibitions in
+   * <code>TRANSACTION_REPEATABLE_READ</code> and further prohibits the
+   * situation where one transaction reads all rows that satisfy
+   * a <code>WHERE</code> condition, a second transaction inserts a row that
+   * satisfies that <code>WHERE</code> condition, and the first transaction
+   * rereads for the same condition, retrieving the additional
+   * "phantom" row in the second read.
+   */
+  val TRANSACTION_SERIALIZABLE: Int = 8
