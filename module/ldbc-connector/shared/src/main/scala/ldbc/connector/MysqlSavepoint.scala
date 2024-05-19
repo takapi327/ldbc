4419da39b3ca9c81fb15e1e0104a37495912e1fe
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-19 22:16:08 +0900

    Create MysqlSavepoint

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/MysqlSavepoint.scala
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector
+
+import ldbc.sql.Savepoint
+
+import ldbc.connector.exception.SQLException
+
+/**
+ * Represents SQL SAVEPOINTS in MySQL.
+ */
+class MysqlSavepoint(name: String) extends Savepoint:
+  override def getSavepointId(): Int = throw SQLException("Only named savepoints are supported.")
+  override def getSavepointName(): String = name
