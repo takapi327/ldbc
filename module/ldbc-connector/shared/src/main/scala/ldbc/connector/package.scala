9d4b00872d305b3544a0684ee7aa1ce90dad472d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-21 00:03:44 +0900

    Create connector package object

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/package.scala
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc
+
+import cats.effect.*
+
+import ldbc.sql.*
+
+package object connector:
+  
+  private trait StringContextSyntax[F[_]: Temporal]:
+
+    extension (sc: StringContext)
+      inline def sql(inline args: ParameterBinder[F]*): SQL[F] =
+        val strings     = sc.parts.iterator
+        val expressions = args.iterator
+        Mysql[F](strings.mkString("?"), expressions.toList)
+
+  /**
+   * Top-level imports provide aliases for the most commonly used types and modules. A typical starting set of imports
+   * might look something like this.
+   *
+   * example:
+   * {{{
+   *   import ldbc.connector.io.*
+   * }}}
+   */
+  val io: StringContextSyntax[IO] = new StringContextSyntax[IO] {}
