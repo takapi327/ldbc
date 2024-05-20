a8fbfbcca866ed5c7ef850a78951b03c5096dae2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-21 00:03:24 +0900

    Create Mysql

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/Mysql.scala
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector
+
+import scala.annotation.targetName
+
+import cats.data.Kleisli
+import cats.syntax.all.*
+
+import cats.effect.Temporal
+
+import ldbc.sql.*
+
+/**
+ * A model with a query string and parameters to be bound to the query string that is executed by PreparedStatement,
+ * etc.
+ *
+ * @param statement
+ * an SQL statement that may contain one or more '?' IN parameter placeholders
+ * @param params
+ * statement has '?' that the statement has.
+ * @tparam F
+ * The effect type
+ */
+case class Mysql[F[_]: Temporal](statement: String, params: Seq[ParameterBinder[F]]) extends SQL[F]:
+
+  @targetName("combine")
+  override def ++(sql: SQL[F]): SQL[F] =
+    Mysql[F](statement ++ " " ++ sql.statement, params ++ sql.params)
+
+  private[ldbc] override def connection[T](
+    statement: String,
+    params:    Seq[ParameterBinder[F]],
+    consumer:  ResultSetConsumer[F, T]
+  ): Kleisli[F, Connection[F], T] =
+    Kleisli { connection =>
+      for
+        prepareStatement <- connection.prepareStatement(statement)
+        resultSet <- params.zipWithIndex.traverse {
+                       case (param, index) => param.bind(prepareStatement, index + 1)
+                     } >> prepareStatement
+                       .executeQuery()
+        result <-
+          consumer
+            .consume(resultSet)
+            <* prepareStatement.close()
+      yield result
+    }
