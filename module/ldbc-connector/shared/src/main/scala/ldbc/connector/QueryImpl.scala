185a53d7c7eaf019a3ee74208d7c7d9619c7fc26
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-23 22:21:27 +0900

    Action sbt scalafmtAll

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/QueryImpl.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/QueryImpl.scala
@@ -7,7 +7,7 @@
 package ldbc.connector
 
 import cats.syntax.all.*
-import cats.effect.{Temporal, Resource}
+import cats.effect.{ Temporal, Resource }
 import cats.effect.kernel.Resource.ExitCase
 
 import ldbc.sql.*
@@ -16,31 +16,32 @@ private[ldbc] case class QueryImpl[F[_]: Temporal, T](run: Connection[F] => F[T]
 
   override def readOnly(connection: Connection[F]): F[T] =
     for
-      _          <- connection.setReadOnly(true)
+      _      <- connection.setReadOnly(true)
       result <- run(connection)
     yield result
 
   override def autoCommit(connection: Connection[F]): F[T] =
     for
-      _ <- connection.setReadOnly(false)
-      _ <- connection.setAutoCommit(true)
+      _      <- connection.setReadOnly(false)
+      _      <- connection.setAutoCommit(true)
       result <- run(connection)
     yield result
 
   override def transaction(connection: Connection[F]): F[T] =
     val acquire = connection.setReadOnly(false) >> connection.setAutoCommit(false) >> Temporal[F].pure(connection)
 
-    val release = (connection: Connection[F], exitCase: ExitCase) => exitCase match {
-      case ExitCase.Errored(ex) => connection.rollback() *> Temporal[F].raiseError(ex)
-      case _ => connection.commit()
-    }
+    val release = (connection: Connection[F], exitCase: ExitCase) =>
+      exitCase match {
+        case ExitCase.Errored(ex) => connection.rollback() *> Temporal[F].raiseError(ex)
+        case _                    => connection.commit()
+      }
 
     Resource.makeCase(acquire)(release).use(run)
 
   override def rollback(connection: Connection[F]): F[T] =
     for
-      _ <- connection.setReadOnly(false)
-      _ <- connection.setAutoCommit(false)
+      _      <- connection.setReadOnly(false)
+      _      <- connection.setAutoCommit(false)
       result <- run(connection)
-      _ <- connection.rollback()
+      _      <- connection.rollback()
     yield result
--- module/ldbc-sql/src/main/scala/ldbc/sql/Query.scala
+++ module/ldbc-sql/src/main/scala/ldbc/sql/Query.scala
@@ -15,18 +15,22 @@ trait Query[F[_], T]:
   def map[B](f: T => B)(using Functor[F]): Query[F, B] =
     new Query[F, B]:
       override def run: Connection[F] => F[B] = conn => Functor[F].map(Query.this.run(conn))(f)
-      override def readOnly(connection: Connection[F]): F[B] = Functor[F].map(Query.this.readOnly(connection))(f)
-      override def autoCommit(connection: Connection[F]): F[B] = Functor[F].map(Query.this.autoCommit(connection))(f)
+      override def readOnly(connection:    Connection[F]): F[B] = Functor[F].map(Query.this.readOnly(connection))(f)
+      override def autoCommit(connection:  Connection[F]): F[B] = Functor[F].map(Query.this.autoCommit(connection))(f)
       override def transaction(connection: Connection[F]): F[B] = Functor[F].map(Query.this.transaction(connection))(f)
-      override def rollback(connection: Connection[F]): F[B] = Functor[F].map(Query.this.rollback(connection))(f)
+      override def rollback(connection:    Connection[F]): F[B] = Functor[F].map(Query.this.rollback(connection))(f)
 
   def flatMap[B](f: T => Query[F, B])(using Monad[F]): Query[F, B] =
     new Query[F, B]:
       override def run: Connection[F] => F[B] = conn => Monad[F].flatMap(Query.this.run(conn))(a => f(a).run(conn))
-      override def readOnly(connection: Connection[F]): F[B] = Monad[F].flatMap(Query.this.readOnly(connection))(a => f(a).readOnly(connection))
-      override def autoCommit(connection: Connection[F]): F[B] = Monad[F].flatMap(Query.this.autoCommit(connection))(a => f(a).autoCommit(connection))
-      override def transaction(connection: Connection[F]): F[B] = Monad[F].flatMap(Query.this.transaction(connection))(a => f(a).transaction(connection))
-      override def rollback(connection: Connection[F]): F[B] = Monad[F].flatMap(Query.this.rollback(connection))(a => f(a).rollback(connection))
+      override def readOnly(connection: Connection[F]): F[B] =
+        Monad[F].flatMap(Query.this.readOnly(connection))(a => f(a).readOnly(connection))
+      override def autoCommit(connection: Connection[F]): F[B] =
+        Monad[F].flatMap(Query.this.autoCommit(connection))(a => f(a).autoCommit(connection))
+      override def transaction(connection: Connection[F]): F[B] =
+        Monad[F].flatMap(Query.this.transaction(connection))(a => f(a).transaction(connection))
+      override def rollback(connection: Connection[F]): F[B] =
+        Monad[F].flatMap(Query.this.rollback(connection))(a => f(a).rollback(connection))
 
   /**
    * Functions for managing the processing of connections in a read-only manner.
@@ -54,32 +58,38 @@ object Query:
     override def map[A, B](fa: Query[F, A])(f: A => B): Query[F, B] =
       new Query[F, B]:
         override def run: Connection[F] => F[B] = conn => Functor[F].map(fa.run(conn))(f)
-        override def readOnly(connection: Connection[F]): F[B] = Functor[F].map(fa.readOnly(connection))(f)
-        override def autoCommit(connection: Connection[F]): F[B] = Functor[F].map(fa.autoCommit(connection))(f)
+        override def readOnly(connection:    Connection[F]): F[B] = Functor[F].map(fa.readOnly(connection))(f)
+        override def autoCommit(connection:  Connection[F]): F[B] = Functor[F].map(fa.autoCommit(connection))(f)
         override def transaction(connection: Connection[F]): F[B] = Functor[F].map(fa.transaction(connection))(f)
-        override def rollback(connection: Connection[F]): F[B] = Functor[F].map(fa.rollback(connection))(f)
+        override def rollback(connection:    Connection[F]): F[B] = Functor[F].map(fa.rollback(connection))(f)
 
   given [F[_]: Monad]: Monad[[T] =>> Query[F, T]] with
     override def pure[A](x: A): Query[F, A] =
       new Query[F, A]:
         override def run: Connection[F] => F[A] = _ => Monad[F].pure(x)
-        override def readOnly(connection: Connection[F]): F[A] = Monad[F].pure(x)
-        override def autoCommit(connection: Connection[F]): F[A] = Monad[F].pure(x)
+        override def readOnly(connection:    Connection[F]): F[A] = Monad[F].pure(x)
+        override def autoCommit(connection:  Connection[F]): F[A] = Monad[F].pure(x)
         override def transaction(connection: Connection[F]): F[A] = Monad[F].pure(x)
-        override def rollback(connection: Connection[F]): F[A] = Monad[F].pure(x)
+        override def rollback(connection:    Connection[F]): F[A] = Monad[F].pure(x)
 
     override def flatMap[A, B](fa: Query[F, A])(f: A => Query[F, B]): Query[F, B] =
       new Query[F, B]:
         override def run: Connection[F] => F[B] = conn => Monad[F].flatMap(fa.run(conn))(a => f(a).run(conn))
-        override def readOnly(connection: Connection[F]): F[B] = Monad[F].flatMap(fa.readOnly(connection))(a => f(a).readOnly(connection))
-        override def autoCommit(connection: Connection[F]): F[B] = Monad[F].flatMap(fa.autoCommit(connection))(a => f(a).autoCommit(connection))
-        override def transaction(connection: Connection[F]): F[B] = Monad[F].flatMap(fa.transaction(connection))(a => f(a).transaction(connection))
-        override def rollback(connection: Connection[F]): F[B] = Monad[F].flatMap(fa.rollback(connection))(a => f(a).rollback(connection))
+        override def readOnly(connection: Connection[F]): F[B] =
+          Monad[F].flatMap(fa.readOnly(connection))(a => f(a).readOnly(connection))
+        override def autoCommit(connection: Connection[F]): F[B] =
+          Monad[F].flatMap(fa.autoCommit(connection))(a => f(a).autoCommit(connection))
+        override def transaction(connection: Connection[F]): F[B] =
+          Monad[F].flatMap(fa.transaction(connection))(a => f(a).transaction(connection))
+        override def rollback(connection: Connection[F]): F[B] =
+          Monad[F].flatMap(fa.rollback(connection))(a => f(a).rollback(connection))
 
     override def tailRecM[A, B](a: A)(f: A => Query[F, Either[A, B]]): Query[F, B] =
       new Query[F, B]:
-        override def run: Connection[F] => F[B] = conn => Monad[F].tailRecM(a)(a =>f(a).run(conn))
+        override def run: Connection[F] => F[B] = conn => Monad[F].tailRecM(a)(a => f(a).run(conn))
         override def readOnly(connection: Connection[F]): F[B] = Monad[F].tailRecM(a)(a => f(a).readOnly(connection))
-        override def autoCommit(connection: Connection[F]): F[B] = Monad[F].tailRecM(a)(a => f(a).autoCommit(connection))
-        override def transaction(connection: Connection[F]): F[B] = Monad[F].tailRecM(a)(a => f(a).transaction(connection))
+        override def autoCommit(connection: Connection[F]): F[B] =
+          Monad[F].tailRecM(a)(a => f(a).autoCommit(connection))
+        override def transaction(connection: Connection[F]): F[B] =
+          Monad[F].tailRecM(a)(a => f(a).transaction(connection))
         override def rollback(connection: Connection[F]): F[B] = Monad[F].tailRecM(a)(a => f(a).rollback(connection))
