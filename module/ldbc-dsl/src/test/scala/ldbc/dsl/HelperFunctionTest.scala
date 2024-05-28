ef32e2a463d1196cb878576de490faf3356acdee
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-29 01:07:35 +0900

    Action sbt scalafmtAll

--- module/ldbc-dsl/src/test/scala/ldbc/dsl/HelperFunctionTest.scala
+++ module/ldbc-dsl/src/test/scala/ldbc/dsl/HelperFunctionTest.scala
@@ -14,7 +14,9 @@ import ldbc.dsl.io.*
 
 class HelperFunctionTest extends munit.CatsEffectSuite:
 
-  test("The statement that constructs VALUES with multiple values of the same type will be the same as the string specified.") {
+  test(
+    "The statement that constructs VALUES with multiple values of the same type will be the same as the string specified."
+  ) {
     val sql = q"INSERT INTO `table` (`column1`, `column2`) " ++ values(NonEmptyList.of(1, 2))
     assertEquals(sql.statement, "INSERT INTO `table` (`column1`, `column2`) VALUES(?),(?)")
   }
@@ -22,7 +24,8 @@ class HelperFunctionTest extends munit.CatsEffectSuite:
   test("A statement that constructs VALUES in multiple sql is the same as the specified string.") {
     case class Value(c1: Int, c2: String)
     val vs: NonEmptyList[Value] = NonEmptyList.of(Value(1, "value1"), Value(2, "value2"))
-    val sql = q"INSERT INTO `table` (`column1`, `column2`) VALUES" ++ comma(vs.map(v => parentheses(p"${v.c1},${v.c2}")))
+    val sql =
+      q"INSERT INTO `table` (`column1`, `column2`) VALUES" ++ comma(vs.map(v => parentheses(p"${ v.c1 },${ v.c2 }")))
     assertEquals(sql.statement, "INSERT INTO `table` (`column1`, `column2`) VALUES(?,?),(?,?)")
   }
 
@@ -61,22 +64,30 @@ class HelperFunctionTest extends munit.CatsEffectSuite:
     assertEquals(sql.statement, "SELECT * FROM `table` WHERE")
   }
 
-  test("A statement that is constructed by ANDing Where clauses with multiple values is the same as the string specified.") {
+  test(
+    "A statement that is constructed by ANDing Where clauses with multiple values is the same as the string specified."
+  ) {
     val sql = q"SELECT * FROM `table` " ++ where("AND", q"`column1` = 1", q"`column2` = 2")
     assertEquals(sql.statement, "SELECT * FROM `table` WHERE `column1` = 1 AND `column2` = 2")
   }
 
-  test("A statement that is constructed by ANDing Where clauses with multiple values is the same as the string specified.") {
+  test(
+    "A statement that is constructed by ANDing Where clauses with multiple values is the same as the string specified."
+  ) {
     val sql = q"SELECT * FROM `table` " ++ where("AND", q"`column1` = 1", None, Some(q"`column2` = 2"))
     assertEquals(sql.statement, "SELECT * FROM `table` WHERE `column1` = 1 AND `column2` = 2")
   }
 
-  test("A statement that is constructed by ORing Where clauses with multiple values is the same as the string specified.") {
+  test(
+    "A statement that is constructed by ORing Where clauses with multiple values is the same as the string specified."
+  ) {
     val sql = q"SELECT * FROM `table` " ++ where("OR", q"`column1` = 1", q"`column2` = 2")
     assertEquals(sql.statement, "SELECT * FROM `table` WHERE `column1` = 1 OR `column2` = 2")
   }
 
-  test("A statement that is constructed by ORing Where clauses with multiple values is the same as the string specified.") {
+  test(
+    "A statement that is constructed by ORing Where clauses with multiple values is the same as the string specified."
+  ) {
     val sql = q"SELECT * FROM `table` " ++ where("OR", q"`column1` = 1", None, Some(q"`column2` = 2"))
     assertEquals(sql.statement, "SELECT * FROM `table` WHERE `column1` = 1 OR `column2` = 2")
   }
