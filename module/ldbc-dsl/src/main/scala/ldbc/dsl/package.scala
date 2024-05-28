3c50c9e14934ac7168234a3f49b5014d1e8168f4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-05-28 23:51:06 +0900

    Added build sql helper functions

--- module/ldbc-dsl/src/main/scala/ldbc/dsl/package.scala
+++ module/ldbc-dsl/src/main/scala/ldbc/dsl/package.scala
@@ -6,8 +6,13 @@
 
 package ldbc
 
+import cats.{Foldable, Functor, Reducible}
+import cats.data.NonEmptyList
+import cats.syntax.all.*
+
 import cats.effect.{ IO, Sync }
 
+import ldbc.sql.*
 import ldbc.dsl.syntax.*
 
 package object dsl:
@@ -16,7 +21,80 @@ package object dsl:
     extends StringContextSyntax[F],
             ConnectionSyntax[F],
             QuerySyntax[F],
-            CommandSyntax[F]
+            CommandSyntax[F]:
+
+    /** Returns `VALUES (sql0), (sql1), ...`. */
+    def values[M[_]: Reducible, T](vs: M[T])(using param: Parameter[F, T]): SQL[F] =
+      q"VALUES" ++ comma(vs.toNonEmptyList.map(v => parentheses(sql"$v")))
+
+    /** Returns `VALUES (sql0, sql1, ...)`. */
+    def values[M[_] : Reducible](vs: M[SQL[F]]): SQL[F] =
+      q"VALUES" ++ parentheses(comma(vs.toNonEmptyList))
+
+    /** Returns `(sql IN (sql0, sql1, ...))`. */
+    def in[M[_]: Reducible: Functor, T](vs: M[T])(using param: Parameter[F, T]): SQL[F] =
+      q"IN" ++ parentheses(comma(vs.toNonEmptyList.map(v => sql"$v")))
+
+    /** Returns `(sql1 AND sql2 AND ... sql*)` for a non-empty collection. */
+    def and[M[_]: Reducible](vs: M[SQL[F]], grouping: Boolean = true): SQL[F] =
+      val expr = vs.nonEmptyIntercalate(q" AND")
+      if grouping then parentheses(expr) else expr
+
+    /** Returns `(sql1 AND sql2 AND ... sql*)`. */
+    def and(s1: SQL[F], s2: SQL[F], ss: SQL[F]*): SQL[F] =
+      and(NonEmptyList(s1, s2 :: ss.toList))
+
+    /** Returns `(sql1 AND sql2 AND ... sql*)` for a non-empty collection. */
+    def andOpt[M[_]: Foldable](vs: M[SQL[F]], grouping: Boolean = true): SQL[F] =
+      NonEmptyList.fromFoldable(vs).map(nel => and(nel, grouping)).getOrElse(q"")
+
+    /** Returns `(sql1 AND sql2 AND ... sql*)` for all defined sql, returning Empty SQL if there are no defined sql */
+    def andOpt(s1: Option[SQL[F]], s2: Option[SQL[F]], ss: Option[SQL[F]]*): SQL[F] =
+      andOpt((s1 :: s2 :: ss.toList).flatten)
+
+    /** Returns `(sql1 OR sql2 OR ... sql*)` for a non-empty collection. */
+    def or[M[_]: Reducible](vs: M[SQL[F]], grouping: Boolean = true): SQL[F] =
+      val expr = vs.nonEmptyIntercalate(q" OR")
+      if grouping then parentheses(expr) else expr
+
+    /** Returns `(sql1 OR sql2 OR ... sql*)`. */
+    def or(s1: SQL[F], s2: SQL[F], ss: SQL[F]*): SQL[F] =
+      or(NonEmptyList(s1, s2 :: ss.toList))
+
+    /** Returns `(sql1 OR sql2 OR ... sql*)` for all defined sql, returning Empty SQL if there are no defined sql */
+    def orOpt[M[_]: Foldable](vs: M[SQL[F]], grouping: Boolean = true): SQL[F] =
+      NonEmptyList.fromFoldable(vs).map(nel => or(nel, grouping)).getOrElse(q"")
+
+    /** Returns `(sql1 OR sql2 OR ... sql*)` for all defined sql, returning Empty SQL if there are no defined sql */
+    def orOpt(s1: Option[SQL[F]], s2: Option[SQL[F]], ss: Option[SQL[F]]*): SQL[F] =
+      orOpt((s1 :: s2 :: ss.toList).flatten)
+
+    /** Returns `WHERE sql1, sql2, ... sql*`. */
+    def where(bind: "AND" | "OR", s1: SQL[F], s2: SQL[F], ss: SQL[F]*): SQL[F] =
+      q"WHERE " ++ NonEmptyList(s1, s2 :: ss.toList).nonEmptyIntercalate(q" $bind ")
+
+    def where(bind: "AND" | "OR", s1: SQL[F], s2: Option[SQL[F]], ss: Option[SQL[F]]*): SQL[F] =
+      q"WHERE " ++ NonEmptyList.fromFoldable((Some(s1) :: s2 :: ss.toList).flatten).map(_.nonEmptyIntercalate(q" $bind ")).getOrElse(q"TRUE")
+
+    /** Returns `SET sql1, sql2, ... sql*`. */
+    def set(s1: SQL[F], ss: SQL[F]*): SQL[F] =
+      set(NonEmptyList(s1, ss.toList))
+
+    /** Returns `SET sql1, sql2, ... sql*`. */
+    def set[M[_]: Reducible](fs: M[SQL[F]]): SQL[F] =
+      q"SET" ++ comma(fs)
+
+    /** Returns `(sql)`. */
+    def parentheses(s: SQL[F]): SQL[F] =
+      q"(" ++ s ++ q")"
+
+    /** Returns `sql1, sql2, ... sql*`. */
+    def comma(s1: SQL[F], s2: SQL[F], ss: SQL[F]*): SQL[F] =
+      comma(NonEmptyList(s1, s2 :: ss.toList))
+
+    /** Returns `sql1, sql2, ... sql*`. */
+    def comma[M[_]:  Reducible](ss: M[SQL[F]]): SQL[F] =
+      ss.nonEmptyIntercalate(q",")
 
   /**
    * Top-level imports provide aliases for the most commonly used types and modules. A typical starting set of imports
