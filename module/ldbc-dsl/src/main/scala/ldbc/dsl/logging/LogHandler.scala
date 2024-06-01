8360be362b2185c129ebfc57eeabdb95102adb49
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-06-02 03:00:22 +0900

    Delete unused

--- module/ldbc-dsl/src/main/scala/ldbc/dsl/logging/LogHandler.scala
+++ module/ldbc-dsl/src/main/scala/ldbc/dsl/logging/LogHandler.scala
@@ -7,6 +7,10 @@
 package ldbc.dsl.logging
 
 import cats.Applicative
+import cats.syntax.all.*
+
+import cats.effect.Sync
+import cats.effect.std.Console
 
 /**
  * copied from doobie:
@@ -21,3 +25,29 @@ trait LogHandler[F[_]]:
 object LogHandler:
 
   def noop[F[_]: Applicative]: LogHandler[F] = (logEvent: LogEvent) => Applicative[F].unit
+
+  def console[F[_]: Console: Sync]: LogHandler[F] =
+    case LogEvent.Success(sql, args) =>
+      Console[F].println(
+        s"""Successful Statement Execution:
+           |  $sql
+           |
+           | arguments = [${args.mkString(",")}]
+           |""".stripMargin
+      )
+    case LogEvent.ProcessingFailure(sql, args, failure) =>
+      Console[F].errorln(
+        s"""Failed ResultSet Processing:
+           |  $sql
+           |
+           | arguments = [${args.mkString(",")}]
+           |""".stripMargin
+      ) >> Console[F].printStackTrace(failure)
+    case LogEvent.ExecFailure(sql, args, failure) =>
+      Console[F].errorln(
+        s"""Failed Statement Execution:
+           |  $sql
+           |
+           | arguments = [${args.mkString(",")}]
+           |""".stripMargin
+      ) >> Console[F].printStackTrace(failure)
