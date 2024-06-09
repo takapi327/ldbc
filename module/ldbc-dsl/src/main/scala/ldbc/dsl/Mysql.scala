6eab69234821cd94d8eeee130959fc1b945fc6b1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-06-10 01:34:42 +0900

    Fixed Mysql

--- module/ldbc-dsl/src/main/scala/ldbc/dsl/Mysql.scala
+++ module/ldbc-dsl/src/main/scala/ldbc/dsl/Mysql.scala
@@ -7,6 +7,7 @@
 package ldbc.dsl
 
 import scala.annotation.targetName
+import scala.deriving.Mirror
 
 import cats.data.Kleisli
 import cats.syntax.all.*
@@ -14,6 +15,7 @@ import cats.syntax.all.*
 import cats.effect.Temporal
 
 import ldbc.sql.*
+import ldbc.dsl.statement.Query
 
 /**
  * A model with a query string and parameters to be bound to the query string that is executed by PreparedStatement,
@@ -26,27 +28,30 @@ import ldbc.sql.*
  * @tparam F
  *   The effect type
  */
-case class Mysql[F[_]: Temporal](statement: String, params: List[Parameter.DynamicBinder]) extends SQL[F]:
+case class Mysql[F[_]: Temporal](statement: String, params: List[Parameter.DynamicBinder]) extends SQL:
 
   @targetName("combine")
-  override def ++(sql: SQL[F]): SQL[F] =
+  override def ++(sql: SQL): SQL =
     Mysql[F](statement ++ sql.statement, params ++ sql.params)
 
-  override def to[T](using consumer: ResultSetConsumer[F, T]): Executor[F, T] =
-    Executor.Impl[F, T](
-      statement,
-      params,
-      connection =>
-        for
-          prepareStatement <- connection.prepareStatement(statement)
-          resultSet <- params.zipWithIndex.traverse {
-                         case (param, index) => param.bind[F](prepareStatement, index + 1)
-                       } >> prepareStatement.executeQuery()
-          result <- consumer.consume(resultSet) <* prepareStatement.close()
-        yield result
-    )
+  def query[T](using reader: ResultSetReader[F, T]): Query[F, T] =
+    given Kleisli[F, ResultSet[F], T] = Kleisli(resultSet => reader.read(resultSet, 1))
+    Query.Impl[F, T](statement, params)
 
-  override def update: Executor[F, Int] =
+  inline def query[P <: Product](using mirror: Mirror.ProductOf[P]): Query[F, P] =
+    given Kleisli[F, ResultSet[F], P] = Kleisli { resultSet =>
+      ResultSetReader
+        .fold[F, mirror.MirroredElemTypes]
+        .toList
+        .zipWithIndex
+        .traverse {
+          case (reader: ResultSetReader[F, Any], index) => reader.read(resultSet, index + 1)
+        }
+        .map(list => mirror.fromProduct(Tuple.fromArray(list.toArray)))
+    }
+    Query.Impl[F, P](statement, params)
+
+  def update: Executor[F, Int] =
     Executor.Impl[F, Int](
       statement,
       params,
@@ -54,12 +59,12 @@ case class Mysql[F[_]: Temporal](statement: String, params: List[Parameter.Dynam
         for
           prepareStatement <- connection.prepareStatement(statement)
           result <- params.zipWithIndex.traverse {
-                      case (param, index) => param.bind[F](prepareStatement, index + 1)
-                    } >> prepareStatement.executeUpdate() <* prepareStatement.close()
+            case (param, index) => param.bind[F](prepareStatement, index + 1)
+          } >> prepareStatement.executeUpdate() <* prepareStatement.close()
         yield result
     )
 
-  override def returning[T <: String | Int | Long](using reader: ResultSetReader[F, T]): Executor[F, T] =
+  def returning[T <: String | Int | Long](using reader: ResultSetReader[F, T]): Executor[F, T] =
     given Kleisli[F, ResultSet[F], T] = Kleisli(resultSet => reader.read(resultSet, 1))
 
     Executor.Impl[F, T](
@@ -69,8 +74,8 @@ case class Mysql[F[_]: Temporal](statement: String, params: List[Parameter.Dynam
         for
           prepareStatement <- connection.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)
           resultSet <- params.zipWithIndex.traverse {
-                         case (param, index) => param.bind[F](prepareStatement, index + 1)
-                       } >> prepareStatement.executeUpdate() >> prepareStatement.getGeneratedKeys()
+            case (param, index) => param.bind[F](prepareStatement, index + 1)
+          } >> prepareStatement.executeUpdate() >> prepareStatement.getGeneratedKeys()
           result <- summon[ResultSetConsumer[F, T]].consume(resultSet) <* prepareStatement.close()
         yield result
     )
