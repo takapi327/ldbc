a0d97fe92d1c8cb6a475128691674e5a4e11139e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-06-18 20:02:32 +0900

    Fixed syntax

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/syntax/CommandSyntax.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/syntax/CommandSyntax.scala
@@ -6,43 +6,12 @@
 
 package ldbc.query.builder.syntax
 
-import cats.data.Kleisli
-import cats.syntax.all.*
-
-import cats.effect.Temporal
-
-import ldbc.sql.{ Statement, ResultSet }
 import ldbc.dsl.*
 import ldbc.query.builder.statement.Command
 
-trait CommandSyntax[F[_]: Temporal]:
+trait CommandSyntax[F[_]]:
 
   extension (command: Command)
-    def update: Executor[F, Int] =
-      Executor.Impl[F, Int](
-        command.statement,
-        command.params,
-        connection =>
-          for
-            prepareStatement <- connection.prepareStatement(command.statement)
-            result <- command.params.zipWithIndex.traverse {
-                        case (param, index) => param.bind[F](prepareStatement, index + 1)
-                      } >> prepareStatement.executeUpdate() <* prepareStatement.close()
-          yield result
-      )
+    def update: Executor[F, Int]
 
-    def returning[T <: String | Int | Long](using reader: ResultSetReader[F, T]): Executor[F, T] =
-      given Kleisli[F, ResultSet[F], T] = Kleisli(resultSet => reader.read(resultSet, 1))
-
-      Executor.Impl[F, T](
-        command.statement,
-        command.params,
-        connection =>
-          for
-            prepareStatement <- connection.prepareStatement(command.statement, Statement.RETURN_GENERATED_KEYS)
-            resultSet <- command.params.zipWithIndex.traverse {
-                           case (param, index) => param.bind[F](prepareStatement, index + 1)
-                         } >> prepareStatement.executeUpdate() >> prepareStatement.getGeneratedKeys()
-            result <- summon[ResultSetConsumer[F, T]].consume(resultSet) <* prepareStatement.close()
-          yield result
-      )
+    def returning[T <: String | Int | Long](using reader: ResultSetReader[F, T]): Executor[F, T]
--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/syntax/QuerySyntax.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/syntax/QuerySyntax.scala
@@ -8,50 +8,18 @@ package ldbc.query.builder.syntax
 
 import scala.deriving.Mirror
 
-import cats.data.Kleisli
-import cats.syntax.all.*
-
-import cats.effect.Temporal
-
-import ldbc.sql.*
-
-import ldbc.dsl.*
-import ldbc.dsl.Query as DslQuery
+import ldbc.dsl.{Query as DslQuery, *}
 
 import ldbc.query.builder.statement.Query
 import ldbc.query.builder.interpreter.Tuples
 
-trait QuerySyntax[F[_]: Temporal]:
+trait QuerySyntax[F[_]]:
 
-  extension [T](query: ldbc.query.builder.statement.Query[T])
+  extension [T](query: Query[T])
 
-    @scala.annotation.targetName("simpleQuery")
-    inline def query: DslQuery[F, Tuples.InverseColumnMap[T]] =
-      given Kleisli[F, ResultSet[F], Tuples.InverseColumnMap[T]] = Kleisli { resultSet =>
-        ResultSetReader
-          .fold[F, Tuples.InverseColumnMap[T]]
-          .toList
-          .zipWithIndex
-          .traverse {
-            case (reader: ResultSetReader[F, Any], index) => reader.read(resultSet, index + 1)
-          }
-          .map(list => Tuple.fromArray(list.toArray).asInstanceOf[Tuples.InverseColumnMap[T]])
-      }
-      DslQuery.Impl[F, Tuples.InverseColumnMap[T]](query.statement, query.params)
+    inline def query: DslQuery[F, Tuples.InverseColumnMap[T]]
 
     inline def queryTo[P <: Product](using
       mirror: Mirror.ProductOf[P],
       check:  Tuples.InverseColumnMap[T] =:= mirror.MirroredElemTypes
-    ): DslQuery[F, P] =
-      given Kleisli[F, ResultSet[F], P] = Kleisli { resultSet =>
-        ResultSetReader
-          .fold[F, mirror.MirroredElemTypes]
-          .toList
-          .zipWithIndex
-          .traverse {
-            case (reader: ResultSetReader[F, Any], index) => reader.read(resultSet, index + 1)
-          }
-          .map(list => mirror.fromProduct(Tuple.fromArray(list.toArray)))
-      }
-
-      DslQuery.Impl[F, P](query.statement, query.params)
+    ): DslQuery[F, P]
