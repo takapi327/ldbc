449032505c07ebb944f3cb0092a0ef596eace355
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-07-09 09:14:56 +0900

    Change docs Setup program

--- docs/src/main/scala/00-Setup.scala
+++ docs/src/main/scala/00-Setup.scala
@@ -4,6 +4,8 @@
  * For more information see LICENSE or https://opensource.org/licenses/MIT
  */
 
+import cats.syntax.all.*
+
 import cats.effect.*
 import cats.effect.unsafe.implicits.global
 
@@ -21,20 +23,85 @@ import ldbc.dsl.logging.LogHandler
   given LogHandler[IO] = LogHandler.noop[IO]
   // #given
 
-  // #setup
+  // #setupDatabase
   val createDatabase: Executor[IO, Int] =
-    sql"CREATE DATABASE IF NOT EXISTS todo".update
+    sql"CREATE DATABASE IF NOT EXISTS sandbox_db".update
+  // #setupDatabase
 
-  val createTable: Executor[IO, Int] =
+  // #setupUser
+  val createUser: Executor[IO, Int] =
     sql"""
-         CREATE TABLE IF NOT EXISTS `task` (
-           `id` INT NOT NULL AUTO_INCREMENT,
-           `name` VARCHAR(255) NOT NULL,
-           `done` BOOLEAN NOT NULL DEFAULT FALSE,
-           PRIMARY KEY (`id`)
-         )
-       """.update
-  // #setup
+      CREATE TABLE IF NOT EXISTS `user` (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(50) NOT NULL,
+        `email` VARCHAR(100) NOT NULL,
+        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      )
+    """.update
+  // #setupUser
+
+  // #setupProduct
+  val createProduct: Executor[IO, Int] =
+    sql"""
+      CREATE TABLE IF NOT EXISTS `product` (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(100) NOT NULL,
+        `price` DECIMAL(10, 2) NOT NULL,
+        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      )
+    """.update
+  // #setupProduct
+
+  // #setupOrder
+  val createOrder: Executor[IO, Int] =
+    sql"""
+      CREATE TABLE IF NOT EXISTS `order` (
+        `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_id` INT NOT NULL,
+        `product_id` INT NOT NULL,
+        `order_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `quantity` INT NOT NULL,
+        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES `user` (id),
+        FOREIGN KEY (product_id) REFERENCES `product` (id)
+      )
+    """.update
+  // #setupOrder
+
+  // #insertUser
+  val insertUser: Executor[IO, Int] =
+    sql"""
+      INSERT INTO user (name, email) VALUES
+       ('Alice', 'alice@example.com'),
+       ('Bob', 'bob@example.com'),
+       ('Charlie', 'charlie@example.com')
+    """.update
+  // #insertUser
+
+  // #insertProduct
+  val insertProduct: Executor[IO, Int] =
+    sql"""
+      INSERT INTO product (name, price) VALUES
+      ('Laptop', 999.99),
+      ('Mouse', 19.99),
+      ('Keyboard', 49.99),
+      ('Monitor', 199.99)
+    """.update
+  // #insertProduct
+
+  // #insertOrder
+  val insertOrder: Executor[IO, Int] =
+    sql"""
+      INSERT INTO `order` (user_id, product_id, quantity) VALUES
+      (1, 1, 1), -- Alice ordered 1 Laptop
+      (1, 2, 2), -- Alice ordered 2 Mice
+      (2, 3, 1), -- Bob ordered 1 Keyboard
+      (3, 4, 1) -- Charlie ordered 1 Monitor
+    """.update
+  // #insertOrder
 
   // #connection
   def connection = Connection[IO](
@@ -45,12 +112,24 @@ import ldbc.dsl.logging.LogHandler
   )
   // #connection
 
+  // #setupTables
+  val setUpTables =
+    createUser *> createProduct *> createOrder
+  // #setupTables
+
+  // #insertData
+  val insertData =
+    insertUser *> insertProduct *> insertOrder
+  // #insertData
+
   // #run
   connection
     .use { conn =>
       createDatabase.commit(conn) *>
-        conn.setSchema("todo") *>
-        createTable.commit(conn).map(println(_))
+        conn.setSchema("sandbox_db") *>
+        (setUpTables *> insertData)
+          .transaction(conn)
+          .as(println("Database setup completed"))
     }
     .unsafeRunSync()
   // #run
