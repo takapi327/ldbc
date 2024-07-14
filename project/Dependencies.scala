e086de5fd13ae70d5bd03d21cbe36a308a8e4a9b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-07-14 21:57:47 +0900

    Delete unused cats dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,6 @@ import ScalaVersions.*
 
 object Dependencies {
 
-  val cats       = "org.typelevel" %% "cats-core"   % "2.10.0"
   val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.4"
 
   val schemaspy = "org.schemaspy" % "schemaspy" % "6.2.4"
