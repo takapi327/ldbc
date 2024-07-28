d0ae7ba605b396901f22a294e082c5634ce66bd1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-07-28 17:02:47 +0900

    Added table for benchmark

--- database/benchmark.sql
+++ database/benchmark.sql
@@ -2,12 +2,22 @@ CREATE DATABASE `benchmark` DEFAULT CHARACTER SET utf8mb4;
 
 USE `benchmark`;
 
-CREATE TABLE `jdbc_test` (
+CREATE TABLE `jdbc_statement_test` (
   `c1` INT NOT NULL,
   `c2` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `ldbc_test` (
+CREATE TABLE `jdbc_prepare_statement_test` (
+  `c1` INT NOT NULL,
+  `c2` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `ldbc_statement_test` (
+  `c1` INT NOT NULL,
+  `c2` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `ldbc_prepare_statement_test` (
   `c1` INT NOT NULL,
   `c2` VARCHAR(255) NOT NULL
 );
