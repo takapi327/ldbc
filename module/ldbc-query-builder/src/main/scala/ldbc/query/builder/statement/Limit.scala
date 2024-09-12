30890e901475bdcc2f52bc77b48d04d939d6e200
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-09-13 00:06:59 +0900

    Fixed Limit statement model

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/statement/Limit.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/statement/Limit.scala
@@ -9,50 +9,78 @@ package ldbc.query.builder.statement
 import scala.annotation.targetName
 
 import ldbc.dsl.{ Parameter, SQL }
-import ldbc.dsl.codec.Encoder
+import ldbc.dsl.codec.{Encoder, Decoder}
 import ldbc.query.builder.*
 
-/**
- * A model for constructing LIMIT statements in MySQL.
- *
- * @param statement
- *   SQL statement string
- * @param params
- *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index
- *   only.
- * @tparam T
- *   Union type of column
- */
-private[ldbc] case class Limit[T](
-  statement: String,
-  params:    List[Parameter.Dynamic]
-) extends Query[T],
-          Command:
-
-  @targetName("combine")
-  override def ++(sql: SQL): SQL =
-    Limit(statement ++ sql.statement, params ++ sql.params)
+trait Limit:
+  
+  /** SQL statement string */
+  def statement: String
+  
+  /** A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index only. */
+  def params:    List[Parameter.Dynamic]
+  
+object Limit:
 
   /**
-   * A method for setting the OFFSET condition in a statement.
+   * A model for constructing read-only query LIMIT statements in MySQL.
+   * 
+   * @param statement
+   *   SQL statement string
+   * @param params
+   *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index only.
+   * @param decoder
+   *   Function to convert values from ResultSet to type T.
+   * @tparam T
+   *   Scala types to be converted by Decoder
    */
-  def offset(length: Long): Encoder[Long] ?=> Offset[T] =
-    Offset(
-      statement = statement ++ " OFFSET ?",
-      params    = params :+ Parameter.Dynamic(length)
-    )
+  case class Q[T](
+    statement: String,
+    params:    List[Parameter.Dynamic],
+    decoder: Decoder[T]
+  ) extends Limit, Query[T]:
+    
+    @targetName("combine")
+    override def ++(sql: SQL): SQL = Q[T](statement ++ sql.statement, params ++ sql.params, decoder)
 
-/**
- * Transparent Trait to provide limit method.
- */
-private[ldbc] transparent trait LimitProvider[T]:
-  self: SQL =>
+    def offset(length: Long): Encoder[Long] ?=> Offset[T] =
+      Offset(
+        statement = statement ++ " OFFSET ?",
+        params    = params :+ Parameter.Dynamic(length),
+        decoder = this.decoder
+      )
+
+  transparent trait QueryProvider[T]:
+    self: Query[T] =>
+
+    def limit(length: Long): Encoder[Long] ?=> Limit.Q[T] =
+      Limit.Q(
+        statement = statement ++ " LIMIT ?",
+        params    = params :+ Parameter.Dynamic(length),
+        decoder = self.decoder
+      )
 
   /**
-   * A method for setting the LIMIT condition in a statement.
+   * A model for constructing write-only query LIMIT statements in MySQL.
+   * 
+   * @param statement
+   *   SQL statement string
+   * @param params
+   *   A list of Traits that generate values from Parameter, allowing PreparedStatement to be set to a value by index only.
    */
-  def limit(length: Long): Encoder[Long] ?=> Limit[T] =
-    Limit(
-      statement = statement ++ " LIMIT ?",
-      params    = params :+ Parameter.Dynamic(length)
-    )
+  case class C(
+    statement: String,
+    params:    List[Parameter.Dynamic]
+  ) extends Limit, Command:
+    
+    @targetName("combine")
+    override def ++(sql: SQL): SQL = C(statement ++ sql.statement, params ++ sql.params)
+    
+  transparent trait CommandProvider:
+    self: Command =>
+
+    def limit(length: Long): Encoder[Long] ?=> Limit.C =
+      Limit.C(
+        statement = statement ++ " LIMIT ?",
+        params    = params :+ Parameter.Dynamic(length)
+      )
