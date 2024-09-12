67ab5d9ee044a1c8c562eb3f1254a8094fd2e7c9
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-09-13 00:03:39 +0900

    Added decoder property in Query

--- module/ldbc-query-builder/src/main/scala/ldbc/query/builder/statement/package.scala
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/statement/package.scala
@@ -9,13 +9,26 @@ package ldbc.query.builder
 import scala.annotation.targetName
 
 import ldbc.dsl.*
+import ldbc.dsl.codec.Decoder
 
 package object statement:
 
+  /**
+   * Trait represents a command in MySQL.
+   */
   trait Command extends SQL
   object Command:
     case class Pure(statement: String, params: List[Parameter.Dynamic]) extends Command:
       @targetName("combine")
       override def ++(sql: SQL): SQL = this.copy(statement = statement ++ sql.statement, params = params ++ sql.params)
 
-  trait Query[T] extends SQL
+  /**
+   * Trait represents a read-only query in MySQL.
+   * 
+   * @tparam T
+   *   Scala types to be converted by Decoder
+   */
+  trait Query[T] extends SQL:
+    
+    /** Function to get a value of type T from a ResultSet */
+    def decoder: Decoder[T]
