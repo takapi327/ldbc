9023767d40b3401f5efc54cc2080088c3521945f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-10-03 01:26:50 +0900

    Added Exception Test

--- tests/src/test/scala/ldbc/tests/SQLStringContextQueryTest.scala
+++ tests/src/test/scala/ldbc/tests/SQLStringContextQueryTest.scala
@@ -33,6 +33,32 @@ class LdbcSQLStringContextQueryTest extends SQLStringContextQueryTest:
       ssl      = SSL.Trusted
     )
 
+  test("If the acquired column name and the field name of the class to be mapped are different, an exception is raised.") {
+    case class City(id: Int, title: String)
+
+    interceptIO[ldbc.connector.exception.SQLException](
+      connection.use { conn =>
+        sql"SELECT Id, Name FROM city LIMIT 1"
+          .query[City]
+          .to[Option]
+          .readOnly(conn)
+      }
+    )
+  }
+
+  test("If the number of columns retrieved is different from the number of fields in the class to be mapped, an exception is raised.") {
+    case class City(id: Int, name: String, age: Int)
+
+    interceptIO[ldbc.connector.exception.SQLException](
+      connection.use { conn =>
+        sql"SELECT Id, Name FROM city LIMIT 1"
+          .query[City]
+          .to[Option]
+          .readOnly(conn)
+      }
+    )
+  }
+
 class JdbcSQLStringContextQueryTest extends SQLStringContextQueryTest:
 
   val ds = new MysqlDataSource()
@@ -44,6 +70,32 @@ class JdbcSQLStringContextQueryTest extends SQLStringContextQueryTest:
 
   override def connection: Resource[IO, Connection[IO]] =
     Resource.make(jdbc.connector.MysqlDataSource[IO](ds).getConnection)(_.close())
+
+  test("If the acquired column name and the field name of the class to be mapped are different, an exception is raised.") {
+    case class City(id: Int, title: String)
+
+    interceptIO[java.sql.SQLException](
+      connection.use { conn =>
+        sql"SELECT Id, Name FROM city LIMIT 1"
+          .query[City]
+          .to[Option]
+          .readOnly(conn)
+      }
+    )
+  }
+
+  test("If the number of columns retrieved is different from the number of fields in the class to be mapped, an exception is raised.") {
+    case class City(id: Int, name: String, age: Int)
+
+    interceptIO[java.sql.SQLException](
+      connection.use { conn =>
+        sql"SELECT Id, Name FROM city LIMIT 1"
+          .query[City]
+          .to[Option]
+          .readOnly(conn)
+      }
+    )
+  }
 
 trait SQLStringContextQueryTest extends CatsEffectSuite:
 
