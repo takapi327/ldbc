56f8e5109480ce59dc1981bef60c9a1cc003565a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-11-04 14:56:47 +0900

    Create Offset statement model

---
+++ module/ldbc-schema-macro/src/main/scala/ldbc/schema/statement/Offset.scala
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.schema.statement
+
+import scala.annotation.targetName
+
+import ldbc.dsl.{Parameter, SQL}
+import ldbc.query.builder.Column
+
+case class Offset[A, B](
+  table: A,
+  columns: Column[B],
+  statement: String,
+  params: List[Parameter.Dynamic]
+) extends Query[A, B]:
+
+  @targetName("combine")
+  override def ++(sql: SQL): SQL =
+    this.copy(statement = statement ++ sql.statement, params = params ++ sql.params)
