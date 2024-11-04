4c2a15c7e56f4bd9ff06b6abc4e612fde4774bed
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-11-04 14:34:39 +0900

    Create Select statement model

---
+++ module/ldbc-schema-macro/src/main/scala/ldbc/schema/statement/Select.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.schema.statement
+
+import scala.annotation.targetName
+
+import ldbc.dsl.{Parameter, SQL}
+import ldbc.query.builder.Column
+import ldbc.query.builder.statement.Expression
+
+case class Select[A, B](
+  table: A,
+  columns: Column[B],
+  statement: String,
+  params: List[Parameter.Dynamic]
+) extends Query[A, B]:
+
+  @targetName("combine")
+  override def ++(sql: SQL): SQL =
+    this.copy(statement = statement ++ sql.statement, params = params ++ sql.params)
+
+  def where(func: A => Expression): Where[A, B] =
+    val expression = func(table)
+    Where[A, B](
+      table = table,
+      columns = columns,
+      statement = statement ++ s" WHERE ${ expression.statement }",
+      params = params ++ expression.parameter
+   )
