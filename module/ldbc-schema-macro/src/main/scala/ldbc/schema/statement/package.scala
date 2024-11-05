9ac4b176e56ee05825f5cb1dc493f95cf9e68bdf
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-11-05 21:14:23 +0900

    Remove unused

--- module/ldbc-schema-macro/src/main/scala/ldbc/schema/statement/package.scala
+++ module/ldbc-schema-macro/src/main/scala/ldbc/schema/statement/package.scala
@@ -6,23 +6,17 @@
 
 package ldbc.schema
 
-import ldbc.dsl.Parameter
+import ldbc.dsl.SQL
 import ldbc.dsl.codec.Decoder
-
-// TODO: Scheduled to migrate to schema
-import ldbc.query.builder.statement.Query as QuerySQL
-import ldbc.query.builder.statement.Command as CommandSQL
 
 package object statement:
 
-  trait Query[A, B] extends QuerySQL[B]:
+  trait Query[A, B] extends SQL:
 
     def table: A
 
     def columns: Column[B]
 
-    def params: List[Parameter.Dynamic]
+    def decoder: Decoder[B] = columns.decoder
 
-    override def decoder: Decoder[B] = columns.decoder
-
-  trait Command extends CommandSQL
+  trait Command extends SQL
