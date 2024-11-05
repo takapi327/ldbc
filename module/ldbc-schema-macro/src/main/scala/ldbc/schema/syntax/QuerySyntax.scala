67ae406d64e4334595d627329b800f901e418451
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-11-05 20:59:07 +0900

    Create QuerySyntax

---
+++ module/ldbc-schema-macro/src/main/scala/ldbc/schema/syntax/QuerySyntax.scala
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.schema.syntax
+
+import scala.deriving.Mirror
+
+import ldbc.dsl.{ Query as DslQuery, * }
+
+import ldbc.schema.statement.Query
+
+trait QuerySyntax[F[_]]:
+
+  extension [A, B](query: Query[A, B])
+
+    /**
+     * A method to convert a query to a [[ldbc.dsl.Query]].
+     *
+     * {{{
+     *   Table[User].select(v => (v.name, v.age)).query
+     * }}}
+     *
+     * @return
+     *   A [[ldbc.dsl.Query]] instance
+     */
+    def query: DslQuery[F, A]
+
+    /**
+     * A method to convert a query to a [[ldbc.dsl.Query]].
+     *
+     * {{{
+     *   Table[User].selectAll.queryTo[User]
+     * }}}
+     *
+     * @return
+     *   A [[ldbc.dsl.Query]] instance
+     */
+    inline def queryTo[P <: Product](using
+                                     m1:    Mirror.ProductOf[P],
+                                     m2:    Mirror.ProductOf[A],
+                                     check: m1.MirroredElemTypes =:= m2.MirroredElemTypes
+                                    ): DslQuery[F, P]
