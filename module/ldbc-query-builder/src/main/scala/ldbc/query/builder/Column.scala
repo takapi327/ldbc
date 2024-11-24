5904f9173f4691aa1f8e16754f6395691ed9539c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-11-24 21:32:17 +0900

    Create Column Annotation

---
+++ module/ldbc-query-builder/src/main/scala/ldbc/query/builder/Column.scala
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.query.builder
+
+import scala.annotation.StaticAnnotation
+
+case class Column(name: String) extends StaticAnnotation
