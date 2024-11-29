2d219f06371e338f0563a7d0cb25a03de82bf401
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-11-30 00:45:01 +0900

    Delete unused

--- module/ldbc-statement/src/main/scala/ldbc/statement/internal/QueryConcat.scala
+++ module/ldbc-statement/src/main/scala/ldbc/statement/internal/QueryConcat.scala
@@ -6,35 +6,25 @@
 
 package ldbc.statement.internal
 
-trait QueryConcat[A, B] extends BinaryTupleOp:
+trait QueryConcat[A, B]:
   type Out
 
 object QueryConcat extends LowPriorityTupleConcat2
 
 trait LowPriorityTupleConcat2 extends LowPriorityTupleConcat1:
-  given concatTuples[A, B, AB](using
-    to: TupleOps.JoinAux[A, B, AB],
-    ta: TupleArity[A],
-    tb: TupleArity[B]
-  ): Aux[A, B, AB] =
+  given concatTuples[A, B, AB](using TupleOps.JoinAux[A, B, AB]): Aux[A, B, AB] =
     new QueryConcat[A, B]:
       override type Out = AB
-      override def leftArity:  Int = ta.arity
-      override def rightArity: Int = tb.arity
 
 trait LowPriorityTupleConcat1 extends LowPriorityTupleConcat0:
 
-  given concatSingleAndTuple[A, B, AB](using to: TupleOps.JoinAux[Tuple1[A], B, AB], ta: TupleArity[B]): Aux[A, B, AB] =
+  given concatSingleAndTuple[A, B, AB](using TupleOps.JoinAux[Tuple1[A], B, AB]): Aux[A, B, AB] =
     new QueryConcat[A, B]:
       override type Out = AB
-      override def leftArity:  Int = 1
-      override def rightArity: Int = ta.arity
 
-  given concatTupleAndSingle[A, B, AB](using tc: TupleOps.JoinAux[A, Tuple1[B], AB], ta: TupleArity[A]): Aux[A, B, AB] =
+  given concatTupleAndSingle[A, B, AB](using TupleOps.JoinAux[A, Tuple1[B], AB]): Aux[A, B, AB] =
     new QueryConcat[A, B]:
       override type Out = AB
-      override def leftArity:  Int = ta.arity
-      override def rightArity: Int = 1
 
 trait LowPriorityTupleConcat0:
 
@@ -43,5 +33,3 @@ trait LowPriorityTupleConcat0:
   given concatSingleAndSingle[A, B, AB](using TupleOps.JoinAux[Tuple1[A], Tuple1[B], AB]): Aux[A, B, AB] =
     new QueryConcat[A, B]:
       override type Out = AB
-      override def leftArity:  Int = 1
-      override def rightArity: Int = 1
