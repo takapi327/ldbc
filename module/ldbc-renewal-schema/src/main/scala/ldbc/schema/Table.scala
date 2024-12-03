e9522cbfc9131d226aaf892879daa5148ee6a0db
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-03 18:32:35 +0900

    Update renewal schema Table

--- module/ldbc-renewal-schema/src/main/scala/ldbc/schema/Table.scala
+++ module/ldbc-renewal-schema/src/main/scala/ldbc/schema/Table.scala
@@ -9,15 +9,13 @@ package ldbc.schema
 import ldbc.dsl.codec.Decoder
 import ldbc.statement.AbstractTable
 
-trait Table[T](private[ldbc] val _name: String) extends AbstractTable[T]:
+trait Table[T](val $name: String) extends AbstractTable[T]:
 
   type Column[A] = ldbc.statement.Column[A]
 
   protected final def column[A](name: String)(using Decoder.Elem[A]): Column[A] =
-    ldbc.statement.Column[A](name, _name)
+    ldbc.statement.Column[A](name, $name)
 
-  override def $name: String = _name
+  override final def statement: String = $name
 
-  override final def statement: String = _name
-
-  override def toString: String = s"Table($_name)"
+  override def toString: String = s"Table($$name)"
