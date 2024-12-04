d5e600d69fbdd3b45c2b08ce7693786296d69a55
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-04 21:46:04 +0900

    Create model for test

---
+++ module/ldbc-renewal-schema/src/test/scala/ldbc/schema/model/GovernmentOffice.scala
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.schema.model
+
+import java.time.LocalDate
+
+import ldbc.schema.*
+
+case class GovernmentOffice(
+                             id: Int,
+                             cityId: Int,
+                             name: String,
+                             establishmentDate: Option[LocalDate]
+                           )
+
+class GovernmentOfficeTable extends Table[GovernmentOffice]("government_office"):
+
+  def id: Column[Int] = column[Int]("ID")
+  def cityId: Column[Int] = column[Int]("CityID")
+  def name: Column[String] = column[String]("Name")
+  def establishmentDate: Column[Option[LocalDate]] = column[Option[LocalDate]]("EstablishmentDate")
+
+  override def * : Column[GovernmentOffice] = (id *: cityId *: name *: establishmentDate).to[GovernmentOffice]
