4a19ae5b4ded1d789ebc604811e82cd6c145d4cc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-07 21:56:34 +0900

    Create Reference

---
+++ module/ldbc-renewal-schema/src/main/scala/ldbc/schema/Reference.scala
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.schema
+
+import java.sql.DatabaseMetaData.*
+
+import ldbc.schema.interpreter.*
+
+/**
+ * A model for setting reference options used for foreign key constraints, etc.
+ *
+ * @param table
+ *   Referenced table model
+ * @param keyPart
+ *   List of columns for which the Index key is set
+ * @param onDelete
+ *   Reference action on delete
+ * @param onUpdate
+ *   Reference action on update
+ */
+case class Reference[T <: Tuple](
+  table:    Table[?],
+  keyPart:  T,
+  onDelete: Option[Reference.ReferenceOption],
+  onUpdate: Option[Reference.ReferenceOption]
+):
+
+  private val label: String = "REFERENCES"
+
+  def queryString: String =
+    s"$label `${ table.$name }` (${ keyPart.toList.mkString(", ") })"
+      + onDelete.fold("")(v => s" ON DELETE ${ v.label }")
+      + onUpdate.fold("")(v => s" ON UPDATE ${ v.label }")
+
+  def onDelete(option: Reference.ReferenceOption): Reference[T] =
+    this.copy(onDelete = Some(option))
+
+  def onUpdate(option: Reference.ReferenceOption): Reference[T] =
+    this.copy(onUpdate = Some(option))
+
+object Reference:
+
+  enum ReferenceOption(val label: String, val code: Int):
+    case RESTRICT    extends ReferenceOption("RESTRICT", importedKeyRestrict)
+    case CASCADE     extends ReferenceOption("CASCADE", importedKeyCascade)
+    case SET_NULL    extends ReferenceOption("SET NULL", importedKeySetNull)
+    case NO_ACTION   extends ReferenceOption("NO ACTION", importedKeyNoAction)
+    case SET_DEFAULT extends ReferenceOption("SET DEFAULT", importedKeySetDefault)
