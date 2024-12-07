71d39396342614a6968aa025e11b7d6c68439f91
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-07 22:37:38 +0900

    Create ColumnImpl

---
+++ module/ldbc-renewal-schema/src/main/scala/ldbc/schema/ColumnImpl.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.schema
+
+import ldbc.dsl.codec.Decoder
+import ldbc.statement.Column
+import ldbc.schema.attribute.Attribute
+
+private[ldbc] case class ColumnImpl[T](
+                                        name:       String,
+                                        alias:      Option[String],
+                                        decoder:    Decoder[T],
+                                        dataType:   Option[DataType[T]],
+                                        attributes: List[Attribute[T]],
+                                      ) extends Column[T]:
+
+  override def as(name: String): Column[T] =
+    this.copy(
+      alias   = Some(name),
+      decoder = new Decoder[T]((resultSet, prefix) => decoder.decode(resultSet, Some(name)))
+    )
+
+  override def updateStatement: String = s"$name = ?"
+  override def duplicateKeyUpdateStatement: String = s"$name = VALUES(${alias.getOrElse(name)})"
+
+  /**
+   * Define SQL query string for each Column
+   *
+   * @return
+   *   SQL query string
+   */
+  def queryString: String =
+    dataType.fold(s"`$name`")(dataType => s"`$name` ${dataType.queryString}") + attributes.map(v => s" ${v.queryString}").mkString("")
