9f4dc19ce8e3cc371aac64c15fd17880f2122f6c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-08 15:45:34 +0900

    Fixed DataTypeTest for js

--- module/ldbc-renewal-schema/.js/src/test/scala/ldbc/schema/DataTypeTest.scala
+++ module/ldbc-renewal-schema/.js/src/test/scala/ldbc/schema/DataTypeTest.scala
@@ -17,11 +17,11 @@ class DataTypeTest extends AnyFlatSpec:
 
   it should "The query string generated from the Bit DataType model matches the specified one." in {
     assert(BIT[Byte](1).queryString === "BIT(1) NOT NULL")
-    assert(BIT[Byte](64).DEFAULT("byte".getBytes.head).queryString === "BIT(64) NOT NULL DEFAULT '98'")
+    assert(BIT[Byte](64).DEFAULT("byte".getBytes.head).queryString === "BIT(64) NOT NULL DEFAULT 98")
     assert(BIT[Option[Short]](1).queryString === "BIT(1) NULL")
     assert(BIT[Option[Short]](64).DEFAULT(None).queryString === "BIT(64) NULL DEFAULT NULL")
     assert(BIT[Byte].queryString === "BIT NOT NULL")
-    assert(BIT[Byte].DEFAULT("byte".getBytes.head).queryString === "BIT NOT NULL DEFAULT '98'")
+    assert(BIT[Byte].DEFAULT("byte".getBytes.head).queryString === "BIT NOT NULL DEFAULT 98")
     assert(BIT[Option[Short]].queryString === "BIT NULL")
     assert(BIT[Option[Short]].DEFAULT(None).queryString === "BIT NULL DEFAULT NULL")
   }
@@ -29,12 +29,12 @@ class DataTypeTest extends AnyFlatSpec:
   it should "The query string generated from the Tinyint DataType model matches the specified one." in {
     assert(TINYINT[Byte](1).queryString === "TINYINT(1) NOT NULL")
     assert(TINYINT[Byte](1).UNSIGNED.queryString === "TINYINT(1) UNSIGNED NOT NULL")
-    assert(TINYINT[Byte](64).DEFAULT("byte".getBytes.head).queryString === "TINYINT(64) NOT NULL DEFAULT '98'")
+    assert(TINYINT[Byte](64).DEFAULT("byte".getBytes.head).queryString === "TINYINT(64) NOT NULL DEFAULT 98")
     assert(
       TINYINT[Byte](64)
         .DEFAULT("byte".getBytes.head)
         .UNSIGNED
-        .queryString === "TINYINT(64) UNSIGNED NOT NULL DEFAULT '98'"
+        .queryString === "TINYINT(64) UNSIGNED NOT NULL DEFAULT 98"
     )
     assert(TINYINT[Option[Byte]](1).queryString === "TINYINT(1) NULL")
     assert(TINYINT[Option[Byte]](1).UNSIGNED.queryString === "TINYINT(1) UNSIGNED NULL")
@@ -42,18 +42,18 @@ class DataTypeTest extends AnyFlatSpec:
     assert(
       TINYINT[Option[Byte]](64)
         .DEFAULT("byte".getBytes.headOption)
-        .queryString === "TINYINT(64) NULL DEFAULT '98'"
+        .queryString === "TINYINT(64) NULL DEFAULT 98"
     )
     assert(TINYINT[Option[Byte]](64).DEFAULT(None).queryString === "TINYINT(64) NULL DEFAULT NULL")
     assert(TINYINT[Option[Byte]].DEFAULT(None).UNSIGNED.queryString === "TINYINT UNSIGNED NULL DEFAULT NULL")
     assert(TINYINT[Byte].queryString === "TINYINT NOT NULL")
     assert(TINYINT[Byte].UNSIGNED.queryString === "TINYINT UNSIGNED NOT NULL")
-    assert(TINYINT[Byte].DEFAULT("byte".getBytes.head).queryString === "TINYINT NOT NULL DEFAULT '98'")
+    assert(TINYINT[Byte].DEFAULT("byte".getBytes.head).queryString === "TINYINT NOT NULL DEFAULT 98")
     assert(
       TINYINT[Byte]
         .DEFAULT("byte".getBytes.head)
         .UNSIGNED
-        .queryString === "TINYINT UNSIGNED NOT NULL DEFAULT '98'"
+        .queryString === "TINYINT UNSIGNED NOT NULL DEFAULT 98"
     )
     assert(TINYINT[Option[Byte]].queryString === "TINYINT NULL")
     assert(TINYINT[Option[Byte]].UNSIGNED.queryString === "TINYINT UNSIGNED NULL")
@@ -61,7 +61,7 @@ class DataTypeTest extends AnyFlatSpec:
     assert(
       TINYINT[Option[Byte]]
         .DEFAULT("byte".getBytes.headOption)
-        .queryString === "TINYINT NULL DEFAULT '98'"
+        .queryString === "TINYINT NULL DEFAULT 98"
     )
     assert(TINYINT[Option[Byte]].DEFAULT(None).queryString === "TINYINT NULL DEFAULT NULL")
     assert(TINYINT[Option[Byte]].DEFAULT(None).UNSIGNED.queryString === "TINYINT UNSIGNED NULL DEFAULT NULL")
