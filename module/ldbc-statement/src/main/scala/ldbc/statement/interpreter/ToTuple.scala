a52bad5a90d942b2f5391b44a026b7ddd79ccbb7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-24 01:23:09 +0900

    Create ToTuple type function

---
+++ module/ldbc-statement/src/main/scala/ldbc/statement/interpreter/ToTuple.scala
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.statement.interpreter
+
+type ToTuple[T] <: Tuple = T match
+  case h *: EmptyTuple => Tuple1[h]
+  case h *: t => h *: ToTuple[t]
+  case _ => Tuple1[T]
