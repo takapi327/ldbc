563be6d932cfb9035718ae83889326c54ef2e68f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-24 01:23:54 +0900

    Action sbt scalafmtAll

--- module/ldbc-statement/src/main/scala/ldbc/statement/interpreter/ToTuple.scala
+++ module/ldbc-statement/src/main/scala/ldbc/statement/interpreter/ToTuple.scala
@@ -8,5 +8,5 @@ package ldbc.statement.interpreter
 
 type ToTuple[T] <: Tuple = T match
   case h *: EmptyTuple => Tuple1[h]
-  case h *: t => h *: ToTuple[t]
-  case _ => Tuple1[T]
+  case h *: t          => h *: ToTuple[t]
+  case _               => Tuple1[T]
