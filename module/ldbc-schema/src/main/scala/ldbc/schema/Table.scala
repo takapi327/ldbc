e4fdcd4a98a4922077b9c2eba4aee065a6d95a1f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-12-28 15:27:54 +0900

    Added Encoder property for schema

--- module/ldbc-schema/src/main/scala/ldbc/schema/Table.scala
+++ module/ldbc-schema/src/main/scala/ldbc/schema/Table.scala
@@ -9,7 +9,7 @@ package ldbc.schema
 import scala.language.dynamics
 import scala.deriving.Mirror
 
-import ldbc.dsl.codec.Decoder
+import ldbc.dsl.codec.{Decoder, Encoder}
 import ldbc.statement.{ AbstractTable, Column }
 import ldbc.schema.interpreter.*
 import ldbc.schema.attribute.Attribute
@@ -18,19 +18,20 @@ trait Table[T](val $name: String) extends AbstractTable[T]:
 
   type Column[A] = ldbc.statement.Column[A]
 
-  protected final def column[A](name: String)(using elem: Decoder.Elem[A]): Column[A] =
+  protected final def column[A](name: String)(using elem: Decoder.Elem[A], encoder: Encoder[A]): Column[A] =
     val decoder = new Decoder[A]((resultSet, prefix) => elem.decode(resultSet, prefix.getOrElse(s"${ $name }.$name")))
-    ColumnImpl[A](name, Some(s"${ $name }.$name"), decoder, None, List.empty)
+    ColumnImpl[A](name, Some(s"${ $name }.$name"), decoder, encoder, None, List.empty)
 
-  protected final def column[A](name: String, dataType: DataType[A])(using elem: Decoder.Elem[A]): Column[A] =
+  protected final def column[A](name: String, dataType: DataType[A])(using elem: Decoder.Elem[A], encoder: Encoder[A]): Column[A] =
     val decoder = new Decoder[A]((resultSet, prefix) => elem.decode(resultSet, prefix.getOrElse(s"${ $name }.$name")))
-    ColumnImpl[A](name, Some(s"${ $name }.$name"), decoder, Some(dataType), List.empty)
+    ColumnImpl[A](name, Some(s"${ $name }.$name"), decoder, encoder, Some(dataType), List.empty)
 
   protected final def column[A](name: String, dataType: DataType[A], attributes: Attribute[A]*)(using
-    elem: Decoder.Elem[A]
+    elem: Decoder.Elem[A], 
+                                                                                                encoder: Encoder[A]
   ): Column[A] =
     val decoder = new Decoder[A]((resultSet, prefix) => elem.decode(resultSet, prefix.getOrElse(s"${ $name }.$name")))
-    ColumnImpl[A](name, Some(s"${ $name }.$name"), decoder, Some(dataType), attributes.toList)
+    ColumnImpl[A](name, Some(s"${ $name }.$name"), decoder, encoder, Some(dataType), attributes.toList)
 
   /**
    * Methods for setting key information for tables.
