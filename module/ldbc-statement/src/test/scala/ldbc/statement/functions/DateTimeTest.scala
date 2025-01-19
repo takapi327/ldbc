3c4b5374dd8179ea6a48e18bf4e31782463ca10c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-01-19 17:48:05 +0900

    Create DateTime ADDTIME Test

--- module/ldbc-statement/src/test/scala/ldbc/statement/functions/DateTimeTest.scala
+++ module/ldbc-statement/src/test/scala/ldbc/statement/functions/DateTimeTest.scala
@@ -17,7 +17,9 @@ class DateTimeTest extends AnyFlatSpec, DateTime:
 
   private val c1 = Column.Impl[LocalDate]("local_date")
   private val c2 = Column.Impl[Option[LocalDate]]("local_date")
-  
+  private val c3 = Column.Impl[LocalTime]("local_time")
+  private val c4 = Column.Impl[Option[LocalTime]]("local_time")
+
   it should "Statement generated using the ADDDATE function matches the specified string." in {
     assert(ADDDATE(c1, Interval.DAY(1)).name == "ADDDATE(local_date, INTERVAL 1 DAY)")
     assert(ADDDATE(c1, Interval.MONTH(1)).name == "ADDDATE(local_date, INTERVAL 1 MONTH)")
@@ -28,4 +30,10 @@ class DateTimeTest extends AnyFlatSpec, DateTime:
     assert(ADDDATE(LocalDate.of(2021, 1, 1), Interval.DAY(1)).name == "ADDDATE('2021-01-01', INTERVAL 1 DAY)")
     assert(ADDDATE(LocalDate.of(2021, 1, 1), Interval.MONTH(1)).name == "ADDDATE('2021-01-01', INTERVAL 1 MONTH)")
     assert(ADDDATE(LocalDate.of(2021, 1, 1), Interval.YEAR(1)).name == "ADDDATE('2021-01-01', INTERVAL 1 YEAR)")
+  }
+
+  it should "Statement generated using the ADDTIME function matches the specified string." in {
+    assert(ADDTIME(c3, LocalTime.of(1, 1, 1, 1)).name == "ADDTIME(local_time, '01:01:01.000000001')")
+    assert(ADDTIME(c4, LocalTime.of(1, 1, 1, 1)).name == "ADDTIME(local_time, '01:01:01.000000001')")
+    assert(ADDTIME(LocalTime.of(1, 1, 1), LocalTime.of(1, 1, 1, 1)).name == "ADDTIME('01:01:01', '01:01:01.000000001')")
   }
