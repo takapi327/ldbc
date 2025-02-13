7b4dd1696e76bf4050a38f2405397d2685a94f78
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-02-14 01:10:44 +0900

    Delete Type param upper bound

--- module/ldbc-schema/src/main/scala/ldbc/schema/attribute/AutoInc.scala
+++ module/ldbc-schema/src/main/scala/ldbc/schema/attribute/AutoInc.scala
@@ -9,7 +9,7 @@ package ldbc.schema.attribute
 /**
  * Model for specifying an additional attribute AUTO_INCREMENT for DataType.
  */
-private[ldbc] case class AutoInc[T <: Byte | Short | Int | Long | BigInt | Option[Byte | Short | Int | Long | BigInt]]()
+private[ldbc] case class AutoInc[T]()
   extends Attribute[T]:
 
   override def queryString: String = "AUTO_INCREMENT"
