6f1197d2c44ac8476287c4a4068ab246ab4e04b3
takapi327 <t.takapi0327@gmail.com>
2025-02-25 11:14:49 +0900

    Create HikariCP example project

---
+++ examples/hikariCP/src/main/scala/Main.scala
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+import com.zaxxer.hikari.HikariDataSource
+
+import cats.effect.*
+
+import jdbc.connector.MysqlDataSource
+import ldbc.dsl.io.*
+
+case class City(
+                 id:          Int,
+                 name:        String,
+                 countryCode: String,
+                 district:    String,
+                 population:  Int
+               )
+
+object Main extends ResourceApp.Simple:
+
+  private val ds = new HikariDataSource()
+  ds.setJdbcUrl("jdbc:mysql://127.0.0.1:13306/world")
+  ds.setUsername("ldbc")
+  ds.setPassword("password")
+
+  override def run: Resource[IO, Unit] =
+    (for
+      hikari <- Resource.fromAutoCloseable(IO(ds))
+      connection <- Resource.make(MysqlDataSource[IO](hikari).getConnection)(_.close())
+    yield connection).evalMap { conn =>
+      sql"SELECT * FROM `city` WHERE ID = ${1}"
+        .query[City]
+        .to[Option]
+        .readOnly(conn)
+        .map(_.foreach(println))
+    }
