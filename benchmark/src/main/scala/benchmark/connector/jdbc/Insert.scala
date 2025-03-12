4ddc848e6fd7d876c27a33586ae2ca381875bc99
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-03-12 19:06:45 +0900

    Action sbt scalafmtAll

--- benchmark/src/main/scala/benchmark/connector/jdbc/Insert.scala
+++ benchmark/src/main/scala/benchmark/connector/jdbc/Insert.scala
@@ -99,7 +99,7 @@ class Insert:
       .use { conn =>
         for
           statement <- conn.createStatement()
-          _ <- conn.setAutoCommit(false)
+          _         <- conn.setAutoCommit(false)
           _ <-
             statement.executeUpdate(
               s"INSERT INTO jdbc_statement_test (c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15) VALUES ${ records
