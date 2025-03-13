a2b2693c9c4ff794704c655401284df5d32f0947
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-03-13 15:47:07 +0900

    Create package object

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/package.scala
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc
+
+package object connector:
+  export ldbc.sql.Provider
