9ab6d0d25172dbf9a87938e1fd35a9c93057eddd
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-03-18 15:54:08 +0900

    Change type logHandler to Option

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/Connection.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/Connection.scala
@@ -23,7 +23,7 @@ import fs2.io.net.*
 import org.typelevel.otel4s.trace.Tracer
 
 import ldbc.sql.{ Connection, DatabaseMetaData }
-import ldbc.sql.logging.{ LogEvent, LogHandler }
+import ldbc.sql.logging.{LogEvent, LogHandler}
 
 import ldbc.connector.data.*
 import ldbc.connector.exception.*
@@ -53,34 +53,9 @@ object Connection:
     CapabilitiesFlags.MULTI_FACTOR_AUTHENTICATION
   )
 
-  private[ldbc] def consoleLogger[F[_]: Console: Sync]: LogHandler[F] =
-    case LogEvent.Success(sql, args) =>
-      Console[F].println(
-        s"""Successful Statement Execution:
-           |  $sql
-           |
-           | arguments = [${ args.mkString(",") }]
-           |""".stripMargin
-      )
-    case LogEvent.ProcessingFailure(sql, args, failure) =>
-      Console[F].errorln(
-        s"""Failed ResultSet Processing:
-           |  $sql
-           |
-           | arguments = [${ args.mkString(",") }]
-           |""".stripMargin
-      ) >> Console[F].printStackTrace(failure)
-    case LogEvent.ExecFailure(sql, args, failure) =>
-      Console[F].errorln(
-        s"""Failed Statement Execution:
-           |  $sql
-           |
-           | arguments = [${ args.mkString(",") }]
-           |""".stripMargin
-      ) >> Console[F].printStackTrace(failure)
-
   private def unitBefore[F[_]: Async]: Connection[F] => F[Unit]         = _ => Async[F].unit
   private def unitAfter[F[_]: Async]:  (Unit, Connection[F]) => F[Unit] = (_, _) => Async[F].unit
+  private def noopLogger[F[_]: Applicative]: LogHandler[F] = (logEvent: LogEvent) => Applicative[F].unit
 
   def apply[F[_]: Async: Network: Console: Hashing: UUIDGen](
     host: String,
@@ -185,7 +160,7 @@ object Connection:
                       readTimeout,
                       allowPublicKeyRetrieval,
                       databaseTerm,
-                      logHandler.getOrElse(consoleLogger),
+                      logHandler.getOrElse(noopLogger),
                       before,
                       after
                     )
