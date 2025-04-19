869284a317dba647b27fae4681866947f582a330
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-04-19 18:12:18 +0900

    Added require_secure_transport key

--- database/mysql_user.sql
+++ database/mysql_user.sql
@@ -4,6 +4,6 @@ GRANT ALL PRIVILEGES ON *.* TO ldbc_mysql_native_user;
 CREATE USER 'ldbc_sha256_user'@'%' IDENTIFIED WITH sha256_password BY 'ldbc_sha256_password';
 GRANT ALL PRIVILEGES ON *.* TO ldbc_sha256_user;
 
--- SSL接続を要求するユーザーを作成
+-- Create user requesting SSL connection
 CREATE USER 'ldbc_ssl_user'@'%' IDENTIFIED BY 'securepassword' REQUIRE SSL;
 GRANT ALL PRIVILEGES ON *.* TO 'ldbc_ssl_user'@'%';
