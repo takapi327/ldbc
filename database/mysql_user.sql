b13e58a577f1878bb61ff9a8b77e27f42f8ff43d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-04-19 17:14:05 +0900

    Added user settings for ssl connectiongs

--- database/mysql_user.sql
+++ database/mysql_user.sql
@@ -3,3 +3,7 @@ CREATE USER 'ldbc_mysql_native_user'@'%' IDENTIFIED WITH mysql_native_password B
 GRANT ALL PRIVILEGES ON *.* TO ldbc_mysql_native_user;
 CREATE USER 'ldbc_sha256_user'@'%' IDENTIFIED WITH sha256_password BY 'ldbc_sha256_password';
 GRANT ALL PRIVILEGES ON *.* TO ldbc_sha256_user;
+
+-- SSL接続を要求するユーザーを作成
+CREATE USER 'ldbc_ssl_user'@'%' IDENTIFIED BY 'securepassword' REQUIRE SSL;
+GRANT ALL PRIVILEGES ON *.* TO 'ldbc_ssl_user'@'%';
