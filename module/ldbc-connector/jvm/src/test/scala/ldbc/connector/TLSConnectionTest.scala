da9bbc57b1468783ca2ad7f2eca5b97ed36006ba
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-04-20 13:41:26 +0900

    Change use ssl platform function

--- module/ldbc-connector/jvm/src/test/scala/ldbc/connector/TLSConnectionTest.scala
+++ module/ldbc-connector/jvm/src/test/scala/ldbc/connector/TLSConnectionTest.scala
@@ -6,24 +6,9 @@
 
 package ldbc.connector
 
-import cats.ApplicativeError
-
 import cats.effect.*
 
-import fs2.io.net.tls.TLSContext
-import fs2.io.net.Network
-
 class TLSConnectionTest extends FTestPlatform:
-
-  object KeyStore extends SSL:
-    override def tlsContext[F[_]: Network](using ae: ApplicativeError[F, Throwable]): Resource[F, TLSContext[F]] =
-      Resource.eval(
-        Network[F].tlsContext.fromKeyStoreResource(
-          "keystore.jks",
-          "password".toCharArray,
-          "password".toCharArray
-        )
-      )
 
   test("Verify that you can connect to MySQL with a TLS connection") {
     assertIO(
@@ -35,7 +20,7 @@ class TLSConnectionTest extends FTestPlatform:
           "securepassword",
           "world"
         )
-        .setSSL(KeyStore)
+        .setSSL(SSL.fromKeyStoreResource("keystore.jks", "password".toCharArray, "password".toCharArray))
         .use { conn =>
           for
             statement <- conn.createStatement()
