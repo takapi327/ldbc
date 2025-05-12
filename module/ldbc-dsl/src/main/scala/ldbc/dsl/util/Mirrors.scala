3d8d439d500433cdf50c0655747054eedc3377a4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-05-12 23:55:45 +0900

    Action sbt scalafmtAll

--- module/ldbc-dsl/src/main/scala/ldbc/dsl/util/Mirrors.scala
+++ module/ldbc-dsl/src/main/scala/ldbc/dsl/util/Mirrors.scala
@@ -14,16 +14,16 @@ object Mirrors:
   /** A type function to pull a type parameter it has from a type with one type parameter. */
   type Extract[T] = T match
     case Option[t] => Extract[t]
-    case Array[t] => Extract[t]
-    case List[t] => Extract[t]
-    case Seq[t] => Extract[t]
-    case Set[t] => Extract[t]
-    case _ => T
+    case Array[t]  => Extract[t]
+    case List[t]   => Extract[t]
+    case Seq[t]    => Extract[t]
+    case Set[t]    => Extract[t]
+    case _         => T
 
   /** A type function that derives its type from the type parameters that Option has. */
   type ExtractOption[T] = T match
     case Option[t] => Extract[t]
-    case _ => T
+    case _         => T
 
   inline def summonLabels[T <: Tuple]: List[String] =
     inline erasedValue[T] match
