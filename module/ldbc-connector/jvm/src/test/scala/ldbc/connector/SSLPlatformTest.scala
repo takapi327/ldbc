b89266fc3e63e5651c600af08eb862dd57454c4b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-05-15 08:20:55 +0900

    Update SSLPlatformTest

--- module/ldbc-connector/jvm/src/test/scala/ldbc/connector/SSLPlatformTest.scala
+++ module/ldbc-connector/jvm/src/test/scala/ldbc/connector/SSLPlatformTest.scala
@@ -6,6 +6,10 @@
 
 package ldbc.connector
 
+import java.nio.file.Paths
+import java.security.KeyStore
+import java.io.FileInputStream
+
 import javax.net.ssl.SSLContext
 
 import cats.effect.*
@@ -27,12 +31,44 @@ class SSLPlatformTest extends FTestPlatform:
     assertIOBoolean(result)
   }
 
+  test("SSL.fromKeyStoreFile") {
+    val classLoader = getClass.getClassLoader
+    val resource = classLoader.getResource("keystore.jks")
+    val keyStorePath = Paths.get(resource.toURI)
+    val storePassword = "password".toCharArray
+    val keyPassword = "password".toCharArray
+
+    val ssl = SSL.fromKeyStoreFile(keyStorePath, storePassword, keyPassword)
+    val result = ssl.tlsContext[IO](Network[IO], implicitly).use(_ => IO.pure(true))
+    assertIOBoolean(result)
+  }
+
   test("SSL.fromKeyStoreResource") {
     val resource      = "keystore.jks"
     val storePassword = "password".toCharArray
     val keyPassword   = "password".toCharArray
 
     val ssl    = SSL.fromKeyStoreResource(resource, storePassword, keyPassword)
+    val result = ssl.tlsContext[IO](Network[IO], implicitly).use(_ => IO.pure(true))
+    assertIOBoolean(result)
+  }
+
+  test("SSL.fromKeyStore") {
+    val classLoader = getClass.getClassLoader
+    val resource = classLoader.getResource("keystore.jks")
+    val keyStore = KeyStore.getInstance("JKS")
+    val storePassword = "password".toCharArray
+    val keyPassword = "password".toCharArray
+    
+    // Initialize the KeyStore
+    val inputStream = new FileInputStream(resource.getFile)
+    try {
+      keyStore.load(inputStream, storePassword)
+    } finally {
+      inputStream.close()
+    }
+
+    val ssl = SSL.fromKeyStore(keyStore, keyPassword)
     val result = ssl.tlsContext[IO](Network[IO], implicitly).use(_ => IO.pure(true))
     assertIOBoolean(result)
   }
