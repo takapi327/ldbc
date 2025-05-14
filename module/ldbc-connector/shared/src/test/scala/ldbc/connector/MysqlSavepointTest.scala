39503102671a659207e605382a48bcc897586006
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-05-14 22:30:45 +0900

    Action sbt scalafmtAll

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/MysqlSavepointTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/MysqlSavepointTest.scala
@@ -12,32 +12,32 @@ class MysqlSavepointTest extends FTestPlatform:
 
   test("getSavepointName should return the name provided in the constructor") {
     val savePointName = "test_savepoint"
-    val savepoint = MysqlSavepoint(savePointName)
-    
+    val savepoint     = MysqlSavepoint(savePointName)
+
     assertEquals(savepoint.getSavepointName(), savePointName)
   }
-  
+
   test("getSavepointId should throw SQLFeatureNotSupportedException") {
     val savepoint = MysqlSavepoint("test_savepoint")
-    
+
     val exception = intercept[SQLFeatureNotSupportedException] {
       savepoint.getSavepointId()
     }
-    
+
     assert(exception.getMessage.contains("Only named savepoints are supported"))
   }
-  
+
   test("different savepoints with same name should be distinct objects") {
-    val name = "same_name_savepoint"
+    val name       = "same_name_savepoint"
     val savepoint1 = MysqlSavepoint(name)
     val savepoint2 = MysqlSavepoint(name)
-    
+
     assertNotEquals(savepoint1, savepoint2)
   }
-  
+
   test("different savepoints with different names should have different names") {
     val savepoint1 = MysqlSavepoint("first_savepoint")
     val savepoint2 = MysqlSavepoint("second_savepoint")
-    
+
     assertNotEquals(savepoint1.getSavepointName(), savepoint2.getSavepointName())
   }
