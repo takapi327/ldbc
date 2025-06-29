6c85cb9f9ac862fd4d2153ddc72cf81a77411665
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-06-29 23:49:08 +0900

    Create ServerCursorFetchTest for jdbc

---
+++ tests/jvm/src/test/scala/ldbc/tests/JdbcServerCursorFetchTest.scala
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.tests
+
+import com.mysql.cj.jdbc.MysqlDataSource
+
+import cats.effect.*
+
+import ldbc.sql.*
+import jdbc.connector.*
+
+class JdbcServerCursorFetchTest extends ServerCursorFetchTest:
+
+  val ds = new MysqlDataSource()
+  ds.setServerName(host)
+  ds.setPortNumber(port)
+  ds.setDatabaseName(database)
+  ds.setUser(user)
+  ds.setPassword(password)
+  ds.setUseCursorFetch(true)
+
+  override def provider: Provider[IO] =
+    ConnectionProvider.fromDataSource(ds, ExecutionContexts.synchronous)
