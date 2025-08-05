42358db1b758a595f7f9ec510833a4e9c389f99e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-08-06 00:20:03 +0900

    Delete unused

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/pool/PooledConnection.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/pool/PooledConnection.scala
@@ -6,7 +6,6 @@
 
 package ldbc.connector.pool
 
-
 import cats.effect.*
 
 import ldbc.connector.Connection
@@ -34,47 +33,3 @@ case class PooledConnection[F[_]](
   lastValidatedAt:  Ref[F, Long],
   leakDetection:    Ref[F, Option[Fiber[F, Throwable, Unit]]]
 )
-
-/**
- * State of a pooled connection.
- */
-enum ConnectionState:
-  case Idle
-  case InUse
-  case Removed
-  case Reserved // Intermediate state during acquisition
-
-/**
- * Pool adjustment decision for adaptive sizing.
- */
-enum PoolAdjustment:
-  case Grow(by: Int)
-  case Shrink(by: Int)
-  case NoChange
-
-/**
- * Internal state of the connection pool.
- *
- * @param connections all connections in the pool
- * @param idleConnections IDs of idle connections (for efficient lookup)
- * @param waitQueue queue of waiting acquisition requests
- * @param metrics current pool metrics
- * @param closed whether the pool is closed
- * @tparam F the effect type
- */
-case class PoolState[F[_]](
-  connections:     Vector[PooledConnection[F]],
-  idleConnections: Set[String], // Track idle connection IDs
-  waitQueue:       Vector[Deferred[F, Either[Throwable, Connection[F]]]],
-  metrics:         PoolMetrics,
-  closed:          Boolean = false
-)
-
-object PoolState:
-  def empty[F[_]]: PoolState[F] = PoolState(
-    connections     = Vector.empty,
-    idleConnections = Set.empty,
-    waitQueue       = Vector.empty,
-    metrics         = PoolMetrics.empty,
-    closed          = false
-  )
