efdbe6b687f70cde880fb366a473e9d72b161940
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-08-05 23:19:37 +0900

    Create PoolStatus

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/pool/PoolStatus.scala
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.pool
+
+/**
+ * Current status of the connection pool.
+ *
+ * @param total the total number of connections
+ * @param active the number of active connections
+ * @param idle the number of idle connections
+ * @param waiting the number of waiting requests
+ */
+case class PoolStatus(
+                       total:   Int,
+                       active:  Int,
+                       idle:    Int,
+                       waiting: Int
+                     )
