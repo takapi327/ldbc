72461366ff46dd72015a87275f9fbb7861b76c15
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-08-19 21:55:07 +0900

    Fixed HouseKeeperTest

--- module/ldbc-connector/shared/src/test/scala/ldbc/connector/pool/HouseKeeperTest.scala
+++ module/ldbc-connector/shared/src/test/scala/ldbc/connector/pool/HouseKeeperTest.scala
@@ -25,9 +25,10 @@ class HouseKeeperTest extends FTestPlatform:
     val testConfig = config
       .setMinConnections(2)
       .setMaxConnections(5)
-      .setMaxLifetime(30.seconds)       // Minimum allowed lifetime
+      .setMaxLifetime(40.seconds)       // Minimum allowed lifetime
       .setMaintenanceInterval(1.second) // Minimum allowed interval
       .setIdleTimeout(20.seconds)       // Must be less than maxLifetime
+      .setKeepaliveTime(30.seconds) // Must be less than maxLifetime
 
     val resource = for
       tracker <- Resource.eval(PoolMetricsTracker.inMemory[IO])
