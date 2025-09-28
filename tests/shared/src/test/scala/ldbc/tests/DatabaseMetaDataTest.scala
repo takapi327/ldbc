fa76b62fecfbc87a54375e7543550292e87ad188
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-09-29 00:14:27 +0900

    Fixed LdbcDatabaseMetaDataTest

--- tests/shared/src/test/scala/ldbc/tests/DatabaseMetaDataTest.scala
+++ tests/shared/src/test/scala/ldbc/tests/DatabaseMetaDataTest.scala
@@ -180,7 +180,7 @@ trait DatabaseMetaDataTest extends CatsEffectSuite:
         for metaData <- conn.getMetaData()
         yield metaData.getDriverMinorVersion()
       },
-      if prefix == "jdbc" then 4 else 3
+      if prefix == "jdbc" then 2 else 4
     )
   }
 
