a5a17e97bb11ad04cb0294b6934fc17b59f1e754
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-10-05 19:42:50 +0900

    Action sbt scalafmtAll

--- module/ldbc-zio-interop/src/main/scala/ldbc/zio/interop/package.scala
+++ module/ldbc-zio-interop/src/main/scala/ldbc/zio/interop/package.scala
@@ -18,7 +18,7 @@ import zio.interop.catz.*
 
 package object interop:
   implicit def consoleToZIO: cats.effect.std.Console[Task] = cats.effect.std.Console.make[Task]
-  implicit def uuidGenToZIO: UUIDGen[Task] = new UUIDGen[Task]:
+  implicit def uuidGenToZIO: UUIDGen[Task]                 = new UUIDGen[Task]:
     override def randomUUID: Task[UUID] = ZIO.attempt(UUID.randomUUID())
   implicit def hashingToZIO: Hashing[Task] = Hashing.forSync[Task]
   implicit def networkToZIO: Network[Task] = Network.forAsync[Task]
