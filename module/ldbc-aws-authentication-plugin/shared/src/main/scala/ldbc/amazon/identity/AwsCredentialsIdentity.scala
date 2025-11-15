d619e7c0d4f32c81e4ebcf921be069867d9aed67
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-11-15 19:21:27 +0900

    Create AwsCredentialsProvider

--- module/ldbc-aws-authentication-plugin/shared/src/main/scala/ldbc/amazon/identity/AwsCredentialsIdentity.scala
+++ module/ldbc-aws-authentication-plugin/shared/src/main/scala/ldbc/amazon/identity/AwsCredentialsIdentity.scala
@@ -2,6 +2,14 @@ package ldbc.amazon.identity
 
 import ldbc.amazon.identity.internal.DefaultAwsCredentialsIdentity
 
+/**
+ * Provides access to the AWS credentials used for accessing services: AWS access key ID and secret access key. These
+ * credentials are used to securely sign requests to services (e.g., AWS services) that use them for authentication.
+ *
+ * <p>For more details on AWS access keys, see:
+ * <a href="https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html#access-keys-and-secret-access-keys">
+ * https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html#access-keys-and-secret-access-keys</a></p>
+ */
 trait AwsCredentialsIdentity extends Identity:
 
   /**
---
+++ module/ldbc-aws-authentication-plugin/shared/src/main/scala/ldbc/amazon/identity/AwsCredentialsProvider.scala
@@ -0,0 +1,15 @@
+package ldbc.amazon.identity
+
+trait AwsCredentialsProvider:
+
+  /**
+   * Returns [[AwsCredentials]] that can be used to authorize an AWS request. Each implementation of AWSCredentialsProvider
+   * can choose its own strategy for loading credentials. For example, an implementation might load credentials from an existing
+   * key management system, or load new credentials when credentials are rotated.
+   *
+   * <p>If an error occurs during the loading of credentials or credentials could not be found, a runtime exception will be
+   * raised.</p>
+   *
+   * @return AwsCredentials which the caller can use to authorize an AWS request.
+   */
+  def resolveCredentials(): AwsCredentials
