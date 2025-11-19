13e942d2aa9a35f3a784d6738c62347ec8286485
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-11-20 00:20:30 +0900

    Added licence header

--- module/ldbc-aws-authentication-plugin/shared/src/main/scala/ldbc/amazon/exception/SdkClientException.scala
+++ module/ldbc-aws-authentication-plugin/shared/src/main/scala/ldbc/amazon/exception/SdkClientException.scala
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
 package ldbc.amazon.exception
 
 class SdkClientException(message: String) extends RuntimeException:
