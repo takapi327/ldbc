f18c5124fa290124b63b8d16aa9041d791049831
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-11-29 16:59:36 +0900

    Create MysqlClearPasswordPlugin

---
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/authenticator/MysqlClearPasswordPlugin.scala
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.authenticator
+
+import java.nio.charset.StandardCharsets
+
+import scodec.bits.ByteVector
+
+import cats.effect.kernel.Sync
+
+class MysqlClearPasswordPlugin[F[_]: Sync] extends AuthenticationPlugin[F]:
+
+  override def name:                                                  String        = "mysql_clear_password"
+  override def hashPassword(password: String, scramble: Array[Byte]): F[ByteVector] =
+    if password.isEmpty then Sync[F].pure(ByteVector.empty)
+    else Sync[F].delay(ByteVector(password.getBytes(StandardCharsets.UTF_8)))
+
+object MysqlClearPasswordPlugin:
+  def apply[F[_]: Sync](): MysqlClearPasswordPlugin[F] = new MysqlClearPasswordPlugin[F]()
