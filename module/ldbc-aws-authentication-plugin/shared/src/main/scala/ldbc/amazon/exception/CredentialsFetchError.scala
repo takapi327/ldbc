038556811e58dee63863c7e487ce20836bb3b8af
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2025-12-01 01:00:21 +0900

    Create CredentialsFetchError

---
+++ module/ldbc-aws-authentication-plugin/shared/src/main/scala/ldbc/amazon/exception/CredentialsFetchError.scala
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.amazon.exception
+
+class CredentialsFetchError(message: String) extends Exception:
+  override def getMessage: String = message
