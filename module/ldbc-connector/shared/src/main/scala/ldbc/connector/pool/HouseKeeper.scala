191b8972d60afdaed966f12ac9974fe753d335ec
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-02-01 20:18:39 +0900

    Fix idle connections bag state

--- module/ldbc-connector/shared/src/main/scala/ldbc/connector/pool/HouseKeeper.scala
+++ module/ldbc-connector/shared/src/main/scala/ldbc/connector/pool/HouseKeeper.scala
@@ -101,12 +101,37 @@ object HouseKeeper:
         else
           for
             now <- Clock[F].realTime.map(_.toMillis)
+            _   <- reconcileIdleConnections(pool)
             _   <- removeExpiredConnections(pool, now)
             _   <- removeIdleConnections(pool)
             _   <- validateIdleConnections(pool, now)
             _   <- ensureMinimumConnections(pool)
             _   <- updateMetrics(pool)
           yield ()
+      }
+
+    /**
+     * Reconcile idleConnections set with actual connections.
+     * This ensures consistency by removing orphaned IDs from idleConnections
+     * that no longer have corresponding connections in the pool.
+     */
+    private def reconcileIdleConnections(
+      pool: PooledDataSource[F]
+    ): F[Unit] =
+      pool.poolState.modify { state =>
+        val validIds           = state.connections.map(_.id).toSet
+        val reconciledIdleConns = state.idleConnections.intersect(validIds)
+
+        if reconciledIdleConns.size != state.idleConnections.size then
+          // Found orphaned IDs, clean them up
+          (state.copy(idleConnections = reconciledIdleConns), true)
+        else
+          (state, false)
+      }.flatMap { wasReconciled =>
+        if wasReconciled then
+          pool.poolLogger.debug("Reconciled idleConnections: removed orphaned connection IDs")
+        else
+          Temporal[F].unit
       }
 
     /**
