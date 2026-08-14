1be4f3ba984aa05998aaae584d80e4633433bdfc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-08-14 19:04:35 +0900

    Create FxRuntime test

---
+++ module/ldbc-fx/shared/src/test/scala/ldbc/fx/FxRuntimeTest.scala
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.fx
+
+import scala.concurrent.{ Future, Promise }
+import scala.concurrent.duration.*
+
+/**
+ * Cross-platform (JVM / JS / Native) tests for the platform [[FxRuntime]] behind `Fx.sleep`
+ * (`scheduleOnce`) and `Fx.blocking` (`executeBlocking`). Tests are `Future`-based so they run on
+ * the single-threaded JS runtime as well.
+ */
+class FxRuntimeTest extends munit.FunSuite:
+
+  given scala.concurrent.ExecutionContext = munitExecutionContext
+
+  private def toFuture[A](fx: Fx[A]): Future[A] =
+    val promise = Promise[A]()
+    fx.unsafeRun(result => promise.complete(result.toTry))
+    promise.future
+
+  test("scheduleOnce: sleep completes with unit"):
+    toFuture(Fx.sleep(20.millis)).map(u => assertEquals(u, ()))
+
+  test("scheduleOnce: sleep then flatMap sequences"):
+    toFuture(Fx.sleep(10.millis).flatMap(_ => Fx.pure(7))).map(v => assertEquals(v, 7))
+
+  test("executeBlocking: blocking returns the computed value"):
+    toFuture(Fx.blocking(6 * 7)).map(v => assertEquals(v, 42))
+
+  test("executeBlocking: blocking failure propagates"):
+    toFuture(Fx.blocking[Int](throw new RuntimeException("boom"))).failed.map(e => assertEquals(e.getMessage, "boom"))
