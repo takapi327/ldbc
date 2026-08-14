c9a8ff6ecdfb9b44ed28a86a4d662565de738c77
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-08-15 06:04:22 +0900

    Create PlatformSSLContext alias

---
+++ module/ldbc-net/native/src/main/scala/ldbc/net/PlatformSSLContext.scala
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.net
+
+/**
+ * There is no platform TLS context on this platform, so [[SSL.Platform]] cannot be constructed here
+ * (its `context` field is uninhabited). Custom trust is expressed via [[SSL.Custom]] instead.
+ */
+type PlatformSSLContext = Nothing
