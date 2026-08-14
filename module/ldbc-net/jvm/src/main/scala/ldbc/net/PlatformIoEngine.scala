c830640eb51aca4d8b11e3efacfad000f11773a2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-08-15 06:08:23 +0900

    Create PlatformIoEngine

---
+++ module/ldbc-net/jvm/src/main/scala/ldbc/net/PlatformIoEngine.scala
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2023-2025 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.net
+
+import java.nio.channels.Selector
+
+/** JVM platform entry point: starts the daemon selector thread and exposes the [[SelectorEngine]]. */
+private[net] object PlatformIoEngine:
+  lazy val global: IoEngine =
+    val engine = new SelectorEngine(Selector.open())
+    val th     = new Thread(() => engine.loop(), "fx-io-engine")
+    th.setDaemon(true)
+    th.start()
+    engine
