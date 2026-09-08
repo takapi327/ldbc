19eaf49d27aad75a35e2b36421134621a9a53c18
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-09-09 00:49:34 +0900

    Update stream test

--- tests/shared/src/test/scala/ldbc/tests/StreamQueryTest.scala
+++ tests/shared/src/test/scala/ldbc/tests/StreamQueryTest.scala
@@ -14,8 +14,6 @@ import munit.*
 
 import ldbc.dsl.*
 
-import ldbc.connector.*
-
 import ldbc.catseffect.*
 import ldbc.fx.Fx
 import ldbc.Connector
@@ -23,6 +21,7 @@ import ldbc.Connector
 import zio.Task
 
 class LdbcStreamQueryTest extends StreamQueryTest[IO] with IODatabaseSuite:
+  import ldbc.connector.{ MySQLDataSource, SSL }
 
   private val datasource = MySQLDataSource
     .build[IO](host, port, user)
