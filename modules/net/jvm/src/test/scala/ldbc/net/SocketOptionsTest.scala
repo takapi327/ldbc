52e0186b82255f5433c9d24bdbf50b4fcc880353
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-09-20 15:30:20 +0900

    Update test

--- modules/net/jvm/src/test/scala/ldbc/net/SocketOptionsTest.scala
+++ modules/net/jvm/src/test/scala/ldbc/net/SocketOptionsTest.scala
@@ -6,71 +6,45 @@
 
 package ldbc.net
 
-import java.net.{ ServerSocket, StandardSocketOptions }
-import java.util.concurrent.{ CountDownLatch, TimeUnit }
-import java.util.concurrent.atomic.AtomicReference
+import java.net.StandardSocketOptions
+import java.nio.channels.SocketChannel
 
 class SocketOptionsTest extends munit.FunSuite:
 
-  private val engine = PlatformRawEngine.global
-
-  private def acceptingServer(): Int =
-    val server = new ServerSocket(0)
-    val thread = new Thread(() =>
-      try while true do server.accept()
-      catch { case _: Throwable => () }
-    )
-    thread.setDaemon(true)
-    thread.start()
-    server.getLocalPort
-
-  private lazy val port = acceptingServer()
-
-  private def connectWith(options: SocketOptions): NioRawSocket =
-    val ref   = new AtomicReference[Either[Throwable, RawSocket]](null)
-    val latch = new CountDownLatch(1)
-    engine.connect("127.0.0.1", port, options, r => { ref.set(r); latch.countDown() })
-    assert(latch.await(10, TimeUnit.SECONDS), "connect がタイムアウトした")
-    ref.get().fold(e => fail(s"connect 失敗: $e"), _.asInstanceOf[NioRawSocket])
+  private def applied[A](options: SocketOptions)(read: SocketChannel => A): A =
+    val ch = SocketChannel.open()
+    try
+      NioRawEngine.withOptions(ch, options)
+      read(ch)
+    finally ch.close()
 
   test("TCP_NODELAY and SO_KEEPALIVE follow the given options"):
-    val on  = connectWith(SocketOptions(noDelay = true, keepAlive = true))
-    val off = connectWith(SocketOptions(noDelay = false, keepAlive = false))
-    try
-      assertEquals(on.channel.getOption(StandardSocketOptions.TCP_NODELAY), java.lang.Boolean.TRUE)
-      assertEquals(on.channel.getOption(StandardSocketOptions.SO_KEEPALIVE), java.lang.Boolean.TRUE)
-      assertEquals(off.channel.getOption(StandardSocketOptions.TCP_NODELAY), java.lang.Boolean.FALSE)
-      assertEquals(off.channel.getOption(StandardSocketOptions.SO_KEEPALIVE), java.lang.Boolean.FALSE)
-    finally
-      on.close()
-      off.close()
+    applied(SocketOptions(noDelay = true, keepAlive = true)): ch =>
+      assertEquals(ch.getOption(StandardSocketOptions.TCP_NODELAY), java.lang.Boolean.TRUE)
+      assertEquals(ch.getOption(StandardSocketOptions.SO_KEEPALIVE), java.lang.Boolean.TRUE)
+    applied(SocketOptions(noDelay = false, keepAlive = false)): ch =>
+      assertEquals(ch.getOption(StandardSocketOptions.TCP_NODELAY), java.lang.Boolean.FALSE)
+      assertEquals(ch.getOption(StandardSocketOptions.SO_KEEPALIVE), java.lang.Boolean.FALSE)
 
   test("sendBufferSize and receiveBufferSize reach the socket"):
     val requested = 1024 * 1024
-    val control   = connectWith(SocketOptions.default)
-    val tuned     = connectWith(
-      SocketOptions(sendBufferSize = Some(requested), receiveBufferSize = Some(requested))
-    )
-    try
-      val controlSend = control.channel.getOption(StandardSocketOptions.SO_SNDBUF).intValue
-      val controlRecv = control.channel.getOption(StandardSocketOptions.SO_RCVBUF).intValue
-      val tunedSend   = tuned.channel.getOption(StandardSocketOptions.SO_SNDBUF).intValue
-      val tunedRecv   = tuned.channel.getOption(StandardSocketOptions.SO_RCVBUF).intValue
-      assert(
-        tunedSend > controlSend,
-        s"sendBufferSize がソケットに届いていない（既定 $controlSend / 指定後 $tunedSend）"
+    val default   = applied(SocketOptions.default): ch =>
+      (
+        ch.getOption(StandardSocketOptions.SO_SNDBUF).intValue,
+        ch.getOption(StandardSocketOptions.SO_RCVBUF).intValue
       )
-      assert(
-        tunedRecv > controlRecv,
-        s"receiveBufferSize がソケットに届いていない（既定 $controlRecv / 指定後 $tunedRecv）"
-      )
-    finally
-      control.close()
-      tuned.close()
+    applied(SocketOptions(sendBufferSize = Some(requested), receiveBufferSize = Some(requested))): ch =>
+      val send = ch.getOption(StandardSocketOptions.SO_SNDBUF).intValue
+      val recv = ch.getOption(StandardSocketOptions.SO_RCVBUF).intValue
+      assert(send > default._1, s"sendBufferSize がソケットに届いていない（既定 ${ default._1 } / 指定後 $send）")
+      assert(recv > default._2, s"receiveBufferSize がソケットに届いていない（既定 ${ default._2 } / 指定後 $recv）")
 
-  test("leaving the buffer sizes unset does not touch them"):
-    val control = connectWith(SocketOptions.default)
+  test("leaving the buffer sizes unset keeps the platform defaults"):
+    val untouched = SocketChannel.open()
     try
-      assert(control.channel.getOption(StandardSocketOptions.SO_SNDBUF).intValue > 0)
-      assert(control.channel.getOption(StandardSocketOptions.SO_RCVBUF).intValue > 0)
-    finally control.close()
+      val send = untouched.getOption(StandardSocketOptions.SO_SNDBUF)
+      val recv = untouched.getOption(StandardSocketOptions.SO_RCVBUF)
+      applied(SocketOptions.default): ch =>
+        assertEquals(ch.getOption(StandardSocketOptions.SO_SNDBUF), send)
+        assertEquals(ch.getOption(StandardSocketOptions.SO_RCVBUF), recv)
+    finally untouched.close()
